5134c678d72eb12b19fdf6845ddfceea3db442b4
Mikel Cabezas <mikelcabezas@gmail.com>
2023-03-15 21:38:22 +0100

    add my prework

---
+++ staff/mikel-cabezas/prework/pasapalabra/pasapalabra5.js
@@ -0,0 +1,534 @@
+const styleGreen = [
+  'color: #bde093',
+].join(';');
+const styleRed = [
+  'color: #ff5722',
+].join(';');
+const styleBlue = [
+  'color: #4fc3f7',
+].join(';');
+const bigFont = [
+  'font-size: 16px',
+].join(';');
+const questions = [
+{
+  letter: "a",
+  answer: [
+      "asura",
+      "armadillo",
+      "a"
+  ]
+  ,
+  status: 0,
+  question:[
+      "¿Cómo se llama la técnica de espada más poderosa de Roronoa Zoro en One Piece?",
+    "¿Qué animal de la selva amazónica tiene una armadura natural?",
+    "Primera letra del abecedario inglés"
+  ],
+},
+{
+  letter: "b",
+  answer: [
+      "baroque works",
+      "ballena",
+      "b"
+  ],
+  status: 0,
+  question: [
+    "¿Cómo se llama la organización criminal creada por el antiguo Señor de la Guerra del Mar Crocodile en One Piece?",
+    "¿Qué animal es el más grande del mundo marino?",
+    "Segunda letra del abecedario inglés",
+  ]
+ },
+{
+  letter: "c",
+  answer: [ 
+  "chopper",
+  "canguro",
+  "c",
+  ],
+  status: 0,
+  question: [ 
+  "Medico de los Mugiwara en One Piece",
+  "¿Qué animal es famoso por ser el animal nacional de Australia?",
+  "Tercera letra del abecedario inglés",
+  ]
+}, 
+{
+  letter: "d",
+  answer: [
+      "Doflamingo",
+      "dinosaurio",
+      "d"
+  ],
+  status: 0,
+  question: [
+      "Nombre del capitan de los piratas Donquixote en One Piece",
+    "¿Qué raza animal se extinguó por los meteoritos?",
+    "Cuarta letra del abecedario inglés"
+    ]
+},
+{
+  letter: "e",
+  answer: [
+      "eiichiro oda",
+      "elefante",
+      "e",
+  ],
+  status: 0,
+  question: [
+    "¿Cómo se llama el autor de One Piece?",
+    "¿Qué animal es el más grande del mundo?",
+    "Quinta letra del abecedario inglés",
+  ]
+},
+{
+  letter: "f",
+  answer: [
+      "foxy",
+      "flamenco",
+      "f",
+  ],
+  status: 0,
+  question: [
+      "Pirata que tiene el poder de una fruta que le permite ralentizar a sus oponentes en One Piece",
+      "¿Qué animal es conocido por su pelaje y grandes patas traseras?",
+      "Sexta letra del abecedario inglés",
+  ]
+},
+{
+  letter: "g",
+  answer: [
+      "going merry",
+      "guepardo",
+      "g",
+  ],
+  status: 0,
+  question:[
+    "nombre del primer barco de los mugiwara en One Piece",
+    "¿Qué animal es famoso por su velocidad?",
+    "Séptima letra del abecedario inglés",
+  ]
+},
+{
+  letter: "h",
+  answer: [
+      "hawkins",
+      "hormigas",
+      "h",
+  ],
+  status: 0,
+  question: [
+      "Como se llama el Pirata y mago del North Blue que tiene extraños símbolos negros en sus cejas",
+      "Insectos hipersociales que suelen vivir en comunidades organizadas bajo tierra, en túmulos a nivel del suelo o en árboles",
+      "Octava letra del abecedario inglés"
+  ]
+},
+{
+  letter: "i",
+  answer: [
+      "ivankov",
+      "iguana",
+      "i",
+  ],
+  status: 0,
+  question: [
+      "Reina del Reino de Kamabakka y uno de los miembros fundadores del Ejército Revolucionario en One Piece",
+      "¿Qué animal puede cambiar de color su piel?",
+      "Novena letra del abecedario inglés",
+  ],
+},
+{
+  letter: "j",
+  answer: [
+      "jinbe",
+      "jirafa",
+      "j",
+  ],
+  status: 0,
+  question: [
+      "Nombre del gyojin tripulante de los mugiwara en One Piece",
+      "¿Qué animal tiene un cuello muy largo?",
+      "Décima letra del abecedario inglés",
+  ],
+},
+{
+  letter: "k",
+  answer: [
+      "katakuri",
+      "koala",
+      "k",
+  ],
+  status: 0,
+  question: [
+      "Hijo de Big Mom y enemigo de Luffy en el arco de Whole Cake",
+      "¿Cómo se llama el marsupial australiano que vive en los árboles?",
+      "Decimoprimera letra del abecedario inglés",
+  ],
+},
+{
+  letter: "l",
+  answer: [
+      "laboon",
+      "león",
+      "l",
+  ],
+  status: 0,
+  question: [
+      "Ballena compañera de brook en One Piece",
+      "¿Qué animal es un depredador y es el rey de la selva?",
+      "duodécima letra del abecedario inglés",
+  ],
+},
+{
+  letter: "m",
+  answer: [
+      "monkey d. luffy",
+      "murciélago",
+      "m",
+  ],
+  status: 0,
+  question: [
+    "Nombre completo del capitán de los Mugiwara",
+    "¿Qué animal vuela por la noche y se orienta gracias al eco?",
+    "decimotercera letra del abecedario inglés",
+  ]
+},
+{
+  letter: "n",
+  answer: [
+      "nami",
+      "nútria",
+      "n",
+  ],
+  status: 0,
+  question: [
+      "Como se llama la tripulante y navegante de los Piratas del Sombrero de Paja",
+      "¿Qué animal de la selva amazónica tiene un pelaje negro brillante y una larga cola prensil?",
+      "decimocuarta letra del abecedario inglés",
+  ],
+},
+{
+  letter: "o",
+  answer: [
+      "oden",
+      "oso polar",
+      "o",
+  ],
+  status: 0,
+  question: [
+    "Cual fue el antiguo daimyo de Kuri en el País de Wano, que mató Kaido.",
+    "¿Qué animal que comienza con la letra 'O' tiene una capa de grasa para protegerse del frío?",
+    "decimoquinta letra del abecedario inglés.",
+  ]
+},
+{
+  letter: "p",
+  answer: [
+      "poneglyph",
+      "pulpo",
+      "p",
+  ],
+  status: 0,
+  question: [
+    "Donde estan escritas las pistas para encontrar el One Piece",
+    "¿Qué animal vive en el océano y tiene tentáculos?",
+    "decimosexta letra del abecedario inglés",
+  ]
+},
+{
+  letter: "q",
+  answer: [
+      "queen",
+      "quebrantahuesos",
+      "q",
+  ],
+  status: 0,
+  question: [
+    "Cabeza de cartel Piaratas Bestia",
+    "ave rapaz diurna hace honor a su nombre al tirar huesos y caparazones desde considerables alturas poder alimentarse",
+    "decimoséptima letra del abecedario inglés",
+  ]
+},
+{ 
+  letter: "r", 
+  answer: [
+    "roronoa",
+    "rana",
+    "r",
+  ], 
+  status: 0, 
+  question: [
+    "Prenombre de Zoro",
+    "¿Qué animal es un reptil con una cresta en su espalda?",
+    "decimoctava letra del abecedario inglés",
+  ]
+},
+{
+  letter: "s",
+  answer: [
+      "skypiea",
+      "salmón",
+      "s",
+  ],
+  status: 0,
+  question: [
+  "Isla en el cielo de One Piece",
+  "pez marino y de agua dulce distribuido por los océanos y mares de casi todo el mundo",
+  "vigésima letra del abecedario inglés",
+]
+},
+{
+  letter: "t",
+  answer: [
+      "torao",
+      "tigre",
+      "t",
+  ],
+  status: 0,
+  question: [
+    "Diminutivo que sua Luffy para dirigirse a Law",
+    "¿Qué animal es el más grande del mundo de los felinos?",
+    "vigesimoprimera letra del abecedario inglés",
+  ]
+},
+{
+  letter: "u",
+  answer: [
+      "utah",
+      "orangután",
+      "u",
+  ],
+  status: 0,
+  question: [
+    "Hija de Shanks en One Piece",
+    "¿Qué animal vive en los árboles y se mueve con la ayuda de sus brazos?",
+    "vigesimosegunda letra del abecedario inglés",
+  ]
+},
+{
+  letter: "v",
+  answer: [
+      "nefertari vivi",
+      "víbora",
+      "v",
+  ],
+  status: 0,
+  question: [
+    "princesa heredera del reino de Arabasta",
+    "¿Qué animal es famoso por su veneno mortal?",
+    "vigesimotercera letra del abecedario inglés",
+  ]
+},
+{
+  letter: "w",
+  answer: [
+      "wapol",
+      "wapiti",
+      "w",
+  ],
+  status: 0,
+  question: [
+    "CONTIENE LA W. Rey del Reino de Black Drum",
+    "CONTIENE LA W. Tipo de ciervo canadiense",
+    "CONTIENE LA W. vigesimocuarta letra del abecedario inglés",
+  ]
+},
+{
+  letter: "x",
+  answer: [
+      "xebec",
+      "xantus",
+      "x",
+  ],
+  status: 0,
+  question: [
+      "capitán de los Piratas de Rocks conocido cono Rocks",
+      "Especie de colibrí mexicano",
+      "vigesimoquinta letra del abecedario inglés",
+]
+},
+{
+  letter: "y",
+  answer: [
+      "yamato",
+      "yegua",
+      "y",
+  ],
+  status: 0,
+  question: [
+    "Hija de Kaido",
+    "Hembra del caballo",
+    "vigesimosexta letra del abecedario inglés",
+  ]
+},
+{
+  letter: "z",
+  answer: [
+      "zunesha",
+      "zorro",
+      "z",
+  ],
+  status: 0,
+  question: [
+    "nombre del  elefante que vaga por el Nuevo Mundo cargando en su lomo la isla de Zou",
+    "Animal salvaje astuto, ágil inteligente y escurridizo zorro",
+    "vigesimoseptima letra del abecedario inglés",
+  ]
+},
+];
+
+let updatedRanking = []
+let gameStatus = true
+let userPoints = 0
+let questionsPassed = []
+let gamePlayerControl = 0
+
+const getGameStatus = () => {
+  if (!gameStatus) {
+      return console.log('bye')
+  }
+  playWords(questions)
+}
+const playWords = (questions) => {
+  questions.forEach(currentAnswer => {
+      if (currentAnswer.status === 0 && gameStatus) {    
+          const userInput = getUserInput(currentAnswer, gamePlayerControl)
+          controlFlux(currentAnswer)
+      }
+  })
+  if (gameStatus) {
+      playWords(questions)
+  }
+}
+
+
+
+const getUserInput = (currentAnswer, questionNumber) => {
+  const askQiestion = prompt(`With the letter ${currentAnswer.letter}: \n${currentAnswer.question[questionNumber]}.\nWrite your answer or type pasapalabra or end`)
+  console.log(`%cCon la letra ${currentAnswer.letter}: \n${currentAnswer.question[questionNumber]}`, styleBlue)
+
+  if (!askQiestion) {
+      alert('Empty value is not valid ❌. Please type anything.')
+      return getUserInput(currentAnswer, gamePlayerControl)
+  }
+
+  const answer = askQiestion.toLowerCase()
+
+  if (answer === 'end') {
+    console.log(`%cGood bye! Have a beatutiful time!`, bigFont)
+    return gameStatus = false
+  }
+
+  if (answer === 'pasapalabra') {
+      currentAnswer.status = 0
+      gameStatus = true
+  }
+
+  if (answer !== currentAnswer.answer[questionNumber] && answer !== 'end' && answer !== 'pasapalabra') {
+      currentAnswer.status = 2
+      console.log(`%cSorry, ${answer} is incorrect. The correct word is: ${currentAnswer.answer[questionNumber]}`, styleRed)
+      questionsPassed.push(currentAnswer.status)
+  }
+  
+  if (currentAnswer.answer[questionNumber] === answer && answer !== 'end' && answer !== 'pasapalabra') {
+      
+      userPoints = userPoints + 1
+      console.log(`%c${answer} is correct and you win 1 point! Total points: ${userPoints}`, styleGreen)
+      currentAnswer.status = 1
+      questionsPassed.push(currentAnswer.status) 
+  }
+
+}
+
+const controlFlux = (currentAnswer) => {
+  if (questions.length === questionsPassed.length) {
+      getPoints(currentAnswer)
+      gameStatus = false
+      return false
+  }
+  
+  if (currentAnswer.status > 0) {
+      return 
+  }
+  
+  if (currentAnswer.status = 0) {
+      gameStatus = true
+      return true
+  }
+}
+
+const getPoints = (currentAnswer) => {
+  if (userPoints === questionsPassed.length) {
+      console.log(`%cThe game is end. You have answered all questions correctly! Your score is: ${userPoints} points`, styleGreen)
+  } else {
+  console.log(`%c\nThe game is end. You have answered ${userPoints} questions correctly, and ${questionsPassed.length - userPoints} wrong`, styleBlue)
+  }
+  playAgain(currentAnswer)
+}
+
+const playAgain = (currentAnswer) => {
+  const userWantToPlayAgain = confirm(`Do you want to play the game again?`)
+
+  if (userWantToPlayAgain) {
+      gameStatus = true
+      questionsPassed = []
+
+      for (const question of questions) {
+          question.status = 0
+      }
+      
+      getGamePlayerControl(currentAnswer)
+      updateRanking()
+      playGame()
+  } else {
+      gameStatus = false
+      updateRanking()
+      getRanking()
+      console.log(`%cGood bye! Have a beatutiful time!`, bigFont)
+  }
+}
+
+
+const getGamePlayerControl = (currentAnswer) => {
+  gamePlayerControl = gamePlayerControl + 1
+  const questionsLength = currentAnswer.answer.length;
+
+  if (gamePlayerControl >= questionsLength) {
+      gamePlayerControl = 0
+  }
+}
+
+const updateRanking = () => {
+  const rankingCurrentUser = {name: userName, points: userPoints}
+  updatedRanking.push(rankingCurrentUser)
+}
+
+const getRanking = () => {
+  updatedRanking.sort((less, more) => more.points - less.points)
+  console.log('And the rank is...')
+  updatedRanking.forEach(position => console.log(`${position.name} ${position.points}`));
+}
+
+const playGame = () =>{
+  userPoints = 0
+  userName = prompt('Welcome to ISDI Pasapalabra! Before start the game, whats your name?')
+
+  if (!userName || !/^[a-zA-Z]+$/.test(userName)) {
+    alert('Please, write your name only with letters ❌')
+    playGame()
+  }
+
+  alert(`Nice ${userName}! This game has, ${questions.length} questions. Any question has a hint and a letter it contains wich word. Good Luck! 🤘`)
+  const startGame = confirm(`${userName}, do you want to start the game?`)
+
+  if (startGame) {
+      while (gameStatus) {
+          getGameStatus()
+      }
+  } else {
+      console.log(`%cGood bye! Have a beatutiful time!`, bigFont)
+  }
+}
+
+playGame()
