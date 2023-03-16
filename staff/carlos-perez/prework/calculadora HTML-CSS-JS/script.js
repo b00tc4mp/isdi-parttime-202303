bf0f82f47e4b1a4f2f96b63021142fa73fabe4a3
iCarlosPro <cjps91@gmail.com>
2023-03-16 20:09:59 +0100

    Update Calculadora HTML

--- staff/carlos-perez/prework/calculadora HTML-CSS-JS/script.js
+++ staff/carlos-perez/prework/calculadora HTML-CSS-JS/script.js
@@ -1,56 +1,126 @@
-let register='';
+let register = '';
 
 const toScreen = (value) => {
     document.getElementById("screen").textContent += value;
 }
 
-const calculate= () => {
-    let expresion = document.getElementById("screen").textContent;
-    document.getElementById("previous").textContent=expresion;
-    let result=eval(expresion);
-    if(result=='Infinity'){
-        clearScreen();
-        alert('No puedes dividir algo entre 0. Es una indeterminación');
-    }
-    else{
-        document.getElementById("screen").textContent =result;
-    }
-}
 
 const clearScreen = () => {
     document.getElementById("screen").textContent = "";
-    document.getElementById("previous").textContent="";
+    document.getElementById("previous").textContent = "";
 }
 
 const signChange = () => {
     let expresion = document.getElementById("screen").textContent;
-    let result=eval(expresion);
-    result=result*(-1);
+    let result = calculator(tokenize(expresion));
+    result=isDecimal(result);
+    result = result * (-1);
     document.getElementById("screen").textContent = result;
 }
 
 const percentage = () => {
     let expresion = document.getElementById("screen").textContent;
-    let result=eval(expresion);
-    result=result/100;
+    let result = calculator(tokenize(expresion));
+    result=isDecimal(result);
+    result = result / 100;
     document.getElementById("screen").textContent = result;
 }
 
 const useRegister = () => {
-    if(register!==''){
+    if (register !== '') {
         toScreen(register);
     }
 }
 
-const addToRegister =  () => {
-    register=document.getElementById("screen").textContent;
+const addToRegister = () => {
+    register = document.getElementById("screen").textContent;
 }
 
 const deleteRegister = () => {
-    register='';
+    register = '';
 }
 
 const generateRandom = () => {
-    let randomNumber=Math.random();
+    let randomNumber = Math.random();
     toScreen(randomNumber.toFixed(6).toString());
+}
+
+
+function tokenize(s) {
+    const r = [];
+    let token = '';
+    for (const character of s) {
+        if ('^*/+-'.includes(character)) {
+            if (token === '' && character === '-') {
+                token = '-';
+            } else {
+                r.push(parseFloat(token), character);
+                token = '';
+            }
+        } else {
+            token += character;
+        }
+    }
+    if (token !== '') {
+        r.push(parseFloat(token));
+    }
+    return r;
+}
+
+function calculator(tokens) {
+    const operatorPrecedence = [{'^': (a, b) => Math.pow(a, b)},
+               {'*': (a, b) => a * b, '/': (a, b) => a / b},
+               {'+': (a, b) => a + b, '-': (a, b) => a - b}];
+    let operator;
+    for (const operators of operatorPrecedence) {
+        const newTokens = [];
+        for (const token of tokens) {
+            if (token in operators) {
+                operator = operators[token];
+            } else if (operator) {
+                newTokens[newTokens.length - 1] = 
+                    operator(newTokens[newTokens.length - 1], token);
+                operator = null;
+            } else {
+                newTokens.push(token);
+            }
+        }
+        tokens = newTokens;
+    }
+    if (tokens.length > 1) {
+        console.log('Error: No puede resolverse la operación');
+        return tokens;
+    } else {
+        return tokens[0];
+    }
+}
+
+const isDecimal = (number) =>{
+    if(Number.isInteger(number)===false){
+        return number.toFixed(6);
+    }
+    else{
+        return number;
+    }
+}
+
+const calculate = () => {
+    let expresion = document.getElementById("screen").textContent;
+    document.getElementById("previous").textContent = expresion;
+    let result = calculator(tokenize(expresion));
+    result=isDecimal(result);
+    if (isNaN(result)) {
+        clearScreen();
+        alert('Error. Operación incorrecta');
+    }
+    else if(result === "Infinity"){
+        clearScreen();
+        alert('No se puede dividir por 0. Indeterminación');
+    }
+    else {
+        document.getElementById("screen").textContent = result;
+        if(expresion.length>15){
+            document.getElementById("previous").textContent="";
+        }
+    }
 }
