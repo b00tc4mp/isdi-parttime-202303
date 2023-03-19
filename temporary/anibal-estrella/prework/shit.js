8939ba9a520e7ef01e5403c373cafa35384299f6
anibalestrella <anibalestrella@gmail.com>
2023-03-19 18:46:10 +0100

    commit Merge branch 'feature/prework' into develop

--- temporary/anibal-estrella/prework/shit.js
+++
@@ -1,6 +0,0 @@
-const word = "ajedrez";
-const alphabet="abcdefghijklmnopqrstuvwxyz";
-// generate an object from the word
-const wordsToGuess= word.split("").map((letter) => ({letter: letter, isGuessed: false }));
-
-
