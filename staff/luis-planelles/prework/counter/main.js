74b8374104a2a456bb1da620f4450d8bb778aa44
BirdOnTheBranch <planelles.luis@gmail.com>
2023-03-21 18:27:36 +0100

    add 'use strict'

--- staff/luis-planelles/prework/counter/main.js
+++ staff/luis-planelles/prework/counter/main.js
@@ -1,37 +1,41 @@
 //
-const buttons = document.querySelectorAll(".button"),
-  increaseButton = buttons[0],
-  decreaseButton = buttons[1],
-  restartButton = buttons[2];
+(() => {
+  "use strict";
 
-let score = 0,
-  small = document.querySelector("#score");
+  const buttons = document.querySelectorAll(".button"),
+    increaseButton = buttons[0],
+    decreaseButton = buttons[1],
+    restartButton = buttons[2];
 
-counter = () => {
-  const increaseScore = (limitScoreMax = 50) => {
-    score = score < limitScoreMax ? score + 1 : limitScoreMax;
-    return score;
+  let score = 0,
+    small = document.querySelector("#score");
+
+  const counter = () => {
+    const increaseScore = (limitScoreMax = 50) => {
+      score = score < limitScoreMax ? score + 1 : limitScoreMax;
+      return score;
+    };
+
+    const decreaseScore = (limitScoreMin = 0) => {
+      score = score > limitScoreMin ? score - 1 : limitScoreMin;
+      return score;
+    };
+
+    increaseButton.addEventListener("click", () => {
+      let scoreIncreased = increaseScore();
+      small.innerText = scoreIncreased;
+    });
+
+    decreaseButton.addEventListener("click", () => {
+      let scoreDecreased = decreaseScore();
+      small.innerText = scoreDecreased;
+    });
+
+    restartButton.addEventListener("click", () => {
+      score = 0;
+      small.innerText = score;
+    });
   };
 
-  const decreaseScore = (limitScoreMin = 0) => {
-    score = score > limitScoreMin ? score - 1 : limitScoreMin;
-    return score;
-  };
-
-  increaseButton.addEventListener("click", () => {
-    let scoreResult = increaseScore();
-    small.innerText = scoreResult;
-  });
-
-  decreaseButton.addEventListener("click", () => {
-    let scoreResult = decreaseScore();
-    small.innerText = scoreResult;
-  });
-
-  restartButton.addEventListener("click", () => {
-    score = 0;
-    small.innerText = score;
-  });
-};
-
-counter();
+  counter();
+})();
