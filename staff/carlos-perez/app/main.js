27f6c85dacde3b7229371dfbddc05fcccf575889
iCarlosPro <cjps91@gmail.com>
2023-03-22 19:23:52 +0100

    Add form and on and off functions

--- staff/carlos-perez/app/main.js
+++ staff/carlos-perez/app/main.js
@@ -1,1 +1,19 @@
 /*Add form functions*/
+
+function initiate(){
+    document.querySelector('.login').classList.add('off');
+    document.querySelector('.home').classList.add('off');
+}
+
+initiate();
+
+document.querySelector('.formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+    document.querySelector('.registro').classList.add('off');
+    document.querySelector('.login').classList.remove('off');
+})
+document.querySelector('.formulario-login').addEventListener('submit', function(event) {
+    event.preventDefault();
+    document.querySelector('.login').classList.add('off');
+    document.querySelector('.home').classList.remove('off');
+})
