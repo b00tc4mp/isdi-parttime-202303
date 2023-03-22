e1eb6d08305a7763e16fd0d7e889537b6403bdc7
iCarlosPro <cjps91@gmail.com>
2023-03-22 20:07:43 +0100

    Add navigation functions

--- staff/carlos-perez/app/main.js
+++ staff/carlos-perez/app/main.js
@@ -1,19 +1,33 @@
-/*Add form functions*/
+var registerPage = document.querySelector('.registro');
+var loginPage= document.querySelector('.login');
+var homePage= document.querySelector('.home');
 
 function initiate(){
-    document.querySelector('.login').classList.add('off');
-    document.querySelector('.home').classList.add('off');
+    loginPage.classList.add('off');
+    homePage.classList.add('off');
 }
 
 initiate();
 
 document.querySelector('.formulario').addEventListener('submit', function(event) {
     event.preventDefault();
-    document.querySelector('.registro').classList.add('off');
-    document.querySelector('.login').classList.remove('off');
+    registerPage.classList.add('off');
+    loginPage.classList.remove('off');
 })
 document.querySelector('.formulario-login').addEventListener('submit', function(event) {
     event.preventDefault();
-    document.querySelector('.login').classList.add('off');
-    document.querySelector('.home').classList.remove('off');
+    loginPage.classList.add('off');
+    homePage.classList.remove('off');
+})
+
+registerPage.querySelector('a').addEventListener('click', function(event){
+    event.preventDefault();
+    registerPage.classList.add('off');
+    loginPage.classList.remove('off');
+})
+
+loginPage.querySelector('a').addEventListener('click', function(event){
+    event.preventDefault();
+    loginPage.classList.add('off');
+    registerPage.classList.remove('off');
 })
