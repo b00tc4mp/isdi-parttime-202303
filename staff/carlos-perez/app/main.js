7dca2222d6cfd26526be5be26196cf24146da2b7
iCarlosPro <cjps91@gmail.com>
2023-03-22 21:34:09 +0100

    Add user logic

--- staff/carlos-perez/app/main.js
+++ staff/carlos-perez/app/main.js
@@ -1,3 +1,29 @@
+//Data
+
+let users = [];
+
+users.push({
+    name: 'Wendy Darling',
+    email: 'wendy@darling.com',
+    password: '123123123'
+})
+
+users.push({
+    name: 'Peter Pan',
+    email: 'peter@pan.com',
+    password: '123123123'
+})
+
+users.push({
+    name: 'Pepito Grillo',
+    email: 'pepito@grillo.com',
+    password: '123123123'
+})
+
+//Logic
+
+//Presentation
+
 var registerPage = document.querySelector('.registro');
 var loginPage= document.querySelector('.login');
 var homePage= document.querySelector('.home');
@@ -9,15 +35,68 @@ function initiate(){
 
 initiate();
 
+function userExist(email){
+    let userPosition=null;
+    for(let i=0; i<users.length; i++){
+        let user=users[i];
+        if(user.email===email){
+            userPosition=i;
+            break;
+        }
+    }
+    if(userPosition !== null){
+        return userPosition;
+    }
+    else{
+        return -1;
+    }
+}
+
+function addUser(name,email,password){
+    users.push({
+        name: name,
+        email: email,
+        password: password
+    });
+}
+
 document.querySelector('.formulario').addEventListener('submit', function(event) {
     event.preventDefault();
-    registerPage.classList.add('off');
-    loginPage.classList.remove('off');
+
+    var name=registerPage.querySelector('input[id=nombre]').value;
+    var email=registerPage.querySelector('input[type=email]').value;
+    var password=registerPage.querySelector('input[type=password]').value;
+
+    let userPosition=userExist(email);
+
+    if(userPosition === -1){
+        addUser(name, email, password);
+        registerPage.classList.add('off');
+        loginPage.classList.remove('off');
+    }
+    else{
+        alert("Ha habido un error en el registro");
+    }
+
+    
 })
 document.querySelector('.formulario-login').addEventListener('submit', function(event) {
     event.preventDefault();
-    loginPage.classList.add('off');
-    homePage.classList.remove('off');
+
+    var email=loginPage.querySelector('input[type=email]').value;
+    var password=loginPage.querySelector('input[type=password]').value;
+    
+    let userPosition=userExist(email);
+
+    if(userPosition !== -1 && users[userPosition].password===password){
+        loginPage.classList.add('off');
+        homePage.classList.remove('off');
+    }
+    else{
+        alert("Usuario o contraseña incorrectos");
+    }
+
+    
 })
 
 registerPage.querySelector('a').addEventListener('click', function(event){
