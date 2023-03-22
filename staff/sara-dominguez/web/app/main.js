8ab8811323789431ce17b12ecc8ace096b2a4341
sarad11 <125995487+sarad11@users.noreply.github.com>
2023-03-22 23:50:32 +0100

    modify files

---
+++ staff/sara-dominguez/web/app/main.js
@@ -0,0 +1,74 @@
+// data
+
+var users = []
+
+users.push({
+    name: 'Wendy Darling',
+    email: 'wendy@darling.com',
+    password: '123123123'
+})
+
+users.push({
+    name: 'Peter Pan',
+    email: 'peter@pan.com',
+    password: '123123123'
+})
+
+users.push({
+    name: 'Pepito Grillo',
+    email: 'pepito@grillo.com',
+    password: '123123123'
+})
+
+// logic
+
+// presentation
+
+var registerPage = document.querySelector('.register')
+var loginPage = document.querySelector('.login')
+var homePage = document.querySelector('.home')
+
+registerPage.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    registerPage.classList.add('off')
+    loginPage.classList.remove('off')
+})
+
+loginPage.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var email = loginPage.querySelector('input[name=email]').value
+    var password = loginPage.querySelector('input[name=password]').value
+
+    var foundUser
+
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i]
+
+        if (user.email === email) {
+            foundUser = user
+
+            break
+        }
+    }
+
+    if (foundUser !== undefined && foundUser.password === password) {
+        loginPage.classList.add('off')
+        homePage.classList.remove('off')
+    } else alert('wrong email or password')
+})
+
+registerPage.querySelector('a').addEventListener('click', function (event) {
+    event.preventDefault()
+
+    registerPage.classList.add('off')
+    loginPage.classList.remove('off')
+})
+
+loginPage.querySelector('a').addEventListener('click', function (event) {
+    event.preventDefault()
+
+    loginPage.classList.add('off')
+    registerPage.classList.remove('off')
+})
