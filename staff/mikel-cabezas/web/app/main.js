6492efbb93ca4fa7db164b43a10b5443a53ff252
Mikel Cabezas <mikelcabezas@gmail.com>
2023-03-22 19:04:26 +0100

    update web project

--- staff/mikel-cabezas/web/app/main.js
+++ staff/mikel-cabezas/web/app/main.js
@@ -1,19 +1,10 @@
-const loginMenu = document.querySelector('.submenu-element .login')
-const registerMenu = document.querySelector('.submenu-element .register')
-
-window.onload = () => {
-    loginMenu.onclick = () => {
-        loginMenu.classList.add('hidden')
-        registerMenu.classList.remove('hidden')
-    }
-}
-
-const createFirstNumber = (screenNumber,screenNumbersButton, completeScreenNumber) => {
-    let numberButtons = document.querySelectorAll(".numberButton");
-    numberButtons.forEach((numberButton) => {
-        numberButton.addEventListener("click",function(){
-            screenNumber.push(numberButton.textContent);
-            setNumberOnScreen(screenNumber,screenNumbersButton,completeScreenNumber);
-        })
-    })
-}
+document.querySelector('.section.register form.register-form').addEventListener('submit', function(event) {
+    event.preventDefault()
+    document.querySelector('.section.register').classList.add('off')
+    document.querySelector('.section.login.section').classList.remove('off')
+})
+document.querySelector('.section.login form.login-form').addEventListener('submit', function(event) {
+    event.preventDefault()
+    document.querySelector('.section.login').classList.add('off')
+    document.querySelector('.section.home').classList.remove('off')
+})
