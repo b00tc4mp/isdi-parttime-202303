d33043f884fcd5073ece3d27f95704da458ac5fc
Mikel Cabezas <mikelcabezas@gmail.com>
2023-03-22 21:57:01 +0100

    add js functionality web project

--- staff/mikel-cabezas/web/app/main.js
+++ staff/mikel-cabezas/web/app/main.js
@@ -1,10 +1,63 @@
+const users = []
+
+users.push({
+    name: 'Wendy Darling',
+    email: 'wendy@darling.com',
+    password: '123123123'
+})
+users.push({
+    name: 'Peter Pan',
+    email: 'peter@pan.com',
+    password: '123123123'
+})
+users.push({
+    name: 'Pepito Grillo',
+    email: 'pepito@grillo.com',
+    password: '123123123'
+})
+
+const registerPage = document.querySelector('.section.register')
+const loginPage = document.querySelector('.section.login')
+const homePage = document.querySelector('.section.home')
+const bodyPage = document.querySelector('body')
+
 document.querySelector('.section.register form.register-form').addEventListener('submit', function(event) {
     event.preventDefault()
-    document.querySelector('.section.register').classList.add('off')
-    document.querySelector('.section.login.section').classList.remove('off')
+    registerPage.classList.add('off')
+    loginPage.classList.remove('off')
 })
 document.querySelector('.section.login form.login-form').addEventListener('submit', function(event) {
     event.preventDefault()
-    document.querySelector('.section.login').classList.add('off')
-    document.querySelector('.section.home').classList.remove('off')
+    // const email = loginPage.querySelector('input[name="email"').value
+    // const password = loginPage.querySelector('input[name="password"').value
+    let foundUser
+
+    for(i = 0; i < users.length; i++ ) {
+
+    } 
+
+    loginPage.classList.add('off')
+    homePage.classList.remove('off')
+    bodyPage.classList.add('logged-in')
+})
+
+document.querySelector('.logout').addEventListener('click', function(event) {
+    event.preventDefault()
+    bodyPage.classList.remove('logged-in')
+    registerPage.classList.remove('off')
+    loginPage.classList.add('off')
+    homePage.classList.add('off')
+})
+
+document.querySelector('.login-link a').addEventListener('click', function(event) {
+    event.preventDefault()
+    loginPage.classList.remove('off')
+    registerPage.classList.add('off')
+})
+
+document.querySelector('.register-link a').addEventListener('click', function(event) {
+    event.preventDefault()
+    alert('')
+    registerPage.classList.remove('off')
+    loginPage.classList.add('off')
 })
