bd85220f5c8dad34dab90779ed9d636eeefbe263
Lucas Diaz <lucasdamian30@gmail.com>
2023-03-22 17:55:05 +0000

    update files in app folder

--- staff/lucas-diaz/web/app/main.js
+++ staff/lucas-diaz/web/app/main.js
@@ -1,20 +1,19 @@
 const registerPage = document.querySelector(".register");
-
+const logInPage = document.querySelector(".login")
+const homePage = document.querySelector(".home");
 
 const registerForm = document.querySelector(".register form");
+const logInForm = document.querySelector(".login form")
 
 registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log("hola")
+    registerPage.classList.add("off");
+    logInPage.classList.remove("off");
 })
 
-
-
-
-
-
-//lucas
-
-
-
+logInForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    logInPage.classList.add("off");
+    homePage.classList.remove("off");
+})
 
