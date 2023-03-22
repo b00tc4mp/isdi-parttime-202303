44bfd06f061e253865dbd58995b0086768a2b1e5
Lucas Diaz <lucasdamian30@gmail.com>
2023-03-22 02:13:22 +0000

    update web

--- staff/lucas-diaz/web/app/main.js
+++ staff/lucas-diaz/web/app/main.js
@@ -1,3 +1,14 @@
-const signUpButton = document.querySelector(".submit-sign-up");
+const registerPage = document.querySelector(".register");
+
+
+const registerSubmitButton = document.querySelector(".submit-sign-up");
+
+registerSubmitButton.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log("hola")
+})
+
+
+
 
 
