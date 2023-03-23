c8dadf176556b1473a45e0da97eeff7d702832de
manuelbarzi <manuelbarzi@gmail.com>
2023-03-23 21:50:16 +0100

    add js cohersion and short-circuit evaluation

---
+++ stuff/js/typeof.js
@@ -0,0 +1,27 @@
+typeof NaNa
+'undefined'
+typeof NaN
+'number'
+typeof 1
+'number'
+typeof true
+'boolean'
+typeof 'hola'
+'string'
+typeof {}
+'object'
+typeof []
+'object'
+typeof null
+'object'
+typeof undefined
+'undefined'
+typeof function() {}
+'function'
+
+m + 1
+// VM7431:1 Uncaught ReferenceError: m is not defined
+//     at <anonymous>:1:1
+// (anonymous) @ VM7431:1
+typeof m
+'undefined'
