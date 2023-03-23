da633d3c6e347c840aae1a3d7b79d4fc5a35ae0d
Lara Amadeo <laraamadeo@Toqios-MacBook-Pro.local>
2023-03-23 17:32:35 +0100

    refine my web app

--- staff/lara-amadeo/web/app/main.js
+++ staff/lara-amadeo/web/app/main.js
@@ -1,13 +1,64 @@
-document.querySelector('.registration').querySelector('.centered-containers').querySelector('form').addEventListener('submit', function(event){
+var registrationPage = document.querySelector('.registration')
+var loginPage = document.querySelector('.login')
+var homePage = document.querySelector('.homepage')
+
+var users = [{username:'example',
+                email:'example@example',
+            password: 'example'}]
+
+
+
+registrationPage.querySelector('form').addEventListener('submit', function(event){
     event.preventDefault()
-   
-    document.querySelector('.registration').classList.add('off')
-    document.querySelector('.login').classList.remove('off')
+    
+   var registrationName = registrationPage.querySelector('input[name=username]').value
+   var registrationEmail = registrationPage.querySelector('input[name=email]').value
+   var registrationPassword = registrationPage.querySelector('input[name=password]').value
+
+
+//    var checkUserExists = (array) => {
+//         for (let i = 0; i < array.length; i++){
+//             var user = array[i]
+//             if(registrationEmail === user.email){
+//                 registrationPage.querySelector('.error-message').classList.remove('off')
+//             } else {
+//                 array.push({
+//                     username: registrationName,
+//                     email: registrationEmail,
+//                     password: registrationPassword
+//                    })
+//                 registrationPage.classList.add('off')
+//                 loginPage.classList.remove('off')
+//             }
+//         }
+//    }
+
+   checkUserExists(users)
 })
 
-document.querySelector('.login .centered-containers form').addEventListener('submit', function(event){
+registrationPage.querySelector('a').addEventListener('click', function(event){
+    event.preventDefault()
+    registrationPage.classList.add('off')
+    loginPage.classList.remove('off')
+})
+
+loginPage.querySelector('.primary-button').addEventListener('click', function(event){
     event.preventDefault()
 
-    document.querySelector('.login').classList.add('off')
-    document.querySelector('.homepage').classList.remove('off')
+    var inputEmail = loginPage.querySelector('input[name=email]').value
+    var inputPassword = loginPage.querySelector('input[name=password]').value
+
+    var checkCredentials = (users) => {
+        for (let i = 0; i < users.length; i++)
+            var user = users[i]
+
+        if (inputEmail === user.email && inputPassword === user.password){
+            loginPage.classList.add('off')
+            homePage.classList.remove('off')
+        } else loginPage.querySelector('.error-message').classList.remove('off')
+    }
+
+    checkCredentials(users)
 })
+
+
