f38ff65134a1ae1deb09954d70bd8ca909a9fc9a
Lucas Diaz <lucasdamian30@gmail.com>
2023-03-23 17:44:57 +0000

    update web folder

--- staff/lucas-diaz/web/app/main.js
+++ staff/lucas-diaz/web/app/main.js
@@ -13,25 +13,26 @@ users.push({
     email: 'peter@pan.com',
     password: '123123123'
 })
-
 users.push({
     name: 'Pepito Grillo',
     email: 'pepito@grillo.com',
     password: '123123123'
 })
 
-
 // logic
 
 // presentation
-
 
 const registerPage = document.querySelector(".register");
 const logInPage = document.querySelector(".login")
 const homePage = document.querySelector(".home");
 const registerForm = document.querySelector(".register form");
-const logInForm = document.querySelector(".login form")
+const logInForm = document.querySelector(".login form");
 const registerFormAnchor = document.querySelector(".login .register-anchor");
+const successRegisterAdivice = document.querySelector(".success-advice-p");
+const failRegisterAdvice = document.querySelector(".register .fail-advice-p");
+const successLogInAdvice = document.querySelector(".login .fail-advice-p")
+
 
 registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -42,8 +43,7 @@ registerForm.addEventListener('submit', function (event) {
         email: registerPage.querySelector("input[type=email]").value,
         password: registerPage.querySelector("input[type=password]").value
     }
-
-    var checkIfUserExist = users.find(user => user.name === temporalUser.name && user.email === temporalUser.email && user.password === temporalUser.password);
+    var checkIfUserExist = users.find(user => user.email === temporalUser.email);
 
     /* find coge la primera coincidencia y devuelve un array, podemos considerarlo como truthy, si no lo encuentra En JavaScript, se considera que undefined, junto con null, 0, NaN, '' (cadena vacía) y false, son valores falsy, es decir, valores que se evalúan como falso en un contexto booleano. entonces aqui checkeamos precisamente eso, si no esta en ese array, si devuelve un falsy, porque es lo mismo que if(!checkIfUserExist === true) 
     
@@ -64,49 +64,47 @@ registerForm.addEventListener('submit', function (event) {
         users.push(temporalUser);
         registerPage.classList.add("off");
         logInPage.classList.remove("off");
+        successRegisterAdivice.classList.remove("off");
+        setTimeout(() => {
+            successRegisterAdivice.classList.add("off")
+        },2000);
     } else {
-        alert("Hay un usuario con esos datos, por favor ingrese otros");
+        failRegisterAdvice.classList.remove("off");
+        setTimeout(() => {
+            failRegisterAdvice.classList.add("off")
+        }, 3000);
         registerPage.querySelector("input[type=text]").value = ""
         registerPage.querySelector("input[type=email]").value = ""
         registerPage.querySelector("input[type=password]").value = ""
     }
-
 })
 
 logInForm.addEventListener('submit', function (event) {
     event.preventDefault();
-
-
     var email = logInPage.querySelector('input[name=email]').value
     var password = logInPage.querySelector('input[name=password]').value
-
     var foundUser;
-
     for (var i = 0; i < users.length; i++) {
         var user = users[i]
-
         if (user.email === email) {
             foundUser = user
-
             break
         }
     }
-
     if (foundUser !== undefined && foundUser.password === password) {
         logInPage.classList.add("off");
         homePage.classList.remove("off");
-    } else alert('wrong email or password')
+    } else {
+        successLogInAdvice.classList.remove("off");
 
+        setTimeout(() => {
+            successLogInAdvice.classList.add("off")
+        }, 3000);
+    }
 })
-
 
 registerFormAnchor.addEventListener("click", function (event){
     event.preventDefault();
     logInPage.classList.add("off");
     registerPage.classList.remove("off");
 })
-
-
-// controlar que el <a> nos lleve a log in
-
-
