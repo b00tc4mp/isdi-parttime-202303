2de5243435c124e2b48133109b145c3afb8bcc97
manuelbarzi <manuelbarzi@gmail.com>
2023-03-23 21:47:05 +0100

    separate responsabilities logic vs presentation

--- staff/manuel-barzi/web/app/scripts/main.js
+++ staff/manuel-barzi/web/app/scripts/main.js
@@ -1,38 +1,23 @@
-// data
-
-var users = []
-
-users.push({
-    name: 'Wendy Darling',
-    email: 'wendy@darling.com',
-    password: '123123123'
-})
-
-users.push({
-    name: 'Peter Pan',
-    email: 'peter@pan.com',
-    password: '123123123'
-})
-
-users.push({
-    name: 'Pepito Grillo',
-    email: 'pepito@grillo.com',
-    password: '123123123'
-})
-
-// logic
-
-// presentation
-
 var registerPage = document.querySelector('.register')
 var loginPage = document.querySelector('.login')
 var homePage = document.querySelector('.home')
+var authenticatedEmail
 
 registerPage.querySelector('form').addEventListener('submit', function (event) {
     event.preventDefault()
 
-    registerPage.classList.add('off')
-    loginPage.classList.remove('off')
+    var name = registerPage.querySelector('input[name=name]').value
+    var email = registerPage.querySelector('input[name=email]').value
+    var password = registerPage.querySelector('input[name=password]').value
+
+    var result = registerUser(name, email, password)
+
+    if (result === false) {
+        alert('user already exists')
+    } else {
+        registerPage.classList.add('off')
+        loginPage.classList.remove('off')
+    }
 })
 
 loginPage.querySelector('form').addEventListener('submit', function (event) {
@@ -41,28 +26,20 @@ loginPage.querySelector('form').addEventListener('submit', function (event) {
     var email = loginPage.querySelector('input[name=email]').value
     var password = loginPage.querySelector('input[name=password]').value
 
-    var foundUser
+    var result = authenticateUser(email, password)
 
-    for (var i = 0; i < users.length; i++) {
-        var user = users[i]
-
-        if (user.email === email) {
-            foundUser = user
-
-            break
-        }
-    }
-
-    if (foundUser !== undefined && foundUser.password === password) {
+    if (result === false) {
+        alert('wrong email or password')
+    } else {
+        authenticatedEmail = email
+    
         loginPage.classList.add('off')
         homePage.classList.remove('off')
-    } else alert('wrong email or password')
+    }
 })
 
 registerPage.querySelector('a').addEventListener('click', function (event) {
     event.preventDefault()
-
-    // TODO implement registration logic here
 
     registerPage.classList.add('off')
     loginPage.classList.remove('off')
@@ -74,3 +51,7 @@ loginPage.querySelector('a').addEventListener('click', function (event) {
     loginPage.classList.add('off')
     registerPage.classList.remove('off')
 })
+
+// TODO show "hello, <username>" on login
+// TODO add link to profile in home page and open a profile panel
+// TODO add a form in profile panel to allow the user to update his/her password (asking current password, and new password and new password confirmation)
