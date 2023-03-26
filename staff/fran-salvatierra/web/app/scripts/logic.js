d6e33c574d7e6b3bae0916006ca53a1250a42c66
unknown <fran6salvatierra@gmail.com>
2023-03-26 18:07:30 +0200

    Weekend work update 0.1

--- staff/fran-salvatierra/web/app/scripts/logic.js
+++ staff/fran-salvatierra/web/app/scripts/logic.js
@@ -1,30 +1,34 @@
-function registerUser(name, email, password) {
+function registerUser(registerName, registerEmail, registerPassword) {
+    debugger
     var foundUser
+    var registerConfirmedPassword = registerPage.querySelector('input[name=confirm-password]').value 
 
     for(var i = 0; i < users.length; i++) {
         var user = users[i]
 
-        if (user.email == email) {
-            foundUser = user
-
+        if (user.email === registerEmail) {
+            foundUser = false
             break
+        } else {
+            foundUser = true
         }
     }
 
-    if (!foundUser) {
+    if (!foundUser) 
         return false
-    } else {
-        if (registerName !== '' && registerEmail !== '' && registerPassword.length >= 6 && registerPassword === registerConfirmedPassword) {
-            users.push({
-                name: `${registerName}`,
-                email: `${registerEmail}`,
-                password: `${registerPassword}`
-            })
-            registerPage.classList.add('off')
-            loginPage.classList.remove('off')
-        } else alert('Los datos introducidos no son correctos')   
-    }
+    
+    if (registerName !== '' && registerEmail !== '' && registerPassword.length >= 6 && registerPassword === registerConfirmedPassword) {
+        users.push({
+            name: `${registerName}`,
+            email: `${registerEmail}`,
+            password: `${registerPassword}`
+        })
+        registerPage.classList.add('off')
+        loginPage.classList.remove('off')
+        return true
+    } else alert('Los datos introducidos no son correctos')   
 }
+
 
 function authenticateUser(email, password) {
     var foundUser
@@ -42,3 +46,9 @@ function authenticateUser(email, password) {
         
     return true
 }
+
+function updateUserPassword(email, password, newPassword, newPasswordConfirm) {
+
+}
+
+//TODO implement me
--- staff/fran-salvatierra/web/app/scripts/main.js
+++ staff/fran-salvatierra/web/app/scripts/main.js
@@ -1,27 +1,22 @@
 var registerPage = document.querySelector('.register')
 var loginPage = document.querySelector('.login')
 var homePage = document.querySelector('.home')
-
-registerPage.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault()
-
-    // TODO how to get all data from inputs
-    registerPage.classList.add('off')
-    loginPage.classList.remove('off')
-})
+var authenticatedEmail
 
 loginPage.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault()
+    loginPage.classList.add('off')
+    homePage.classList.remove('off')
 
     // TODO how to get all data from inputs
     var email = loginPage.querySelector('input[name=email]').value
     var password = loginPage.querySelector('input[name=password]').value
     var result = authenticateUser(email, password)
-    var foundUser
 
     if (!result) {
-        alert('aaa')
+        alert('wrong email or password')
     } else {
+        authenticatedEmail = email
         loginPage.classList.add('off')
         homePage.classList.remove('off')
     }
@@ -29,17 +24,29 @@ loginPage.querySelector('form').addEventListener('submit', function(event) {
 
 registerPage.querySelector('button').addEventListener('click', function (event) {
     event.preventDefault()
+    
     var registerName = registerPage.querySelector('input[name=name]').value
     var registerEmail = registerPage.querySelector('input[name=email]').value
     var registerPassword = registerPage.querySelector('input[name=password]').value
-    var registerConfirmedPassword = registerPage.querySelector('input[name=confirm-password]').value   
+    
+    var result = registerUser(registerName, registerEmail, registerPassword)
+    
+    if (!result) {
+        alert('Used mail')
+    } else {
+        registerPage.classList.add('off')
+        loginPage.classList.remove('off')
+    }
 })
 
 loginPage.querySelector('.form__link').addEventListener('click', function (event) {
-    alert('test')
     event.preventDefault()
-    loginPage.classList.add('off')
-    registerPage.classList.remove('off')
+        loginPage.classList.add('off')
+        registerPage.classList.remove('off')
+})
+
+homePage.querySelector('a').addEventListener('click', function (event) {
+    event.preventDefault()
 })
 
 
