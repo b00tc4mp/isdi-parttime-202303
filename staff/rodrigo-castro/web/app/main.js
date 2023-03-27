bb5b5d5b215ad56d1528edb649a9d4421d110ecf
rodrigocastromartinez <rodrigocastro.mdp@gmail.com>
2023-03-28 01:10:45 +0200

    changes back (js not split)

--- staff/rodrigo-castro/web/app/main.js
+++ staff/rodrigo-castro/web/app/main.js
@@ -1,3 +1,88 @@
+//data
+
+var users = []
+
+users.push({
+    name: 'Wendy Darling',
+    email: 'wendy@darling.com',
+    password: '123123123'
+})
+
+users.push({
+    name: 'Peter Pan',
+    email: 'peter@pan.com',
+    password: '123123123'
+})
+
+users.push({
+    name: 'Pepito Grillo',
+    email: 'pepito@grillo.com',
+    password: '123123123'
+})
+
+var userLogged
+
+// logic
+
+const checkNewUser = (userEmail) => {
+    const userFound = users.find(user => user.email === userEmail)
+    if(userFound === undefined){
+        return false
+    } else {
+        return true
+    }
+}
+
+const registerNewUser = (users, userName, userEmail, userPassword) => {
+    users.push({
+        name: userName,
+        email: userEmail,
+        password: userPassword
+    })
+}
+
+const findUser = (users, userEmail) => {
+    return  users.find(user => user.email === userEmail)
+}
+
+const changePassword = (userLogged, users) => {
+    homePage.querySelector('.red-text').textContent = ''
+    var previousPassword = homePage.querySelector('.previous-password').value
+
+    if(previousPassword !== userLogged.password){
+        homePage.querySelector('.red-text').textContent = 'Your password is incorrect'
+        return
+    }
+
+    var newPassword = homePage.querySelector('.new-password').value
+
+    if(newPassword.length < 8){
+        homePage.querySelector('.red-text').textContent = 'Password must be at least 8 characters long'
+        return
+    }
+
+    if(newPassword === previousPassword){
+        homePage.querySelector('.red-text').textContent = 'New password must be different than previous'
+        return
+    }
+
+    var newPasswordRepeated = homePage.querySelector('.repeat-new-password').value
+
+    if(newPasswordRepeated !== newPassword){
+        homePage.querySelector('.red-text').textContent = `New passwords don't match`
+        return
+    }
+
+    userLogged.password = newPassword
+    users.forEach(user => {
+        if(user.email === userLogged.email){
+            user.password = userLogged.password
+        }
+    })
+    homePage.querySelector('.red-text').textContent = 'Password succesfully changed'
+    homePage.querySelector('.red-text').classList.add('green-text')
+}
+
 //presentation
 
 var registerPage = document.querySelector('.register-page')
