19dbd94f328b880f35ce47e1532d0b785499d7c7
Aida Martin <aidamg93@gmail.com>
2023-03-28 17:21:25 +0200

    implement password change, logout, throw errors

--- staff/aida-martin/web/app/main.js
+++ staff/aida-martin/web/app/main.js
@@ -1,110 +1,147 @@
 const registerPage = document.querySelector(".register");
 const loginPage = document.querySelector(".login");
 const homePage = document.querySelector(".home");
-let username;
-let authenticated;
+const profilePage = document.querySelector(".profile");
+const changePasswordPage = document.querySelector(".change-password");
 let authenticatedEmail;
 
-registerPage
-  .querySelector(".register-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+registerPage.querySelector(".register-form").onsubmit = function (event) {
+  event.preventDefault();
 
-    const name = registerPage.querySelector((input = "[name=name]")).value;
-    const email = registerPage.querySelector((input = "[name=email]")).value;
-    const password = registerPage.querySelector(
-      (input = "[name=password]")
-    ).value;
-    const repeatPassword = registerPage.querySelector(
-      (input = "[name=repeat-password]")
-    ).value;
-    const registerError = registerPage.querySelector(".register-error");
+  const name = registerPage.querySelector((input = "[name=name]")).value;
+  const email = registerPage.querySelector((input = "[name=email]")).value;
+  const password = registerPage.querySelector(
+    (input = "[name=password]")
+  ).value;
+  const repeatPassword = registerPage.querySelector(
+    (input = "[name=repeat-password]")
+  ).value;
+  const registerError = registerPage.querySelector(".register-error");
 
-    const result = registerUser(name, email, password, repeatPassword);
+  registerError.classList.remove("off");
 
-    registerError.classList.remove("off");
-
-    if (password.length < 8) {
-      registerError.innerHTML =
-        "Your password must have at least 8 characters 😥";
-      return;
-    }
-
-    if (repeatPassword !== password) {
-      registerError.innerHTML = "Your passwords have to match! 😥";
-      return;
-    }
-
-    if (!name || !email || !password || !repeatPassword) {
-      registerError.innerHTML = "You have to fill in all the fields! 😥";
-      return;
-    }
-
-    if (result === false) {
-      registerError.innerHTML = "You are already registered! Please login! 😅";
-      return;
-    }
+  try {
+    registerUser(name, email, password, repeatPassword);
 
     registerPage.classList.add("off");
-
     loginPage.classList.remove("off");
-  });
+  } catch (error) {
+    registerError.innerText = error.message;
+  }
+};
 
-loginPage
-  .querySelector(".login-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+loginPage.querySelector(".login-form").onsubmit = function (event) {
+  event.preventDefault();
 
-    const email = loginPage.querySelector((input = "[name=email]")).value;
-    const password = loginPage.querySelector((input = "[name=password]")).value;
-    const error = loginPage.querySelector(".error");
+  const email = loginPage.querySelector((input = "[name=email]")).value;
+  const password = loginPage.querySelector((input = "[name=password]")).value;
+  const loginError = loginPage.querySelector(".error");
 
-    let result = authenticateUser(email, password);
-
-    if (result === false) {
-      error.innerHTML = "Wrong email or password! 😥";
-      error.classList.remove("off");
-      return;
-    }
+  try {
+    authenticateUser(email, password);
 
     authenticatedEmail = email;
-    authenticated = true;
 
-    homePage.querySelector(".name").innerHTML = username;
+    let foundUser = retrieveUser(email);
 
-    error.classList.add("off");
+    homePage.querySelector(".name").innerText = foundUser.name;
+
+    loginPage.querySelector((input = "[name=email]")).value = "";
+    loginPage.querySelector((input = "[name=password]")).value = "";
+
+    loginError.classList.add("off");
 
     loginPage.classList.add("off");
 
     homePage.classList.remove("off");
-  });
+  } catch (error) {
+    loginError.classList.remove("off");
+    loginError.innerText = error.message;
+  }
+};
 
-registerPage.querySelector("a").addEventListener("click", function () {
+registerPage.querySelector("a").onclick = function () {
   loginPage.querySelector((input = "[name=email]")).value = "";
   loginPage.querySelector((input = "[name=password]")).value = "";
 
   registerPage.classList.add("off");
 
   loginPage.classList.remove("off");
-});
+};
 
-loginPage
-  .querySelector(".register-link")
-  .addEventListener("click", function () {
-    const error = loginPage.querySelector(".error");
+loginPage.querySelector(".register-link").onclick = function () {
+  const error = loginPage.querySelector(".error");
 
-    const registerError = registerPage.querySelector(".register-error");
+  const registerError = registerPage.querySelector(".register-error");
 
-    registerPage.querySelector((input = "[name=name]")).value = "";
-    registerPage.querySelector((input = "[name=email]")).value = "";
-    registerPage.querySelector((input = "[name=password]")).value = "";
-    registerPage.querySelector((input = "[name=repeat-password]")).value = "";
+  registerPage.querySelector((input = "[name=name]")).value = "";
+  registerPage.querySelector((input = "[name=email]")).value = "";
+  registerPage.querySelector((input = "[name=password]")).value = "";
+  registerPage.querySelector((input = "[name=repeat-password]")).value = "";
 
-    registerError.classList.add("off");
+  registerError.classList.add("off");
 
-    error.classList.add("off");
+  error.classList.add("off");
 
-    loginPage.classList.add("off");
+  loginPage.classList.add("off");
 
-    registerPage.classList.remove("off");
-  });
+  registerPage.classList.remove("off");
+};
+
+homePage.querySelector(".profile-link").onclick = function (event) {
+  event.preventDefault();
+
+  homePage.classList.add("off");
+
+  profilePage.classList.remove("off");
+};
+
+profilePage.querySelector(".profile-change-password").onclick = function () {
+  profilePage.classList.add("off");
+
+  changePasswordPage.classList.remove("off");
+};
+
+changePasswordPage.querySelector(".change-password-form").onsubmit = function (
+  event
+) {
+  event.preventDefault();
+
+  const email = authenticatedEmail;
+  const password = changePasswordPage.querySelector(
+    (input = "[name=old-password]")
+  ).value;
+  const newPassword = changePasswordPage.querySelector(
+    (input = "[name=new-password]")
+  ).value;
+  const newPasswordConfirm = changePasswordPage.querySelector(
+    (input = "[name=repeat-new-password]")
+  ).value;
+  const changePasswordError = changePasswordPage.querySelector(
+    ".change-password-error"
+  );
+
+  try {
+    changePassword(email, password, newPassword, newPasswordConfirm);
+
+    changePasswordPage.querySelector((input = "[name=old-password]")).value =
+      "";
+    changePasswordPage.querySelector((input = "[name=new-password]")).value =
+      "";
+    changePasswordPage.querySelector(
+      (input = "[name=repeat-new-password]")
+    ).value = "";
+
+    changePasswordPage.classList.add("off");
+    homePage.classList.remove("off");
+  } catch (error) {
+    changePasswordError.classList.remove("off");
+    changePasswordError.innerText = error.message;
+  }
+};
+
+homePage.querySelector(".profile-logout").onclick = function () {
+  homePage.classList.add("off");
+
+  loginPage.classList.remove("off");
+};
