36c145fa9251a7ba8034ee2412ac1edc3faf0f52
BirdOnTheBranch <planelles.luis@gmail.com>
2023-03-30 19:17:28 +0200

    improve return new user but founduser and move validations to validators file

--- staff/luis-planelles/web/app/scripts/logic.js
+++ staff/luis-planelles/web/app/scripts/logic.js
@@ -16,41 +16,32 @@ findUserByEmail = (email) => {
 };
 
 const registerUser = (name, email, password) => {
-  if (!name.length || !email.length || !password.length)
-    throw new Error("Any field is empty");
-  if (typeof name !== "string") throw new Error("name is not a string");
-  if (!name.length) throw new Error("name is empty");
-  if (typeof email !== "string") throw new Error("email is not an string");
-  if (!email.length) throw new Error("email is empty");
-  if (typeof password !== "string") throw new Error("password is not a string");
-  if (!password.length) throw new Error("password is empty");
+  const nameValid = validateName(name);
+  const emailValid = validateEmail(email);
+  const passwordValid = validatePassword(password);
 
   const foundUser = findUserByEmail(email);
 
   if (foundUser) throw new Error("user already exists");
 
-  users.push({
-    email: email,
+  const newUser = {
+    email: emailValid,
     info: {
-      name: name,
-      password: password,
+      name: nameValid,
+      password: passwordValid,
     },
-  });
+  };
 
-  return foundUser;
+  users.push(newUser);
+
+  return newUser;
 };
 
 const authenticateUser = (email, password) => {
-  if (typeof email !== "string") throw new Error("email is not an string");
-  if (!email.length) throw new Error("email is empty");
-  if (typeof password !== "string") throw new Error("password is not a string");
-  if (!password.length) throw new Error("password is empty");
-
   const foundUser = findUserByEmail(email);
 
-  if (!foundUser) throw new Error("User doesnt exist");
-  if (foundUser.info.password !== password)
-    throw new Error("Password incorrect");
+  if (!foundUser || foundUser.info.password !== password)
+    throw new Error("authentication failed");
 
   return foundUser;
 };
@@ -62,16 +53,14 @@ const updateUserPassword = (
   newPasswordConfirm
 ) => {
   const foundUser = findUserByEmail(email);
-  if (!foundUser) throw new Error("User doesn't exists");
+  if (!foundUser) throw new Error("user doesn't exists");
 
-  if (password !== foundUser.info.password)
-    throw new Error("User password doesn't match");
-
-  if (newPassword !== newPasswordConfirm)
-    throw new Error("New password doesn't match with password confirm");
-
-  if (newPassword === password) throw new Error("New password is equal to old");
-  foundUser.password = newPassword;
-
-  alert("Your password has been updated");
+  const newPasswordValid = updatePasswordValidation(
+    foundUser.info.password,
+    password,
+    newPassword,
+    newPasswordConfirm
+  );
+  foundUser.info.password = newPasswordValid;
+  alert("your password has been updated");
 };
