32f1de3428042d03d0d3c6332644ab6ae5853069
Flors Rueda <ruedaflors@gmail.com>
2023-03-31 20:11:53 +0200

    add isArray

---
+++ staff/flors-rueda/web/arrays/isArray.js
@@ -0,0 +1,6 @@
+const isArray = (element) => {
+    return element.constructor === Array;
+}
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/constructor
