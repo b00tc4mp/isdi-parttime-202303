068c55f98edda4eb1a430acb1579a01db1473db3
Flors Rueda <ruedaflors@gmail.com>
2023-03-31 18:13:18 +0200

    add pop

---
+++ staff/flors-rueda/web/arrays/array.prototype.pop.demo.js
@@ -0,0 +1,13 @@
+const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato']
+
+console.log(plants.pop())
+// Expected output: "tomato"
+
+console.log(plants)
+// Expected output: Array ["broccoli", "cauliflower", "cabbage", "kale"]
+
+console.log(plants.pop())
+// Expected output: "kale"
+
+console.log(plants)
+// Expected output: Array ["broccoli", "cauliflower", "cabbage"]
---
+++ staff/flors-rueda/web/arrays/pop.js
@@ -0,0 +1,5 @@
+const pop = (array) => {
+    const lastItem = array[array.length - 1];
+    array.length = array.length - 1;
+    return lastItem;
+}
