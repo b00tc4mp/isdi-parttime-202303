c2224d2f91b9923310912c640f95c5b71c46272c
Aida Martin <aidamg93@gmail.com>
2023-03-31 22:02:34 +0200

    Complete lastIndexOf

--- staff/aida-martin/web/arrays/indexOf/indexOf.js
+++ staff/aida-martin/web/arrays/indexOf/indexOf.js
@@ -1,5 +1,5 @@
 function indexOf(array, element, index = 0) {
-  if (Math.sign(index) === -1) index = array.length + index;
+  if (index < 0) index = array.length + index;
 
   for (let i = index; i < array.length; i++) {
     if (array[i] === element) {
