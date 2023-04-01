5130dd05fd1bca1de4da88268549fa1c44d61563
sarad11 <125995487+sarad11@users.noreply.github.com>
2023-04-01 23:30:32 +0200

    add isArray and fill files

---
+++ staff/sara-dominguez/web/arrays/fill.js
@@ -0,0 +1,26 @@
+function fill(array, elementToInclude, startIndexeElement, finalIndexElement){
+    if((!finalIndexElement || finalIndexElement === ' ') && (!startIndexeElement || startIndexeElement === ' ')){
+        
+        finalIndexElement = array.length;
+        startIndexeElement = 0; 
+        for (let i = startIndexeElement; i < finalIndexElement; i++){
+            array[i] = elementToInclude;
+        }
+        return array;
+    }
+    
+    if(!finalIndexElement || finalIndexElement === ' '){
+        finalIndexElement = array.length;
+        for (let i = startIndexeElement; i < finalIndexElement; i++){
+            array[i] = elementToInclude;
+        }
+        return array;
+    }
+
+    
+
+    for (let i = startIndexeElement; i < finalIndexElement; i++){
+        array[i] = elementToInclude;
+    }
+        return array;
+}
---
+++ staff/sara-dominguez/web/arrays/isArray.js
@@ -0,0 +1,7 @@
+function isArray(element) {
+    
+    if (element = []) {
+        return true;
+    }
+    return false;
+}
