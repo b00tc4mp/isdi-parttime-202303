d580fd8b95705dbfbdab1bc7d76e5718869fca80
sarad11 <125995487+sarad11@users.noreply.github.com>
2023-04-02 00:44:03 +0200

    add concat files

---
+++ staff/sara-dominguez/web/arrays/concat.demo.js
@@ -0,0 +1,6 @@
+const array1 = ['a', 'b', 'c', 6, 'w'];
+const array2 = ['d', 'e', 'f'];
+const array3 = []
+
+console.log(concat(array3, array1, array2));
+// Expected output: Array ["a", "b", "c", "d", "e", "f"]
