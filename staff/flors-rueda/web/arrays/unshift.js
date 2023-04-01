2c78c49ca29743eb7bf73d28c0bf69134615b713
Flors Rueda <ruedaflors@gmail.com>
2023-04-01 14:31:41 +0200

    add unshift

---
+++ staff/flors-rueda/web/arrays/unshift.js
@@ -0,0 +1,9 @@
+const unshift = (array, ...elements) => {
+    for(let i = array.length - 1; i >= 0; i--) {
+        array[i + elements.length] = array[i];
+    }
+    for(let i = 0; i < elements.length; i++) {
+        array[i] = elements[i];
+    }
+    return array.length;
+};
