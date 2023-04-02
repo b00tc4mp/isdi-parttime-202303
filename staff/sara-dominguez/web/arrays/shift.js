06a6a4f7a1365f7ad910f7105ede94f279d791ed
sarad11 <125995487+sarad11@users.noreply.github.com>
2023-04-02 20:47:41 +0200

    add shift modificated files

--- staff/sara-dominguez/web/arrays/shift.js
+++ staff/sara-dominguez/web/arrays/shift.js
@@ -23,19 +23,24 @@ function shift(array){
     newArrayIndex = 0
 
     for(let i = 1; i < array.length; i++){ 
-        if(i === 0){
-            array[0] = firstElement
-            return firstElement
-        }
+
+      if(i === 0){
+        array[0] = firstElement
+        
+    }
         if(i !== 0){
             newArray[newArrayIndex]= array[i]
             newArrayIndex += 1
         }
     }
     
+    array.length = newArray.length
+
+    for(let i = 0; i < newArray.length; i++){ 
+        array[i] = newArray[i]
+    } 
+      
     
-    console.log(newArray)
-    console.log(firstElement)
-    return newArray
+    return array
 }
-//TODO exportar a global el valor nuevo del array y el first element.
+//TODO como retorno firstElement?
