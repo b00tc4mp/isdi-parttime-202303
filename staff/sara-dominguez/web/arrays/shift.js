595d62e0f4fd665743c7b26ad1c13b7f4a1933f8
sarad11 <125995487+sarad11@users.noreply.github.com>
2023-04-02 18:02:58 +0200

    add shift modificated files

--- staff/sara-dominguez/web/arrays/shift.js
+++ staff/sara-dominguez/web/arrays/shift.js
@@ -1,3 +1,4 @@
+/*
 function shift(array){
     const firstElement = array[0];
     const newArray = []
@@ -7,6 +8,32 @@ function shift(array){
         newArray[newArrayIndex]= array[i]
         newArrayIndex += 1
     }
+    
+    
+    console.log(newArray)
+    console.log(firstElement)
+    return newArray
+}
+//TODO exportar a global el valor nuevo del array y el first element.
+*/
+
+function shift(array){
+    const firstElement = array[0];
+    const newArray = []
+    newArrayIndex = 0
+
+    for(let i = 1; i < array.length; i++){ 
+        if(i = 0){
+            array[0] = firstElement
+            return firstElement
+        }
+        if(i !== 0){
+        newArray[newArrayIndex]= array[i]
+        newArrayIndex += 1
+        }
+    }
+    
+    
     console.log(newArray)
     console.log(firstElement)
     return newArray
