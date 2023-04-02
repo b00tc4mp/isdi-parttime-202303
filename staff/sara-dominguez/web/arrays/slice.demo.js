58202e2cbcf4af7a8ff738812299239d2e0cb4dd
sarad11 <125995487+sarad11@users.noreply.github.com>
2023-04-02 14:09:31 +0200

    add slice files

---
+++ staff/sara-dominguez/web/arrays/slice.demo.js
@@ -0,0 +1,5 @@
+var nombres = ['Rita', 'Pedro', 'Miguel', 'Ana', 'Vanesa'];
+//var masculinos = nombres.slice(1, 3);
+
+console.log(slice(nombres, 1, 3))
+// masculinos contiene ['Pedro','Miguel']
---
+++ staff/sara-dominguez/web/arrays/slice.js
@@ -0,0 +1,15 @@
+function slice(array, start, end){
+    const newArray = []
+    newArrayIndex = 0
+    for(let i = start; i < end; i++){
+        newArray[newArrayIndex]= array[i]
+        newArrayIndex += 1
+    }
+    return newArray
+}
+
+    
+       
+        
+   
+    
