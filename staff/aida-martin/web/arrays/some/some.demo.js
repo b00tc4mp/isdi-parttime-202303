4f98eb121d568d8668e8c31529c63b21da26cc83
Aida Martin <aidamg93@gmail.com>
2023-04-04 11:13:29 +0200

    Complete some

---
+++ staff/aida-martin/web/arrays/some/some.demo.js
@@ -0,0 +1,26 @@
+import some from "./some.js";
+
+const array = [1, 2, 3, 4, 5];
+
+// Checks whether an element is even
+const even = (element) => element % 2 === 0;
+
+console.log(some(array, even));
+// Expected output: true
+
+const even1 = (element) => element === 0;
+
+console.log(some(array, even1));
+//EXTRA Expected output: false
+
+const even2 = (element) => element === 1;
+
+console.log(some(array, even2));
+//EXTRA Expected output: true
+
+const array1 = [];
+
+const even3 = (element) => element === "";
+
+console.log(some(array1, even3));
+//EXTRA Expected output: false
---
+++ staff/aida-martin/web/arrays/some/some.js
@@ -0,0 +1,14 @@
+function some(array, callback) {
+  if (array.length === 0) {
+    return false;
+  }
+
+  for (const element of array) {
+    if (callback(element)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export default some;
