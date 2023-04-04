2101138abae5adc81b3f3d20d9a9cdac7ce3e96b
BirdOnTheBranch <planelles.luis@gmail.com>
2023-04-04 18:10:43 +0200

    renturn false when item in array is empty

--- staff/luis-planelles/arrays/scripts/some.js
+++ staff/luis-planelles/arrays/scripts/some.js
@@ -1,6 +1,9 @@
 //
 const some = (array, callback) => {
   for (item of array) {
+    if (item === undefined) {
+      continue;
+    }
     const result = callback(item);
     if (result) {
       return true;
