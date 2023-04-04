3f8bf8976c81c5696bdfb16fd897bbca0c601fee
iCarlosPro <cjps91@gmail.com>
2023-04-04 20:55:27 +0200

    Minor fix

--- staff/carlos-perez/arrays/some/some.js
+++ staff/carlos-perez/arrays/some/some.js
@@ -1,12 +1,12 @@
-function some(array, callback){
-if(array.length===0){
-    return false;
-}
-
-for(const element of array){
-    if(callback(element)){
-        return true;
+function some(array, callback) {
+    if (array.length === 0) {
+        return false;
     }
-}
-return false;
+
+    for (const element of array) {
+        if (callback(element)) {
+            return true;
+        }
+    }
+    return false;
 }
