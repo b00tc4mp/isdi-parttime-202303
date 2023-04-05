c9366607a208fe856540a58ed261c928de15b6b4
BirdOnTheBranch <planelles.luis@gmail.com>
2023-04-05 15:54:58 +0200

    updated version passing every cases

--- staff/luis-planelles/arrays/demos/splice-demo.js
+++ staff/luis-planelles/arrays/demos/splice-demo.js
@@ -70,3 +70,26 @@ console.log(myFish5);
 // myFish is ["parrot", "anemone", "sturgeon"]
 console.log(removed5);
 // removed is ["blue", "trumpet"]
+
+console.warn('myFish6');
+const myFish6 = ['angel', 'clown', 'mandarin', 'sturgeon'];
+const removed6 = splice(myFish6, -2, 1);
+
+console.log(myFish6);
+// myFish is ["angel", "clown", "sturgeon"]
+console.log(removed6);
+// removed is ["mandarin"]
+
+console.warn('myFish7');
+const myFish7 = ['angel', 'clown', 'mandarin', 'sturgeon'];
+const removed7 = splice(myFish7, 2);
+
+console.log(myFish7);
+// myFish is ["angel", "clown"]
+console.log(removed7);
+// removed is ["mandarin", "sturgeon"]
+
+console.warn('arr');
+const arr = [1, , 3, 4, , 6];
+console.log(splice(arr, 1, 2)); // [empty, 3]
+console.log(arr); // [1, 4, empty, 6]
--- staff/luis-planelles/arrays/scripts/splice.js
+++ staff/luis-planelles/arrays/scripts/splice.js
@@ -1,6 +1,7 @@
 const splice = (array, start, itemsAdd = false, ...items) => {
   let spliced = [];
-
+  start = start < 0 ? array.length + start : start;
+  itemsAdd = !itemsAdd ? start : itemsAdd;
   for (i = 0; i < itemsAdd; i++) {
     spliced[i] = array[start + i];
   }
@@ -25,25 +26,3 @@ const splice = (array, start, itemsAdd = false, ...items) => {
   }
   return [];
 };
-
-console.warn('myFish6');
-const myFish6 = ['angel', 'clown', 'mandarin', 'sturgeon'];
-const removed6 = splice(myFish6, -2, 1);
-
-console.log(myFish6);
-// myFish is ["angel", "clown", "sturgeon"]
-console.log(removed6);
-// removed is ["mandarin"]
-
-// const myFish7 = ['angel', 'clown', 'mandarin', 'sturgeon'];
-// const removed7 = splice(myFish7, 2);
-
-// console.log(myFish7);
-// // myFish is ["angel", "clown"]
-// console.log(removed7);
-// // removed is ["mandarin", "sturgeon"]
-
-// console.warn('arr');
-// const arr = [1, , 3, 4, , 6];
-// console.log(splice(arr, 1, 2)); // [empty, 3]
-// console.log(arr); // [1, 4, empty, 6]
