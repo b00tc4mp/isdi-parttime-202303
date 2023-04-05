1cdeb6527d888a0e8dec350287b6ab46dea70d89
manuelbarzi <manuelbarzi@gmail.com>
2023-04-05 20:42:02 +0200

    add splice iterations

--- staff/manuel-barzi/web/arrays/splice.mjs
+++ staff/manuel-barzi/web/arrays/splice.mjs
@@ -1,11 +1,23 @@
 export default function splice(array, start, deleteCount, item1) {
-    for (let i = array.length - 1; i >= start; i--) {
-        const element = array[i]
+    const extracted = []
 
-        array[i + 1] = element
+    for (let i = start; i < start + deleteCount; i++) {
+        const elem = array[i]
+
+        extracted[extracted.length] = elem
+    }
+
+    if (deleteCount > 0) {
+        for (let i = start + deleteCount; i < array.length; i++) {
+            const elem = array[i]
+
+            array[i - (deleteCount - 1)] = elem
+        }
+
+        array.length -= (deleteCount - 1)
     }
 
     array[start] = item1
 
-    return []
+    return extracted
 }
