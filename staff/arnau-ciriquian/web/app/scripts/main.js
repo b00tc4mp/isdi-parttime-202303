1b3fd3e8c657b30e18e1a208cfa7e2fb4d931667
Anna <arnau.m.ciriquian@gmail.com>
2023-04-05 21:40:16 +0200

    add TODOs 20230405

--- staff/arnau-ciriquian/web/app/scripts/main.js
+++ staff/arnau-ciriquian/web/app/scripts/main.js
@@ -114,23 +114,29 @@ function closeProfilePages() {
     homePagePassword.classList.add('off')
 }
 
-/*  TODO Web/App:   
+/*  TODO Web/App - objectiu de la app: xarxa social:
+    - ig de stickers?
+    - estil twitter?
+    - likes, post, comments?:   
+    - rollo ig?
+            - afegir id als users 20230405 2115 i modificar tots els scripts de validar a traves del id en comptes del mail
             - Foto perfil 20230328 2100
             - canviar alerts per missatges en pantalla?o un toast?
             - al fer logout posar un avatar random per evitar deixar lavatar de lusuari previ
             - tancar ulls de les contrasenyes amb el canvi de pagina. Un if? si type text fer un toggle de fa-eye-slash i canvi de tipus
             - modularitzar: aplicar imports i exports a la web/app
+            - passar vars a const i let
             - passar for basics a for of?
-            - objectiu de la app: xarxa social:
-                - rollo ig?
-                - ig de stickers?
-                - estil twitter?
-                - likes, post, comments?
+            - mirar cause pels errors??
+            - poder crear un post (dun en un), text o imatge amb la data del post:
+                comncem amb boto + al  footer
+                seguim amb window modular?
+            - 
 
     TODO Arrays:
             - metodes amb callback:
                 -
-            - seguir amb splice
+            - seguir amb splice, rersoldre afegir X i  als months
             
     DONE    - 
             -
