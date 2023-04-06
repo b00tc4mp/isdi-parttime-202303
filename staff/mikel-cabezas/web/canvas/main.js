669668447d58fb963667b3c100e2f9601efbd3e6
Mikel Cabezas <mikelcabezas@gmail.com>
2023-04-06 18:40:19 +0200

    add canvas TODO

---
+++ staff/mikel-cabezas/web/canvas/main.js
@@ -0,0 +1,46 @@
+const html = document.documentElement
+const canvas = document.querySelector('canvas.my-canvas')
+const context = canvas.getContext('2d')
+
+const frameCount = 100
+
+const currentFrame = index => (
+    `img/img-${index.toString().padStart(3, '0')}.png`
+)
+
+const preloadImages = () => {
+    for (let i = 0; i < frameCount; i++) {
+        const img = new Image()
+        img.src = currentFrame(i)
+    }
+}
+
+const img = new Image()
+img.src = currentFrame(1)
+canvas.width=1500;
+canvas.height=1500;
+
+img.onload = function() {
+    context.drawImage(img, 0, 0)
+}
+
+
+const updateImage = index => {
+    img.src = currentFrame(index)
+    context.drawImage(img, 0, 0)
+}
+
+
+window.onscroll = function() {
+    const scrollTop = html.scrollTop
+    const maxScrollTop = html.scrollHeight - window.innerHeight
+    const scrollFraction = scrollTop / maxScrollTop
+    const frameIndex = Math.min(
+        frameCount -1,
+        Math.floor(scrollFraction * frameCount)
+    )
+    requestAnimationFrame(() => updateImage(frameIndex + 1))
+}
+
+
+preloadImages()
