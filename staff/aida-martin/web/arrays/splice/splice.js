28f1488d7a84eb9f2df59b363ba4f2930d238a4c
Aida Martin <aidamg93@gmail.com>
2023-04-06 14:24:37 +0200

    Improve splice (...items)

--- staff/aida-martin/web/arrays/splice/splice.js
+++ staff/aida-martin/web/arrays/splice/splice.js
@@ -1,18 +1,26 @@
-export default function splice(array, start, deleteCount, item) {
-  const deleteItem = [array[start]];
+export default function splice(array, start, deleteCount, ...items) {
+  const extracted = [];
 
-  for (let i = array.length - 1; i >= start; i--) {
+  for (let i = start; i < start + deleteCount; i++) {
     const element = array[i];
 
-    if (deleteCount !== 0) {
-      array[start] = item;
-      return deleteItem;
-    }
-
-    array[i + 1] = element;
+    extracted[extracted.length] = element;
   }
 
-  array[start] = item;
+  if (deleteCount > 0) {
+    for (let i = start + deleteCount; i < array.length; i++) {
+      const element = array[i];
+      // CAMBIADO EL - 1 POR EL - ITEMS.LENGTH
+      array[i - (deleteCount - items.length)] = element;
+    }
 
-  return [];
+    //CAMBIADO EL - 1 POR - ITEMS.LENGTH
+    array.length -= deleteCount - items.length;
+  }
+
+  //AÑADIDO ESTE FOR PARA RECORRER LOS ITEMS Y AÑADIRLOS EN LA POSICIÓN QUE TOQUE
+  for (let i = 0; i < items.length; i++) {
+    array[start + i] = items[i];
+  }
+  return extracted;
 }
