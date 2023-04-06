7461628fbceb7314c29cbfcf2c5c807ea09408ad
BirdOnTheBranch <planelles.luis@gmail.com>
2023-04-06 15:20:26 +0200

    use strict example

---
+++ staff/luis-planelles/examples/use-strict.js
@@ -0,0 +1,37 @@
+//
+'use strict';
+
+nombre = 'pepe';
+persona = { nombre: 'Carla', edad: 25 };
+
+delete persona;
+
+function saludar(persona, persona) {
+  console.log('hola ' + persona);
+}
+
+hello();
+
+function hello() {
+  string = 'hello world';
+}
+
+const myArray = [1, 2, 3, 4, 5];
+
+for (i = 0; i < myArray.lenght; i++) {
+  array[i] = 'algo';
+}
+
+let a = 10;
+
+let eval = 44;
+let arguments;
+
+const implements = 'algo';
+const interface = 'algo';
+const package = 'algo';
+const private = 'algo';
+const protected = 'algo';
+const public = 'algo';
+const static = 'algo';
+const yield = 'algo';
