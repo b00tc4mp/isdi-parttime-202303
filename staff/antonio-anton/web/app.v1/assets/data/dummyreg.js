3f42a01af062545b22af33bca01c9eb12eccc4e2
Antonio Anton <aanton@ideauto.com>
2023-04-07 11:25:49 +0200

    Several changes some manu adoptions

---
+++ staff/antonio-anton/web/app.v1/assets/data/dummyreg.js
@@ -0,0 +1,38 @@
+users.push({
+    guid:'',
+    name: 'Tom Hanks',
+    email: 'THanks@actors.com',
+    password: 'VDUwODU1MDcz', /*T50855073*/
+    emailnotificatios: true
+})
+
+users.push({
+    guid:'',
+    name: 'Robert de Niro',
+    email: 'RdNiro@actors.com',
+    password: 'VDUwODU1MDcz',
+    emailnotificatios: true
+})
+
+users.push({
+    guid:'',
+    name: 'Nicolas Cage',
+    email: 'NCage@actors.com',
+    password: 'VDUwODU1MDcz',
+    emailnotificatios: true
+})
+
+users.push({
+    guid:'',
+    name: 'Sylvester Stallone',
+    email: 'SStallone@actors.com',
+    password: 'VDUwODU1MDcz',
+    emailnotificatios: true
+})
+users.push({
+    guid:'',
+    name: 'Antonio Antón',
+    email: 'aanton@actors.com',
+    password: 'YW50b25pbw==', /*antonio*/ 
+    emailnotificatios: true
+})
