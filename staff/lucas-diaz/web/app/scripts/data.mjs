89d46efc12c8e55cf1ed94e2ff42c642942c71c9
lucasDDiazCuenca <lucasdamian30@gmail.com>
2023-04-08 18:58:18 +0100

    update data file

--- staff/lucas-diaz/web/app/scripts/data.mjs
+++ staff/lucas-diaz/web/app/scripts/data.mjs
@@ -34,20 +34,21 @@ posts.push({
     id: "post-1",
     author: "user-1",
     image: "https://png.pngtree.com/element_pic/00/16/07/08577f369b99bed.jpg",
-    text: "Love interiorism"
+    text: "Love interiorism",
+    date: new Date()
 })
-
 posts.push({
     id: "post-2",
     author: "user-3",
     image: "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/35137/house-home-clipart-xl.png",
-    text: "Love this style"
+    text: "Love this style",
+    date: new Date()
 })
-
 posts.push({
     id: "post-3",
     author: "user-4",
     image: "https://static.vecteezy.com/system/resources/previews/019/861/654/non_2x/3d-modern-house-or-home-isometric-modern-building-and-architecture-free-png.png",
-    text: "How about this style??"
+    text: "How about this style??",
+    date: new Date()
 })
 
