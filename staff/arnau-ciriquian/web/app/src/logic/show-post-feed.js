7daea361b8bd8ef7d28eef5637a8d04aafeb66ce
Arnau Ciriquian <arnau.m.ciriquian@gmail.com>
2023-04-10 19:05:14 +0200

    main feed logic

--- staff/arnau-ciriquian/web/app/src/logic/show-post-feed.js
+++ staff/arnau-ciriquian/web/app/src/logic/show-post-feed.js
@@ -1,23 +1,34 @@
 import { posts, users } from "../data.js"
 
 export function showPostFeed() {
-    const postImage = posts[0].image
-    const postText = posts[0].text
-    let postAuthor
-    for (const element of users) {
-        if (posts[0].author === element.id) {
-            postAuthor = element.name
-        }
-    }
-    const postDate = posts[0].postDate
+    let latestPostImage
+    let latestPostText
+    let latestPostAuthor
+    let latestPostDate
+    let latestPost
+    let postFeed = ''
 
-    const postFeed = `<div class="inputs__box--feed">
-    <img class="home__post--image" src="${postImage}">
-    <p class="text">${postText}</p>
-    <div class="home__post--info">
-    <p class="text">${postAuthor}</p>
-    <p class="text">${postDate}</p>
-    </div></div>`
+    for (let i = posts.length - 1; i >= 0; i-- ) {
+        const post = posts[i]
+        latestPostImage = post.image
+        latestPostText = post.text
+        for (const user of users) {
+            if (post.author === user.id) {
+                latestPostAuthor = user.name
+            }
+        }
+        latestPostDate = post.date
+
+        latestPost = `<div class="inputs__box--feed">
+            <img class="home__post--image" src="${latestPostImage}">
+            <p class="text">${latestPostText}</p>
+            <div class="home__post--info">
+            <p class="text">${latestPostAuthor}</p>
+            <p class="text">${latestPostDate}</p>
+            </div></div>`
+        
+        postFeed += latestPost
+    }
     
     return postFeed
 }
