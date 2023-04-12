9ed95d3beb26568c19c65b199818d91f13bfd02c
sarad11 <125995487+sarad11@users.noreply.github.com>
2023-04-13 01:03:37 +0200

    add vite App version, add retrieve-posts,renderUser, render-posts

---
+++ staff/sara-dominguez/web/app/app-sara-dominguez/src/logic/retrieve-posts.js
@@ -0,0 +1,12 @@
+import { validateId } from "./helpers/validators.js"
+import { users, posts } from "../data.js"
+
+export default function retrievePost(userId) {
+    validateId(userId)
+
+    const foundUser = users.some(user => user.id === userId)
+
+    if(!foundUser) throw new Error(`User with ${userId} not found`)
+
+    return posts.toReversed() 
+}
---
+++ staff/sara-dominguez/web/app/app-sara-dominguez/src/pages/login-page.js
@@ -0,0 +1,61 @@
+console.log('load login-page')
+
+import { authenticateUser } from '../logic/authenticate-user.js'
+import { retrieveUser } from "../logic/retrieve-user.js"
+import { context, hide, show } from "../ui.js"
+import { registerPage } from "./register-page.js"
+import { homePage, myProfileLink, avatarImage, DEFAULT_AVATAR_URL, renderPosts } from "./home-page.js"
+
+
+
+
+export const loginPage= document.querySelector('.login')
+const loginForm= loginPage.querySelector('form')
+
+
+
+//capture datas Login form
+loginForm.onsubmit = function (event) {
+    event.preventDefault()
+
+    const email = event.target.email.value
+    const password = event.target.password.value
+    
+    try{
+        context.userId = authenticateUser(email,password)
+        
+        const user = retrieveUser(context.userId)
+
+        myProfileLink.innerText = `${user.name}`
+
+        
+        avatarImage.src = user.avatar? user.avatar: DEFAULT_AVATAR_URL
+        
+
+
+        loginForm.reset()
+
+        hide (loginPage)
+        
+        renderPosts()
+        
+        //TODO ORDER PAINT POSTS
+        show(homePage)
+
+        
+        }   catch (error) {
+            alert(error.message)
+        }
+    
+}
+
+
+//configurate anchor <a> Login
+
+loginPage.querySelector('a').onclick = function(event){
+    event.preventDefault()
+
+    loginForm.reset()
+    hide(loginPage)
+    show(registerPage)
+}
