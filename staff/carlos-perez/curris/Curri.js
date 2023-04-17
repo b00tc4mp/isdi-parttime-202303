be00240df1b5726d9d2fd5e16f271f2db0f629f9
iCarlosPro <cjps91@gmail.com>
2023-04-17 21:22:52 +0200

    The Curris are here

---
+++ staff/carlos-perez/curris/Curri.js
@@ -0,0 +1,26 @@
+function Curri() {
+    this.length = 0
+}
+
+Curri.prototype.forEach = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+        const element = this[i]
+        
+        callback(element)
+    }
+}
+
+Curri.prototype.map = function map(callback) {
+    const mapped = new Curri
+
+    for (let i = 0; i < this.length; i++) {
+        const element = this[i]
+        
+        mapped[mapped.length] = callback(element)
+        mapped.length++
+    }
+
+    return mapped
+}
+
+export default Curri
