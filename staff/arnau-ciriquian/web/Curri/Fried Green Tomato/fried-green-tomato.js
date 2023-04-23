7d977dde9f82f0afb3f14fb9b071c74e8d8eeb24
Anna Renau <arenaurosell@gmail.com>
2023-04-23 03:07:17 +0200

    add version 1.0.0 of html-js Fried Green Tomato tester, rendering on html, no browser console

--- staff/arnau-ciriquian/web/Curri/Fried Green Tomato/fried-green-tomato.js
+++ staff/arnau-ciriquian/web/Curri/Fried Green Tomato/fried-green-tomato.js
@@ -1,33 +1,76 @@
+const version = '1.0.0'
+const testVersion = document.createElement('p')
+testVersion.innerText = `v: ${version}`
+const testTittle = document.querySelector('.test-tittle')
+testTittle.append(testVersion)
+const testFeed = document.querySelector('.test-feed')
 export const objectsList = []
 export const methodsList = []
+export let itsList = []
 let lastItem
-console.log(`Fried%cGreen%cTomato %cv0.1`, 'color: green', 'color:tomato', 'font-size: 8px')
+let objectContainer
+let methodContainer
+
 
 export function describe (description, callback) {
-    const result = description
-
+    
     if (!lastItem) {
-        objectsList.push(result)
+        objectsList.push(description)
         lastItem = 'describe'
-    } else if (lastItem === 'describe') {
-        methodsList.push(result)
+        
+        objectContainer = document.createElement('article')
+        objectContainer.classList.add('test-feed__object')
+        
+        const objectName = document.createElement('h1')
+        objectName.innerText = description
+        
+        objectContainer.append(objectName)
+        testFeed.append(objectContainer)
+    } else if (lastItem === 'describe' || lastItem === 'it') {
+        methodContainer = document.createElement('article')
+        methodsList.push(description)
         lastItem = 'describe'
-    }
 
-    console.log(`%c${description}`, 'color: green')
+        methodContainer.classList.add('test-feed__method')
+
+        const methodName = document.createElement('h2')
+        methodName.innerText = description
+
+        methodContainer.append(methodName)
+        objectContainer.append(methodContainer)
+    }
 
     callback()
 }
 
 export function it (description, callback) {
-    try {
-        callback()
-        
-        console.log(`%c· ${description}`, 'color: green')
-    } catch(error) {
-        console.log(`%c· ${description}\n${error.stack}`, 'color: tomato')
+    if (lastItem === 'describe') {
+        itsList = []
     }
 
+    const itContainer = document.createElement('article')
+    itContainer.classList.add('test-feed__it')
+                
+    const itName = document.createElement('h3')
+    itName.innerText = description
+
+    itContainer.append(itName)
+
+    try {
+        lastItem = 'it'
+
+        callback()
+    } catch(error) {
+        const itError = document.createElement('h5')
+        itError.innerText = error.stack
+
+        itContainer.append(itError)
+
+        itError.classList.add('error')
+        itName.classList.add('error')
+    }
+
+    methodContainer.append(itContainer)
 }
 
 export function expect (value) {
@@ -41,3 +84,58 @@ export function expect (value) {
         }
     }
 }
+
+/*export function renderTests() {
+    testFeed.innerHTML = ''
+
+    objectsList.forEach(object => {
+        const objectContainer = document.createElement('article')
+        objectContainer.classList.add('test-feed__object')
+
+        const objectName = document.createElement('h1')
+        objectName.innerText = object
+
+        objectContainer.append(objectName)
+
+        methodsList.forEach(method => {
+            const methodContainer = document.createElement('article')
+            methodContainer.classList.add('test-feed__method')
+
+            const methodName = document.createElement('h2')
+            methodName.innerText = method
+
+            methodContainer.append(methodName)
+            
+            itsList.forEach(it => {
+
+                const itContainer = document.createElement('article')
+                itContainer.classList.add('test-feed__it')
+                
+                const itName = document.createElement('h3')
+                itName.innerText = it[0]
+
+                itContainer.append(itName)
+
+                if (it[1] === 'red' && it[2]) {
+                    const itError = document.createElement('h5')
+                    itError.innerText = it[2]
+
+                    itContainer.append(itError)
+
+                    itError.classList.add('error')
+                    itName.classList.add('error')
+                }
+    
+
+                //expect
+                console.log(itsList)
+                methodContainer.append(itContainer)
+            })
+
+            console.log(methodsList)
+            objectContainer.append(methodContainer)
+        })
+
+        testFeed.appendChild(objectContainer)
+    })
+}*/
