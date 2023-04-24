9554f8968be1edf1a25ea215d0a7c57f2d54ee34
Flors Rueda <ruedaflors@gmail.com>
2023-04-24 11:44:18 +0200

    add svg paths dictionary

--- staff/flors-rueda/web/app/src/components/theme-toggle.js
+++ staff/flors-rueda/web/app/src/components/theme-toggle.js
@@ -1,7 +1,14 @@
+import { svg } from '../../assets/svg-paths.js'
+
 export default function initThemeToggle(context) {
+    const svgMoon = document.querySelector('.theme-toggle__label--moon').querySelector('path')
+    const svgSun = document.querySelector('.theme-toggle__label--sun').querySelector('path')
+    
+    svgMoon.setAttribute('d', svg.moon)
+    svgSun.setAttribute('d', svg.sun)
+        
     let storedTheme = context.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
     if (storedTheme) document.documentElement.setAttribute('data-theme', storedTheme)
-
 
     const themeCheckbox = document.querySelector('.theme-toggle__checkbox');
 
