481d5989a8bb1d8ee11fd1fedb96b9ec157395e8
iCarlosPro <cjps91@gmail.com>
2023-04-24 17:16:10 +0200

    More Curris to the mix

--- staff/carlos-perez/curris/Curri.js
+++ staff/carlos-perez/curris/Curri.js
@@ -92,6 +92,113 @@ Curri.prototype.fill = function fill(fillItem, startIndex = 0, endIndex = this.l
     return this;
 };
 
+Curri.prototype.indexOf = function indexOf(matchItem, startPosition = 0) {
+    for (let i = startPosition; i < this.length; i++) {
+      if (this[i] === matchItem) {
+        return i;
+      }
+    }
+    return -1;
+  };
 
+  Curri.isCurri = function isCurri(argument) {
+    if (argument && typeof argument === 'object' && argument.length && !argument.byteLength) {
+      return true;
+    }
+    return false;
+  };
+
+  Curri.prototype.lastIndexOf = function lastIndexOf(item, index = 0) {
+    let lastIndex = -1;
+  
+    for (let i = index; i < this.length; i++) {
+      if (this[i] === item) {
+        lastIndex = i;
+      }
+    }
+    return lastIndex;
+  };
+  
+  Curri.prototype.shift = function shift() {
+    const item = this[0];
+  
+    for (let i = 0; i < this.length - 1; i++) {
+      this[i] = this[i + 1];
+    }
+    if (this.length) {
+      delete this[this.length - 1];
+      this.length--;
+    }
+    return item;
+  };
+
+  Curri.prototype.toReversed = function toReversed() {
+    const reversed = new Curri();
+    for (let i = 0; i < this.length; i++) {
+      reversed[i] = this[this.length - 1 - i];
+      reversed.length++;
+    }
+    return reversed;
+  };
+
+  Curri.prototype.unshift = function unshift(...args) {
+    const shifted = new Curri();
+    let index = 0;
+  
+    for (let i = 0; i < args.length + this.length; i++) {
+      if (i < args.length) {
+        shifted[i] = args[i];
+      } else {
+        shifted[i] = this[i - args.length];
+      }
+      shifted.length++;
+    }
+    for (let i = 0; i < shifted.length; i++) {
+      this[index] = shifted[i];
+      index++;
+    }
+    this.length = index;
+  
+    return this.length;
+  };
+
+  Curri.prototype.concat = function concat(item) {
+    let result = new Curri();
+  
+    for (let i = 0; i < this.length; i++) {
+      result[i] = this[i];
+      result.length++;
+    }
+  
+    for (let i = 0; i < item.length; i++) {
+      result[result.length] = item[i];
+      result.length++;
+    }
+  
+    return result;
+  };
+
+  Curri.prototype.join = function join(slitItem = ',') {
+    let result = '';
+  
+    for (let i = 0; i < this.length; i++) {
+      if (i > 0) {
+        result += slitItem;
+      }
+      result += this[i];
+    }
+    return result;
+  };
+
+  Curri.prototype.reverse = function reverse() {
+    for (let i = 0; i < this.length / 2; i++) {
+      const item = this[i];
+      this[i] = this[this.length - 1 - i];
+      this[this.length - 1 - i] = item;
+    }
+    return this;
+  };
+  
+  
 
 window.Curri = Curri
--- staff/carlos-perez/curris/Curri.spec.js
+++ staff/carlos-perez/curris/Curri.spec.js
@@ -126,4 +126,118 @@ describe('fill', () => { it('should fill Curri with an item at given indexes', (
 
 })})
 
+describe('indexOf', () => { it('should return the index of an item if it is present in Curri', () =>{
+    const c = new Curri(10, 20, 30);
+
+    const result = c.indexOf(10);
+
+    expect(c).toBeInstanceOf(Curri);
+    expect(result).toBe(0);
+
+    const result2 = c.indexOf(40);
+
+    expect(result2).toBe(-1);
+
+})})
+
+describe('isCurri', () => { it('should tell if an object is Curri or not', () =>{
+    const c = new Curri(10, 20, 30);
+
+    const result = Curri.isCurri(c);
+
+    expect(c).toBeInstanceOf(Curri);
+    expect(result).toBe(true);
+
+    const d = 10;
+
+    const result2 = Curri.isCurri(d);
+    expect(result2).toBe(false);
+
+})})
+
+describe('lastIndexOf', () => { it('should tell the last index of an object if it is Curri or -1 if it is not in Curri', () =>{
+    const c = new Curri(10, 20, 30, 10);
+
+    const result = c.lastIndexOf(10);
+
+    expect(c).toBeInstanceOf(Curri);
+    expect(result).toBe(3);
+
+    const result2 = c.lastIndexOf(1);
+    expect(result2).toBe(-1);
+
+})})
+
+describe('shift', () => { it('should return the first element of Curri and delete it from Curri', () =>{
+    const c = new Curri(10, 20, 30);
+
+    const result = c.shift();
+
+    expect(c).toBeInstanceOf(Curri);
+    expect(result).toBe(10);
+    expect(c.length).toBe(2);
+
+})})
+
+describe('toReversed', () => { it('should return Curri reversed, but not modifying the original one', () =>{
+    const c = new Curri(10, 20, 30);
+
+    const result = c.toReversed();
+
+    expect(c).toBeInstanceOf(Curri);
+    expect(result[0]).toBe(30);
+    expect(result[1]).toBe(20);
+    expect(result[2]).toBe(10);
+    expect(c[0]).toBe(10);
+    expect(c[1]).toBe(20);
+    expect(c[2]).toBe(30);
+
+})})
+
+describe('unshift', () => { it('should add elements at the beginning of Curri and return the new length', () =>{
+    const c = new Curri(10, 20, 30);
+
+    const result = c.unshift(40);
+
+    expect(c).toBeInstanceOf(Curri);
+    expect(result).toBe(4);
+    expect(c[0]).toBe(40);
+
+})})
+
+describe('concat', () => { it('should concatenate elements at the end of Curri and return the result of it. Curri is unmodified', () =>{
+    const c = new Curri(10, 20, 30);
+
+    const result = c.concat(new Curri(40,50));
+
+    expect(c).toBeInstanceOf(Curri);
+    expect(result).toBeInstanceOf(Curri);
+    expect(result.length).toBe(5);
+    expect(result[3]).toBe(40);
+    expect(result[4]).toBe(50);
+
+})})
+
+describe('join', () => { it('should join all elements of Curri and return the result of it as a string', () =>{
+    const c = new Curri(10, 20, 30);
+
+    const result = c.join();
+
+    expect(c).toBeInstanceOf(Curri);
+    expect(result).toBeInstanceOf(String);
+    expect(result).toBe('10,20,30');
+
+})})
+
+describe('reverse', () => { it('should reverse Curri, modifying it', () =>{
+    const c = new Curri(10, 20, 30);
+
+    const result = c.reverse();
+
+    expect(c).toBeInstanceOf(Curri);
+    expect(result).toBeInstanceOf(Curri);
+    expect(c[0]).toBe(result[0]);
+
+})})
+
 })
