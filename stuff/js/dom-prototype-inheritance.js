2a79162cfc4975c3587b39490901b0ced3c26089
manuelbarzi <manuelbarzi@gmail.com>
2023-04-26 21:49:00 +0200

    add more binding demos; add custom elements, custom components

---
+++ stuff/js/dom-prototype-inheritance.js
@@ -0,0 +1,22 @@
+class EventTarget {
+}
+
+class Node extends EventTarget {
+}
+
+class Element extends Node {
+}
+
+class HTMLElement extends Element {
+}
+
+class HTMLAnchorElement extends HTMLElement {
+}
+
+class HTMLULElement extends HTMLElement {
+}
+
+class HTMLParagraphElement extends HTMLElement {
+}
+
+...
---
+++ stuff/js/use-strict.1.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function warn(message) {
+    this.alert(`WARN ${message}`)
+}
+
+warn('suelo mojado')
