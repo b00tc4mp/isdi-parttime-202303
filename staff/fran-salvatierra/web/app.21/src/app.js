9bef9fa71865c77098ef284d61c4c9c9fd5832a1
Ridie-Kings <fran6salvatierra@gmail.com>
2023-04-28 18:46:55 +0200

    Add initial version for class app conversion

---
+++ staff/fran-salvatierra/web/app.21/src/app.js
@@ -0,0 +1,12 @@
+import { Component } from "./library/composito.js";
+import Login from "./pages/login.js"
+
+export default class App extends Component {
+    constructor() {
+        super('<div></div>');
+
+        const login = new Login 
+
+        this.add(login)
+    }
+}
