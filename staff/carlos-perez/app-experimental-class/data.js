65461cb2ae7b4d00dd80924b142e6b883554dec3
iCarlosPro <cjps91@gmail.com>
2023-05-02 18:28:18 +0200

    First version of fixed classes. 3 tables

--- staff/carlos-perez/app-experimental-class/data.js
+++ staff/carlos-perez/app-experimental-class/data.js
@@ -1,73 +1,97 @@
 //Data
 
-export class Like{
+export class Like {
     #likeId;
     #userId;
     #userLikeId;
-    //Habrá que añadir el id del post, dados los cambios hechos hoy
+    #postID;
+    //Quizás nos interese añadir la fecha en el futuro
 
-    constructor(likeId, userId, userLikeId){
-        this.#likeId=likeId;
-        this.#userId=userId;
-        this.#userLikeId=userLikeId;
+    constructor(likeId, userId, userLikeId, postId) { //Id del like, id del usuario del post, id del usuario que ha dado a like, id del post
+        this.#likeId = likeId;
+        this.#userId = userId;
+        this.#userLikeId = userLikeId;
+        this.#postID = postId;
     }
 
-    get getLikeId(){
+    get getLikeId() {
         return this.#likeId;
     }
-    get getUserId(){
+    get getUserId() {
         return this.#userId;
     }
-    get getUserLikeId(){
+    get getUserLikeId() {
         return this.#userLikeId;
+    }
+
+    get getPostId() {
+        return this.#postID;
     }
 
     //No tiene sentido ningún setter, porque no vamos a modificar los valores de un Like
     //Un Like se creará o destruirá, no se modificará
 }
 
-export class Likes{
+export class Likes {
     likes;
 
-    constructor(likes){
-        if(likes.length===0){
-            likes=[];
+    constructor(likes) {
+        if (likes.length === 0) {
+            likes = [];
         }
-        else{
-            this.likes=likes;
-        }
-    }
-
-    searchLike(likeId){
-        for(let i=0; i<this.likes.length; i++){
-            if(likes[i].getLikeId===likeId){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    //Revisar esto
-
-    addLike(likeID,userID, userLikeId){
-        if(this.searchLike(userLikeId)===-1){
-        this.likes.push(new Like(userLikeId+likeID,userID, userLikeId));
-        }
-        else{ //Si el usuario le da a like una segunda vez, el like ya existe, así que elimina el like, porque ya no le gusta
-            this.removeLike(userLikeId);
+        else {
+            this.likes = likes;
         }
     }
 
-    removeLike(userLikeId){
-        const likePosition=this.searchLike(userLikeId);
-        if(likePosition!=-1){
-            this.likes.splice(likePosition,1);
+    searchLike(likeId) {
+        const isLikeId = (element) => element.getLikeId === likeId;
+        return this.likes.findIndex(isLikeId);
+    }
+
+    searchLikeByLikeUserAndPost(userLikeId, postId) {
+        const condition = (element) => (element.getUserLikeId === userLikeId) && (element.getPostId === postId);
+        return this.likes.findIndex(condition);
+    }
+
+    retrieveUserLikes(userLikeId) {
+        return this.likes.filter(element => element.getUserLikeId === userLikeId);
+    }
+
+    retrieveUserLikeAnalytics(userId) {
+        return this.likes.filter(element => element.getUserId === userId);
+    }
+
+    retrievePostLikeAnalytics(postId) {
+        return this.likes.filter(element => element.getPostId === postId);
+    }
+
+    removeLike(likeId) {
+        const likePosition = this.searchLike(likeId);
+        if (likePosition != -1) {
+            this.likes.splice(likePosition, 1);
         }
-        else{
+        else {
             throw new Error('Like not found');
         }
     }
-    
+
+    removeLikeSafely(userLikeId, postId) {
+        this.likes.splice(this.searchLikeByLikeUserAndPost(userLikeId, postId), 1);
+    }
+
+    addLike(likeID, userID, userLikeId, postId) {
+        const position = this.searchLikeByLikeUserAndPost(userLikeId, postId);
+        if (position === -1) {
+            this.likes.push(new Like(likeID, userID, userLikeId, postId));
+        }
+        else { //Si el usuario le da a like una segunda vez, el like ya existe, así que elimina el like, porque ya no le gusta
+            this.removeLikeSafely(userLikeId, postId);
+        }
+    }
+
+
+
 }
 
 export class Post {
@@ -76,89 +100,90 @@ export class Post {
     #text;
     #imageURL;
     #date;
-    likes=[];
-    likeCounter=0;
+    likes = [];
+    likeCounter = 0;
 
-    constructor(id, userId, text, imageURL) {
+    constructor(userId, text, imageURL) {
         if (id != undefined && userId != undefined) {
-            this.#id = id;
+            this.#id = userId + Date.now();
             this.#userId = userId;
             this.#text = text;
             this.#imageURL = imageURL;
-            this.#date=new Date();
+            this.#date = new Date();
         }
         else {
             throw new Error("ID and userID are required");
         }
     }
 
-    get getID(){
+    get getID() {
         return this.#id;
     }
 
-    get getUserID(){
+    get getUserID() {
         return this.#userId;
     }
 
-    get getText(){
+    get getText() {
         return this.#text;
     }
 
-    get getImgURL(){
+    get getImgURL() {
         return this.#imageURL;
     }
 
-    get getDate(){
+    get getDate() {
         return this.#date;
     }
 
     //set setId no tiene sentido, porque el ID del post es PK, no se modifica y se asigna al crearse;
     //set setUserID tampoco tiene sentido, el cambio de autor de un post no está contemplado
 
-    set setText(text){
-        this.#text=text;
+    set setText(text) {
+        this.#text = text;
     }
 
-    set setImgURL(ImgURL){
-        this.#imageURL=ImgURL;
+    set setImgURL(ImgURL) {
+        this.#imageURL = ImgURL;
     }
 
-    set setDate(date){
-        this.#date=date;
+    set setDate(date) {
+        this.#date = date;
     }
 
-    searchLike(userLikeId){
-        for(let i=0; i<this.likes.length; i++){
-            if(likes[i].getUserLikeId===userLikeId){
-                return i;
-            }
+    searchLike(userLikeId) {
+        const condition = (element) => element.getUserLikeId === userLikeId;
+        return this.likes.findIndex(condition);
+    }
+
+    generateLikeId(postId, userLikeId) {
+        return postId + userLikeId;
+    }
+
+    addLike(userID, userLikeId, postId) {
+        if (this.searchLike(userLikeId) === -1) {
+            const currentLikeId = generateLikeId(postId, userLikeId);
+            this.likes.push(new Like(currentLikeId, userID, userLikeId, postId));
+            this.likeCounter++;
+            return currentLikeId;
         }
-        return -1;
-    }
-
-    addLike(userID, userLikeId){
-        if(this.searchLike(userLikeId)===-1){
-        this.likeCounter++;
-        this.likes.push(new Like(this.likeCounter,userID, userLikeId));
-        return this.likeCounter;
-        }
-        else{ //Si el usuario le da a like una segunda vez, el like ya existe, así que elimina el like, porque ya no le gusta
+        else { //Si el usuario le da a like una segunda vez, el like ya existe, así que elimina el like, porque ya no le gusta
             this.removeLike(userLikeId);
         }
     }
 
-    removeLike(userLikeId){
-        const likePosition=this.searchLike(userLikeId);
-        if(likePosition!=-1){
-            this.likes.splice(likePosition,1);
+    removeLike(userLikeId) {
+        const likePosition = this.searchLike(userLikeId);
+        if (likePosition != -1) {
+            this.likes.splice(likePosition, 1);
         }
-        else{
+        else {
             throw new Error('Like not found');
         }
     }
 }
 
-export class Posts{
+export class Posts {
     posts;
 
     constructor(posts) {
@@ -166,52 +191,42 @@ export class Posts{
             this.posts = [];
         }
         else {
-            this.posts=posts;
+            this.posts = posts;
         }
     }
 
-    get getPosts(){
+    get getPosts() {
         return this.posts;
     }
 
-    addPost(id, userId, text, imageURL){
-        this.posts.push(new Post(id, userId, text, imageURL));
+    addPost(userId, text, imageURL) {
+        this.posts.push(new Post(userId, text, imageURL));
     }
 
-    searchPostPosition(id){
-        for(let i=0; i<this.posts.length; i++){
-            if(this.posts[i].getID===id){
-                return i;
-            }
-        }
-        return -1;
+    searchPostPosition(id) {
+        const condition = (element) => element.getID === id;
+        return this.posts.findIndex(condition);
     }
 
-    deletePost(id, userId){
-        const postPosition=this.searchPostPosition(id);
-        if(postPosition!=-1 && this.posts[postPosition].getUserID===userId){
-            this.posts.splice(postPosition,1);
+    deletePost(id, userId) {
+        const postPosition = this.searchPostPosition(id);
+        if (postPosition != -1 && this.posts[postPosition].getUserID === userId && this.posts[postPosition].getID === id) {
+            this.posts.splice(postPosition, 1);
         }
-        else{
+        else {
             throw new Error("El post no se encuentra");
         }
     }
 
-    retrievePostbyID(id){
-        const postPosition=this.searchPostPosition(id);
-        if(postPosition!=-1){
+    retrievePostbyID(id) {
+        const postPosition = this.searchPostPosition(id);
+        if (postPosition != -1) {
             return this.posts[postPosition];
         }
     }
 
-    retrieveUserPosts(userID){
-        const result=[];
-        for(let i=0; i<this.posts.length; i++){
-            if(this.posts[i].getUserID===userID){
-                result.push(this.posts[i]);
-            }
-        }
-        return result;
+    retrieveUserPosts(userID) {
+        return this.posts.filter(element => element.getUserID === userID);
     }
 }
 
@@ -220,7 +235,7 @@ export class User {
     #name;
     #email;
     #password;
-    likes=[];
+    likes = [];
 
     constructor(id, name, email, password) {
 
@@ -265,30 +280,26 @@ export class User {
         this.#password = password;
     }
 
-    searchLike(postID){
-        for(let i=0; i<this.likes.length; i++){
-            if(likes[i]===postID){
-                return i;
-            }
-        }
-        return -1;
+    searchLike(postID) {
+        const condition = (element) => element = postID;
+        this.likes.findIndex(condition);
     }
 
-    like(postID){
-        if(this.searchLike(postID)===-1){
-        this.likes.add(postID);
+    like(postID) {
+        if (this.searchLike(postID) === -1) {
+            this.likes.add(postID);
         }
-        else{
+        else {
             this.unlike(postID); //Si el usuario le da a like una segunda vez, el like ya existe, así que elimina el like, porque ya no le gusta
         }
     }
 
-    unlike(postID){
+    unlike(postID) {
         const likePosition = this.searchLike(postID);
-        if(likePosition!=-1){
-            this.likes.splice(likePosition,1);
+        if (likePosition != -1) {
+            this.likes.splice(likePosition, 1);
         }
-        else{
+        else {
             throw new Error("You do not like this");
         }
     }
@@ -325,3 +336,7 @@ export let appUsers = new Users();
 appUsers.addUser('Wendy Darling', 'wendy@darling.com', '123123123');
 appUsers.addUser('Peter Pan', 'peter@pan.com', '123123123');
 appUsers.addUser('Pepito Grillo', 'pepito@grillo.com', '123123123');
+
+export let appPosts = new Posts();
+
+export let appLikes = new Likes();
