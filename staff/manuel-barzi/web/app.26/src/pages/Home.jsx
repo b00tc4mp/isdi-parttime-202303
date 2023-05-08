2306601e6542e6b525997df438377f9fea3d924d
manuelbarzi <manuelbarzi@gmail.com>
2023-05-08 20:59:33 +0200

    implement edit post functionality

--- staff/manuel-barzi/web/app.26/src/pages/Home.jsx
+++ staff/manuel-barzi/web/app.26/src/pages/Home.jsx
@@ -2,20 +2,17 @@ import Posts from '../components/Posts'
 import { Component } from 'react'
 import AddPostModal from '../components/AddPostModal'
 import Profile from '../components/Profile'
-import EditPostModal from '../components/EditPostModal'
 
 export default class Home extends Component {
     constructor(props) {
         super(props)
 
-        this.state = { view: 'posts', modal: null, postId: null }
+        this.state = { view: 'posts', modal: null }
     }
 
-    handleOpenAddPostModal = () => this.setState({ modal: 'add-post' })
+    handleOpenAddPost = () => this.setState({ modal: 'add-post' })
 
-    handleOpenEditPostModal = postId => this.setState({ modal: 'edit-post', postId })
-
-    handleCloseModal = () => this.setState({ modal: null })
+    handleCloseAddPost = () => this.setState({ modal: null })
 
     handleGoToProfile = event => {
         event.preventDefault()
@@ -23,7 +20,7 @@ export default class Home extends Component {
         this.setState({ view: 'profile' })
     }
 
-    handleGoToPosts = () => this.setState({ view: 'posts' })
+    handleGoToPosts = () => this.setState({ view: 'posts' }) 
 
     render() {
         console.log('Home -> render')
@@ -41,23 +38,17 @@ export default class Home extends Component {
             </header>
 
             <main>
-                {this.state.view === 'posts' && <Posts onEditPost={this.handleOpenEditPostModal} />}
+                {this.state.view === 'posts' && <Posts />}
                 {this.state.view === 'profile' && <Profile />}
 
                 {this.state.modal === 'add-post' && <AddPostModal
-                    onCancel={this.handleCloseModal}
-                    onPostCreated={this.handleCloseModal}
-                />}
-
-                {this.state.modal === 'edit-post' && <EditPostModal
-                    onCancel={this.handleCloseModal}
-                    onPostUpdated={this.handleCloseModal}
-                    postId={this.state.postId}
+                    onCancel={this.handleCloseAddPost}
+                    onPostCreated={this.handleCloseAddPost}
                 />}
             </main>
 
             <footer className="home-footer">
-                <button className="add-post-button" onClick={this.handleOpenAddPostModal}>+</button>
+                <button className="add-post-button" onClick={this.handleOpenAddPost}>+</button>
             </footer>
         </div>
     }
---
+++ staff/manuel-barzi/web/app.27/src/logic/registerUser.js
@@ -0,0 +1,34 @@
+import { validateName, validateEmail, validatePassword } from './helpers/validators'
+import { findUserByEmail } from './helpers/data-managers'
+import { users, saveUsers } from '../data'
+
+export default function registerUser(name, email, password) {
+    validateName(name)
+    validateEmail(email)
+    validatePassword(password)
+
+    const foundUser = findUserByEmail(email)
+
+    if (foundUser)
+        throw new Error('user already exists')
+
+    let id = 'user-1'
+
+    const _users = users()
+
+    const lastUser = _users[_users.length - 1]
+
+    if (lastUser)
+        id = 'user-' + (parseInt(lastUser.id.slice(5)) + 1)
+
+    const user = {
+        id,
+        name,
+        email,
+        password
+    }
+
+    _users.push(user)
+
+    saveUsers(_users)
+}
