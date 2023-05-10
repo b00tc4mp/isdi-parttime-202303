75cfc29e796025ea48983aba67464ddf002e792c
manuelbarzi <manuelbarzi@gmail.com>
2023-05-10 21:39:14 +0200

    add react countdown demo

---
+++ stuff/react/countdown-demo/NASACounter.jsx
@@ -0,0 +1,48 @@
+class NASACounter extends React.Component {
+    constructor(props) {
+        console.log('NASACounter -> constructor')
+
+        super(props)
+
+        this.state = {
+            count: 10
+        }
+    }
+
+    setState(newState) {
+        console.log('NASACounter -> setState')
+
+        super.setState(newState)
+    }
+
+    componentDidMount() {
+        console.log('NASACounter -> componentDidMount')
+
+        this.timer = setInterval(() => {
+            this.state.count > 0 ?
+                this.setState({ count: this.state.count - 1 })
+                :
+                clearInterval(this.timer)
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        console.log('NASACounter -> componentWilUnmount')
+
+        // clearInterval(this.timer)
+    }
+
+    render() {
+        console.log('NASACounter -> render')
+
+        return (
+            <div style={this.props}>
+                <h1>
+                    NASA Countdown: <br /> {this.state.count || "🪐"} <br />
+                    {"⭐".repeat(this.state.count) || "🚀"}
+                </h1>
+                {this.state.count === 0 && <h2>LIFT OFF!!!</h2>}
+            </div>
+        )
+    }
+}
