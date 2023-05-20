9cc4b6cc76c3970feadf12ab5a9c2d6f43f03aeb
Arnau Ciriquian <arnau.m.ciriquian@gmail.com>
2023-05-20 14:49:15 +0200

    backup pre callbackhell

---
+++ staff/arnau-ciriquian/web/app.10/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { context } from './ui'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import Home from './pages/Home.jsx'
+
+export default function App() {
+  const [view, setView] = useState(context.userId ? 'home' : 'login')
+
+  const handleGoToRegister = () => setView('register')
+
+  const handleGoToLogin = () => setView('login')
+
+  const handleGoToHome = () => setView('home')
+
+  console.log('App -> render')
+
+  switch (view) {
+    case 'login':
+      return <Login onRegisterClick={handleGoToRegister} onUserLoggedIn={handleGoToHome} />
+      
+    case 'register':
+      return <Register onLoginClick={handleGoToLogin} onUserRegistered={handleGoToLogin}/>
+
+    case 'home':
+      return <Home onLoggedOut={handleGoToLogin}/>
+  }
+}
---
+++ staff/arnau-ciriquian/web/app.10/src/logic/updatePost.js
@@ -0,0 +1,25 @@
+import { validateId, validateText, validateUrl } from "./helpers/validators";
+import { savePost } from "../data";
+import { findUserById, findPostById } from "./helpers/data-managers";
+
+export function updatePost(userId, postId, image, text) {
+    
+    validateId(userId, 'user id')
+    validateId(postId, 'post id')
+    validateUrl(image, 'image url')
+    validateText(text, 'post text')
+
+    const user = findUserById(userId)
+    if (!user) throw new Error(`user not found`)
+
+    const post = findPostById(postId)
+    if(!post) throw new Error('post not found')
+
+    if (post.author !== userId) throw new Error(`post with id ${postId} does not belong to user with id ${userId}`)
+
+    post.image = image
+    post.text = text
+    post.date = (new Date).toLocaleDateString('en-UK')
+    
+    savePost(post)
+}
