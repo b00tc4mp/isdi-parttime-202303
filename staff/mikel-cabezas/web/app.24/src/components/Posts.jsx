4df09330bd46bde97903d7d0cb88de7b3c4562d0
Mikel Cabezas <mikelcabezas@gmail.com>
2023-05-24 00:17:43 +0200

    add spinner library, and implement in posts before load posts, and configure a message if no posts created. fix likes and saves posts in async

--- staff/mikel-cabezas/web/app.24/src/components/Posts.jsx
+++ staff/mikel-cabezas/web/app.24/src/components/Posts.jsx
@@ -3,8 +3,6 @@ import Post from "./Post"
 import retrievePosts from "../logic/posts/retrievePosts"
 import { context } from "../ui"
 import './Posts.css'
-import { RotatingLines } from  'react-loader-spinner'
-
 
 export default function Posts({onToggleLike, onToggleSave, onEditPost, onAddPostClick, lastPostsUpdate, onToggleLikePostClick, onToggleSavePostClick, user}) {
     const userId = context.userId
@@ -88,13 +86,7 @@ if (posts) {
             <button className="button--create-post" onClick={handleAddPost}>Create post <i className="material-symbols-outlined">add</i></button>
         </div>
         <div className="posts">
-            {posts.length === 0 && 
-            <div>
-                    <h2>There is no posts yet.
-                    <br/>But you can create a new one!</h2>
-
-            </div>}
-            {posts.length > 0 && posts.map(post => {
+            {posts.map(post => {
                 return <Post 
                 key={post.id} 
                 post={post} 
@@ -112,18 +104,8 @@ if (posts) {
         <h2 className="welcome-user"></h2>
         <button className="button--create-post" onClick={handleAddPost}>Create post <i className="material-symbols-outlined">add</i></button>
     </div>
-    <div className="loading">
-        <RotatingLines
-            strokeColor="grey"
-            strokeWidth="5"
-            animationDuration="0.75"
-            width="40"
-            visible={true}
-        />
-        <h2>Loading...</h2>
-    <div>
-
-    </div>
+    <div className="posts">
+    <h2>Loading...</h2>
     </div>
 </>
 }
---
+++ staff/mikel-cabezas/web/app.24/src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import { context } from "../ui.js"
+import authenticateUser from '../logic/users/authenticateUsers.js'
+// import { pushUserDataToHeader } from "../components/helpers/push-user-to-header.js"
+
+export default function Login({onRegisterClick, onUserLoggedIn}) {
+    console.log('Home -> login')
+
+    function handleRegisterClick(event) {
+      event.preventDefault()
+      onRegisterClick()
+    }
+
+    function handleLogin(event) {
+        event.preventDefault()
+        const email = event.target.email.value
+        const password = event.target.password.value
+        let userId
+
+        try {
+            authenticateUser(email, password, (error, userId) => {
+                if (error) {
+                    alert(error.message)
+                    
+                    return
+                }
+                
+                context.userId = userId
+                
+                onUserLoggedIn()
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+  
+    return <section className="section login" id="login">
+        <div className="container">
+            <h1>Login</h1>
+            <form className="login-form" onSubmit={handleLogin}>
+                <div className="message"></div>
+                <label>Email</label>
+                <input type="email" name="email" placeholder="Your email" />
+                <label>Password</label>
+                <div className="password">
+                    <input type="password" name="password" placeholder="Your password" />
+                    <i className="uil uil-eye"></i>
+                </div>
+                <button className="submit" type="submit">Login</button>
+                <div className="register-link">
+                    <p>Not registered? <a href="" onClick={handleRegisterClick}>Create account</a></p>
+                </div>
+            </form>
+        </div>
+    </section>
+  }
