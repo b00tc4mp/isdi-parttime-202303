b87c781dfe2b2b447495faa735010d597cc586fc
Anna Renau <arenaurosell@gmail.com>
2023-05-26 23:40:19 +0200

    game pre classes backup

---
+++ staff/arnau-ciriquian/fullstack/game.1/index.js
@@ -0,0 +1,70 @@
+const readline = require('readline');
+
+// Create a readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Set up the event listener for key presses
+rl.input.on('keypress', (key, data) => {
+   if (data.name === 's' && menuSelection < 2) {
+        menuSelection ++
+    } else if (data.name === 'w' && menuSelection > 0) {
+        menuSelection --
+    } else if (data.name === 'k' && menu) {
+        if (menuSelection === 1) {
+            menuSelection = 0
+            menu = false
+            options = true
+        } else if (menuSelection === 2) process.exit()
+    } else if (data.name === 'k' && options) {
+        menu = true
+        options = false
+    }
+
+    refreshScreen()
+});
+
+// Set the terminal in raw mode to capture individual key presses
+rl.input.setRawMode(true);
+rl.input.resume();
+
+const posMain  = 16, posMenu = 2, posSelector = 18
+let menuSelection = 0
+let menu = true
+let options = false
+
+const renderMenu = () => {
+    console.log('')
+    console.log('')
+    console.log(' '.repeat(posMain), 'Final  ⚓️ FantaSea')
+    console.log(' '.repeat(posSelector), (menuSelection === 0 ? '🔱' : ' '.repeat(posMenu)), 'New Game')
+    console.log(' '.repeat(posSelector), (menuSelection === 1 ? '🔱' : ' '.repeat(posMenu)), 'Options')
+    console.log(' '.repeat(posSelector), (menuSelection === 2 ? '🔱' : ' '.repeat(posMenu)), 'Exit Game')
+    console.log('')
+    console.log('')
+    console.log('(Use "s" and "d" to navigate the menu and "k" to accept)')
+    console.log(menuSelection)
+}
+
+const renderOptions = () => {
+    console.log('')
+    console.log('')
+    console.log(' '.repeat(posMain), 'Final  ⚓️ FantaSea')
+    console.log('Final FantSea is a turn based fighting game')
+    console.log('Choose your character and battle against your foes')
+    console.log('Each turn choose your action to defeat them')
+    console.log('Last creature standing wins!')
+    console.log(' '.repeat(posSelector), (menuSelection === 0 ? '🔱' : ' '.repeat(posMenu)), 'Go Back')
+
+}
+
+refreshScreen = () => {
+    console.clear()
+
+    if (menu) renderMenu()
+    if (options) renderOptions()
+}
+
+refreshScreen()
