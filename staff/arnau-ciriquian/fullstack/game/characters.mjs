0420bac3aad2d05ce4f19335fa7267d446a40fcb
Arnau Ciriquian <arnau.m.ciriquian@gmail.com>
2023-05-27 23:35:49 +0200

    pressing random keys bug fixed

---
+++ staff/arnau-ciriquian/fullstack/game/characters.mjs
@@ -0,0 +1,35 @@
+//CHARACTER MANAGEMENT
+class Character {
+    constructor(icon, name) {
+        this.icon = icon
+        this.name = name
+    }
+}
+
+export class Warrior extends Character {
+    super() {
+        this.dmg = 100
+        this.heal = 50
+        this.hp = 1000
+    }
+
+    attack() {
+        this.dmg += Math.round(20 * (1 - Math.random()))
+        return this.dmg
+    }
+
+    specialAttack() {
+        this.dmg += Math.round(20 * (1 - Math.random()))
+        return this.dmg * 2
+    }
+
+    healHp() {
+        this.heal += Math.round(50 * (1 - Math.random()))
+        return this.heal
+    }
+
+    reset(stat) {
+        if (stat === 'damage') this.dmg = 100
+        if (stat === 'hp') this.hp = 1000
+    }
+}
---
+++ staff/arnau-ciriquian/fullstack/game/index.mjs
@@ -0,0 +1,180 @@
+// IMPORTS
+import readline from 'readline'
+import { writeFile } from 'fs/promises'
+import { Warrior } from './characters.mjs';
+import { renderMenu, renderOptions, renderGame, renderActionsMenu, renderAction, renderLostGame, renderWonGame } from './renders.mjs';
+
+// TEXT SAVE - LOGGER
+//const fs = require('fs')
+
+let result = ''
+
+
+
+// KEY MANAGEMENT
+//const readline = require('readline');
+
+// Create a readline interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Set up the event listener for key presses
+rl.input.on('keypress', (key, data) => {
+    if (data.name === 's' && menuSelection < 2) {
+        if (menu || (game && playerTurn && !playerAction)) menuSelection++
+    } else if (data.name === 'w' && menuSelection > 0) {
+        if (menu || (game && playerTurn && !playerAction)) menuSelection--
+    } else if (data.name === 'k' && menu) {
+        if (menuSelection === 0) {
+            menuSelection = 0
+            menu = false
+            game = true
+            playerTurn = true
+
+        } else if (menuSelection === 1) {
+            menuSelection = 0
+            menu = false
+            options = true
+
+        } else if (menuSelection === 2) process.exit()
+
+    } else if (data.name === 'k' && game) {
+        result = 'k pressed in game'
+        if (menuSelection === 0 && playerTurn && !playerAction) {
+            playerAction = 'attack'
+            menuSelection = 0
+            //
+            resolveAction(megalodon, kraken, playerAction)
+            megalodon.reset('damage')
+            //
+
+        } else if (menuSelection === 1 && playerTurn && !playerAction) {
+            playerAction = 'special attack'
+            menuSelection = 0
+
+        } else if (menuSelection === 2 && playerTurn && !playerAction) {
+            playerAction = 'heal'
+            menuSelection = 0
+
+        } else if (menuSelection === 0 && playerTurn && playerAction) {
+            playerAction = ''
+            playerTurn = false
+            enemyTurn = true
+            //
+            determineEnemyAction()
+            resolveAction(kraken, megalodon, enemyAction)
+            kraken.reset('damage')
+            //
+        } else if (menuSelection === 0 && enemyTurn && enemyAction) {
+            enemyAction = ''
+            playerTurn = true
+            enemyTurn = false
+        }
+
+    } else if (data.name === 'k' && options) {
+        menu = true
+        options = false
+
+    } else if (data.name === 'k' && endGame) {
+        menu = true
+        game = false
+    }
+
+    refreshScreen()
+    console.log('Key Pressed:', key)
+});
+
+
+const kraken = new Warrior('🦑', 'Kraken')
+kraken.super()
+const megalodon = new Warrior('🦈', 'Megalodon')
+megalodon.super()
+
+//GAME RENDERING
+export let menuSelection = 0
+let menu = true
+let options = false
+let game = false
+let endGame = false
+let playerTurn = false
+let enemyTurn = false
+let playerAction = ''
+let enemyAction = ''
+let damage = 0
+let amountHealed = 0
+
+const determineEnemyAction = () => {
+    const actionIndex = Math.floor(Math.random() * 3)
+
+    if (actionIndex === 0) enemyAction = 'attack'
+    if (actionIndex === 1) enemyAction = 'special attack'
+    if (actionIndex === 2) enemyAction = 'heal'
+}
+
+const resolveAction = (attacker, defender, action) => {
+    if (action === 'attack') {
+        damage = attacker.attack()
+        defender.hp = defender.hp - damage
+    } else if (action === 'special attack') {
+        damage = attacker.specialAttack()
+        defender.hp = defender.hp - damage
+    } else if (action === 'heal') {
+        amountHealed = attacker.healHp()
+        attacker.hp = attacker.hp + amountHealed
+    }
+
+    if (attacker.hp <= 0 || defender.hp <= 0) {
+        game = false
+        endGame = true
+        playerAction = ''
+        enemyAction = ''
+    }
+}
+
+const refreshScreen = () => {
+    console.clear()
+
+    if (menu) renderMenu()
+    else if (options) renderOptions()
+    else if (game) {
+        renderGame(megalodon, kraken)
+        if (playerTurn && !playerAction && megalodon.hp > 0) renderActionsMenu(megalodon)
+        else if (playerTurn && playerAction && megalodon.hp > 0 && kraken.hp > 0){
+            // resolveAction(megalodon, kraken, playerAction)
+            // megalodon.reset('damage')
+            renderAction(megalodon, kraken, playerAction, damage, amountHealed)
+        } else if (enemyTurn && kraken.hp > 0 && megalodon.hp > 0) {
+            // determineEnemyAction()
+            // resolveAction(kraken, megalodon, enemyAction)
+            // kraken.reset('damage')
+            renderAction(kraken, megalodon, enemyAction, damage, amountHealed)
+        }
+        
+        if (endGame && megalodon.hp <= 0 && kraken.hp > 0) {
+            console.clear()
+            renderLostGame()
+            megalodon.reset('hp')
+            kraken.reset('hp')
+        }
+        else if (endGame && kraken.hp <= 0 && megalodon.hp > 0) {
+            console.clear()
+            renderWonGame()
+            megalodon.reset('hp')
+            kraken.reset('hp')
+        }
+    }
+
+    console.log(menuSelection)
+
+    writeFile('/Users/Arnau/workspace/isdi-parttime-202303/staff/arnau-ciriquian/fullstack/game/logger.txt', result, err => {
+        if (err) {
+            console.error(err)
+        }
+    })
+
+    result = ''
+}
+
+refreshScreen()
