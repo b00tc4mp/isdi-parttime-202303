fd0bce7bc84169975c9ed0d20671e01bacfaf3d0
Iván Canuto <canutosorianoivan@gmail.com>
2023-05-28 23:47:40 +0200

    create console game and write code in another document with it

--- staff/ivan-canuto/fullstack/game/index.js
+++ staff/ivan-canuto/fullstack/game/index.js
@@ -1,1 +1,82 @@
-console.log('GAME v0')
+// import fs from 'fs'
+const fs = require('fs')
+
+const fileRoute  = 'results.txt'
+
+const STEP = 5, LAPSE = 200
+
+class CarRace {
+  constructor(name) {
+    this.name = name
+    this.pos = 0
+    this.time = 0
+  }
+
+  move() {
+    this.pos += Math.round(STEP * (1 - Math.random()))
+  }
+
+  render() {
+    console.log(' '.repeat(this.pos), this.name);
+  }
+
+  status() {
+    console.log(`${this.name} = post = ${this.pos}, time = ${this.time}`)
+    return `${this.name} = post = ${this.pos}, time = ${this.time}`
+  }
+}
+
+const taxi = new CarRace('taxi')
+const police = new CarRace('police')
+const thief = new CarRace('thief')
+
+let finalTime = 0
+
+let interval = setInterval(() => {
+  console.clear()
+  finalTime += LAPSE
+
+  if(taxi.pos < 100) {
+    taxi.render()
+    taxi.move()
+    taxi.time = finalTime
+  }
+  if(police.pos < 100) {
+    police.render()
+    police.move()
+    police.time = finalTime
+  }
+  if(thief.pos < 100) {
+    thief.render()
+    thief.move()
+    thief.time = finalTime  
+  }
+
+  // if(taxi.pos >= 100) {
+  //   console.log('The race is finished. \nThe winner is the Taxist!!')
+  //   clearInterval(interval)
+  // }
+  // else if(police.pos >= 100) {
+  //   console.log('The race is finished. \nThe winner is the police!!')
+  //   clearInterval(interval)
+  // }
+  // else if(thief.pos >= 100) {
+  //   console.log('The race is finished. \nThe winner is the thief!!')
+  //   clearInterval(interval)
+  // }
+
+  if(taxi.pos >= 100 && police.pos >= 100 && thief.pos >= 100) {
+    const results = [taxi.status(), police.status(), thief.status()]
+    
+    fs.writeFile(fileRoute, results.join('\n'), (error) => {
+      if(error) {
+        alert(error.message)
+        console.log(error.stack)
+        
+        return
+      }
+
+      clearInterval(interval)
+    })
+  }
+}, LAPSE)
