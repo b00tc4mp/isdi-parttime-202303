f063dc21a63b35b60275657bbb19b818f445452a
LucasDDiazCuenca <lucasDamian30@gmail.com>
2023-05-29 17:52:46 +0100

    add forecast logic

---
+++ staff/lucas-diaz/web/app-React/src/logic/weather.js
@@ -0,0 +1,27 @@
+
+
+function retrieveLanzaroteWeather(callback) {
+    var xhr = new XMLHttpRequest
+
+    xhr.onload = () => {
+        const payload = JSON.parse(xhr.response)
+
+        callback(null, payload)
+    }
+
+    xhr.onerror = () => {
+        callback(new Error("conection error"));
+    }
+
+
+    xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat=28.96129906535264&lon=-13.553940113479198&appid=26fb0678db44629f04dc85a19d221f38")
+    xhr.send()
+}
+
+retrieveLanzaroteWeather((error, weather) => {
+    if (error) {
+        alert(error.message)
+        return
+    }
+    console.log(weather)
+})
