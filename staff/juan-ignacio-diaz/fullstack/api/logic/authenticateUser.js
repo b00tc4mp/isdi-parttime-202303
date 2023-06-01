ae3a3f97e6f3de4ca906f314cb9071c3047179a8
Juan Ignacio Díaz Lazaro <juanignacio.diaz@ideauto.com>
2023-06-01 19:12:45 +0200

    ver10.4.2

---
+++ staff/juan-ignacio-diaz/fullstack/api/logic/authenticateUser.js
@@ -0,0 +1,35 @@
+import { validateEmail, validatePassword, validateCallback } from "./helpers/validators"
+
+const { readFile } = require('fs')
+
+module.exports = function authenticateUser(email, password, callback) {
+    validateEmail(email)
+    validatePassword(password)
+    validateCallback(callback)
+
+    readFile('./data/users.json', 'utf8', (error, json) => {
+        if (error) {
+            callback(error)
+
+            return
+        }
+
+        const users = JSON.parse(json)
+
+        const user = users.find(user => user.email === email)
+
+        if (!user) {
+            callback(new Error(`user with email ${email} not found`))
+
+            return
+        }
+
+        if (user.password !== password) {
+            callback(new Error('wrong credentials'))
+
+            return
+        }
+
+        callback(null, user.id)
+    })
+}
---
+++ staff/juan-ignacio-diaz/fullstack/api/logic/retrieveUser.js
@@ -0,0 +1,35 @@
+import { validateId } from './helpers/validators'
+
+const { readFile } = require('fs')
+
+module.exports = function retrieveUser(userId, callback) {
+    validateId(userId)
+
+    readFile('./data/users.json', 'utf8', (error, json) => {
+        if (error) {
+            callback(error)
+
+            return
+        }
+
+        const users = JSON.parse(json)
+
+        const user = users.find(user => user.id === userId)
+
+        if (!user) {
+            callback(new Error(`user with id ${userId} not found`))
+
+            return
+        }
+
+        const tmpuser = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar,
+            favs: user.favs
+        }
+
+        callback(null, user)
+    })
+
+}
