bae0756b252320939745996f985525c42250e756
iCarlosPro <cjps91@gmail.com>
2023-06-03 20:19:00 +0200

    Profile renders now

--- staff/carlos-perez/ReactAsyncApp/App/src/components/Profile.jsx
+++ staff/carlos-perez/ReactAsyncApp/App/src/components/Profile.jsx
@@ -28,27 +28,27 @@ export default function Profile() {
     return <div className="profile-view">
         <button className="boton boton--primario button-close-profile">Cerrar</button>
         <div className="campo-viewonly">
-            <label className="campo__label" for="profile-name">Nombre</label>
+            <label className="campo__label" htmlFor="profile-name">Nombre</label>
             <p className="profile-name" id="profile-name">{name}</p>
         </div>
 
         <div className="mail-change">
             <div className="campo">
-                <label className="campo__label" for="mail-old">Correo antiguo</label>
+                <label className="campo__label" htmlFor="mail-old">Correo antiguo</label>
                 <input className="campo__field mail-old" type="text" placeholder="Tu correo antiguo" id="mail-old"
-                    required> </input>
+                    required/>
             </div>
 
             <div className="campo">
-                <label className="campo__label" for="mail-new">Correo nuevo</label>
+                <label className="campo__label" htmlFor="mail-new">Correo nuevo</label>
                 <input className="campo__field mail-new" type="text" placeholder="Tu correo nuevo" id="mail-new"
-                    required> </input>
+                    required/> 
             </div>
 
             <div className="campo">
-                <label className="campo__label" for="mail-new-check"> Repite correo nuevo</label>
+                <label className="campo__label" htmlFor="mail-new-check"> Repite correo nuevo</label>
                 <input className="campo__field mail-new-check" type="text" placeholder="Tu correo nuevo"
-                    id="mail-check-new" required> </input>
+                    id="mail-check-new" required/> 
             </div>
             <div className="buttons">
 
@@ -59,21 +59,21 @@ export default function Profile() {
         </div>
         <div className="password-change">
             <div className="campo">
-                <label className="campo__label" for="password-old">Contraseña antigua</label>
+                <label className="campo__label" htmlFor="password-old">Contraseña antigua</label>
                 <input className="campo__field password-old" type="password" placeholder="Tu contraseña antigua"
-                    id="password-old" required> </input>
+                    id="password-old" required/> 
             </div>
 
             <div className="campo">
-                <label className="campo__label" for="password-new">Contraseña nueva</label>
+                <label className="campo__label" htmlFor="password-new">Contraseña nueva</label>
                 <input className="campo__field password-new" type="password" placeholder="Tu contraseña nueva"
-                    id="password-new" required> </input>
+                    id="password-new" required/>
             </div>
 
             <div className="campo">
-                <label className="campo__label" for="password-new-check"> Repite contraseña nueva</label>
+                <label className="campo__label" htmlFor="password-new-check"> Repite contraseña nueva</label>
                 <input className="campo__field password-new-check" type="password" placeholder="Tu contraseña nueva"
-                    id="password-new-check" required> </input>
+                    id="password-new-check" required/>
             </div>
 
             <div className="buttons">
