3ef6cc5ec3e087a1d3680c7beac600570f61fb6f
Arnau Ciriquian <arnau.m.ciriquian@gmail.com>
2023-06-05 12:57:21 +0200

    all posts logic added

---
+++ staff/arnau-ciriquian/fullstack/api/logic/toggleFavPost.js
@@ -0,0 +1,64 @@
+import fs from "fs"
+import { validateCallback, validateId } from "../../com/validators.js"
+
+export default function toggleFavPost(userId, postId, callback) {
+    validateId(userId, 'user id')
+    validateId(postId, 'post id')
+    validateCallback(callback)
+
+    fs.readFile('../data/users.json', 'utf-8', (error, json) => {
+        if (error) {
+            callback(error)
+
+            return
+        }
+
+        const users = JSON.parse(json)
+
+        const user = users.find(user => user.id === userId)
+
+        if (!user) {
+            callback(new Error(`user with id ${userId} not found`))
+
+            return
+        }
+
+        fs.readFile('../data/posts.json', 'utf-8', (error, json) => {
+            if (error) {
+                callback(error)
+    
+                return
+            }
+
+            const posts = JSON.parse(json)
+
+            const post = posts.find(post => post.id === postId)
+
+            if (!post) {
+                callback(new Error(`post with id ${postId} not found`))
+
+                return
+            }
+
+            const index = user.favs.indexOf(postId)
+
+            if (index < 0)
+                user.favs.push(postId)
+            else {
+                user.favs.splice(index, 1)
+            }
+
+            json = JSON.stringify(users)
+
+            fs.writeFile('../data/users.json', json, 'utf-8', error => {
+                if (error) {
+                    callback(error)
+    
+                    return
+                }
+    
+                callback(null)
+            })
+        })
+    })
+}
