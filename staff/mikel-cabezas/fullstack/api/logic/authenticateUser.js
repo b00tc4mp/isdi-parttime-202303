f80cac37a17b5b6538d70e3a1acd1cdbb96b3d7c
Mikel Cabezas <mikelcabezas@gmail.com>
2023-06-05 11:30:43 +0200

    start backend logics

---
+++ staff/mikel-cabezas/fullstack/api/logic/authenticateUser.js
@@ -0,0 +1,32 @@
+const { readFile } = require( 'fs' )
+
+module.exports = function authenticateUser(email, password, callback) {
+    // TODO Validate inputs
+
+    readFile('./data/users.json', 'utf8', (error, json) => {
+        if(error) {
+            callback(error)
+
+            return
+        }
+
+        const users = JSON.parse(json)
+
+        const user = users.find(user => user.email === email)
+
+        if(!user) {
+            callback(new Error(`user with email ${email} not found`))
+
+            return
+        }
+
+        if(user.password !== password) {
+            callback(new Error('Incorrect user or password'))
+
+            return
+        }
+
+        callback(null, user.id)
+    })
+
+}
---
+++ staff/mikel-cabezas/fullstack/api/logic/retrieveUser.js
@@ -0,0 +1,32 @@
+const { readFile } = require( 'fs' )
+
+module.exports = function retrieveUser(userId, callback) {
+    // TODO Validate inputs
+
+    readFile('./data/users.json', 'utf8', (error, json) => {
+        if(error) {
+            callback(error)
+
+            return
+        }
+
+        const users = JSON.parse(json)
+
+        const user = users.find(user => user.id === userId)
+
+        if(!user) {
+            callback(new Error(`user with id ${userId} not found`))
+
+            return
+        }
+
+        const _user = {
+            name: user.name, 
+            email: user.email, 
+            image: user.image,
+            favPosts: user.favPosts
+        }        
+        callback(null, _user)
+    })
+
+}
