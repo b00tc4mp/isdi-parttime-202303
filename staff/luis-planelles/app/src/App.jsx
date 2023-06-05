0a6a4c7689e9d9d5e93aac053c86553379a54472
BirdOnTheBranch <planelles.luis@gmail.com>
2023-06-05 11:08:58 +0200

    add context and custom alerts

--- staff/luis-planelles/app/src/App.jsx
+++ staff/luis-planelles/app/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import Context from './Context';
+import Alert from './components/Alert';
+import Loader from './library/Loader';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -6,22 +9,32 @@ import { context } from './ui';
 
 const App = () => {
 
-  const [view, setView ] = useState(context.userId ? 'home' : 'login');
+  const [view, setView ] = useState(context.userId ? 'home' : 'login'),
+    [feedback, setFeedback] = useState(null),
+    [loader, setLoader] = useState(false);
   
   const handleGoToRegister = () =>  setView('register'), 
   
   handleGoToLogin = () =>  setView('login'), 
   
-  handleGoToHome = () => setView('home');
+  handleGoToHome = () => setView('home'),
 
-  switch (view) {
-      case 'login':
-        return <Login onRegisterClick={handleGoToRegister} onUserLoggedIn={handleGoToHome} />;
-      case 'register':
-        return <Register onLoginClick={handleGoToLogin} onUserRegistered={handleGoToLogin}/>;
-      case 'home':
-        return <Home onLoggedOut={handleGoToLogin}/>
-    }
+  handleAcceptAlert = () => setFeedback(null),
+
+  alert = (message, level = 'info') => setFeedback({ message, level }),
+
+  freeze = () => setLoader(true),
+
+  unfreeze = () => setLoader(false);
+
+
+return <Context.Provider value={{ alert, freeze, unfreeze }}>
+        {view === 'login' && <Login onRegisterClick={handleGoToRegister} onUserLoggedIn={handleGoToHome} />}
+        {view === 'register' && <Register onLoginClick={handleGoToLogin} onUserRegistered={handleGoToLogin} />}
+        {view === 'home' && <Home onLoggedOut={handleGoToLogin} />}
+        {feedback && <Alert message={feedback.message} level={feedback.level} onAccept={handleAcceptAlert} />}
+        {loader && <Loader />}
+    </Context.Provider>
 }
 
 export default App
