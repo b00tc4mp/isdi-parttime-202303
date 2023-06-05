c664d72a54ef35a5bac1d50d709098f68a85f788
iCarlosPro <cjps91@gmail.com>
2023-06-05 18:33:09 +0200

    TW fix

---
+++ staff/carlos-perez/ReactAsyncTWApp/App/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+import helloworldPlugin from './tailwind.helloworld.plugin'
+import helloworldPlugin1 from './tailwind.helloworld.plugin.1'
+
+export default {
+  content: ["./index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {}
+  },
+  plugins: [helloworldPlugin, helloworldPlugin1]
+}
