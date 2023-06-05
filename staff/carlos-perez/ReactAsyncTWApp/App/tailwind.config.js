f3ada2430e04648d3909d448e342b5fed64773f7
iCarlosPro <cjps91@gmail.com>
2023-06-05 18:39:41 +0200

    TW adding

--- staff/carlos-perez/ReactAsyncTWApp/App/tailwind.config.js
+++ staff/carlos-perez/ReactAsyncTWApp/App/tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-import helloworldPlugin from './tailwind.helloworld.plugin'
-import helloworldPlugin1 from './tailwind.helloworld.plugin.1'
-
 export default {
   content: ["./index.html", "./src/**/*.jsx"],
   theme: {
     extend: {}
   },
-  plugins: [helloworldPlugin, helloworldPlugin1]
+  plugins: []
 }
