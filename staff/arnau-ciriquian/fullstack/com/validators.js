4648579c57db0673760d07626804b1060d1834ef
Arnau Ciriquian <arnau.m.ciriquian@gmail.com>
2023-06-07 15:09:16 +0200

    com to common js

--- staff/arnau-ciriquian/fullstack/com/validators.js
+++ staff/arnau-ciriquian/fullstack/com/validators.js
@@ -1,10 +1,10 @@
-export function validateEmail(email) {
+function validateEmail(email) {
     if (typeof email !== 'string') throw new Error('email is not a string')
     if (!email.length) throw new Error('email is empty')
     if (!email.match(/\S+@\S+\.\S+/)) throw new Error('email is not a valid adress')
 }
 
-export function validateNewPassword(password, explain = 'password') {
+function validateNewPassword(password, explain = 'password') {
     if (typeof password !== 'string') throw new Error('password is not a string')
     if (password.length < 4) throw new Error(`${explain} is shorter than 4 characters`)
     if (password.search(/[a-z]/) < 0) throw new Error(`${explain} does not include a lowercase`)
@@ -13,37 +13,49 @@ export function validateNewPassword(password, explain = 'password') {
     if (password.search(/\s/) > 0) throw new Error(`${explain} includes a blank space`)
 }
 
-export function validateName(name, explain = 'name') {
+function validateName(name, explain = 'name') {
     if (typeof name !== 'string') throw new Error(`${explain} is not a string`)
     if (!name.length) throw new Error(`${explain} is empty`)
 }
 
-export function validatePasswordConfirm(password, passwordConfirm, explainOne = 'password confirmation', explainTwo ='password') {
+function validatePasswordConfirm(password, passwordConfirm, explainOne = 'password confirmation', explainTwo ='password') {
     if (typeof passwordConfirm !== 'string') throw new Error(`${explain}confirmation is not a string`)
     if(!passwordConfirm.length) throw new Error(`${explainOne} confirmation is empty`)
     if (password !== passwordConfirm) throw new Error(`${explainOne} confirmation is different than ${explainTwo}`)
 }
 
-export function validatePassword(password) {
+function validatePassword(password) {
     if (typeof password !== 'string') throw new Error('password is not a string')
     if(!password.length) throw new Error('password is empty')
 }
 
-export function validateId(id, explain = 'id') {
+function validateId(id, explain = 'id') {
     if (typeof id !== 'string') throw new Error(`${explain} is not a string`)
     if (!id.trim().length) throw new Error(`${explain} is empty`)
 }
 
-export function validateUrl(url, explain = 'url') {
+function validateUrl(url, explain = 'url') {
     if (typeof url !== 'string') throw new Error(`${explain} is not a string`)
     if (!url.trim().length) throw new Error(`${explain} is empty`)
 }
 
-export function validateText(text, explain = 'text') {
+function validateText(text, explain = 'text') {
     if (typeof text !== 'string') throw new Error(`${explain} is not a string`)
     if (!text.trim().length) throw new Error(`${text} is empty`)
 }
 
-export function validateCallback(callback, explain = 'callback') {
+function validateCallback(callback, explain = 'callback') {
     if (typeof callback !== 'function') throw new Error(`${explain} is not a function`)
+}
+
+module.exports = { 
+    validateEmail,
+    validatePassword,
+    validateName,
+    validateNewPassword,
+    validateCallback,
+    validateText,
+    validateId,
+    validateUrl,
+    validatePasswordConfirm
 }
