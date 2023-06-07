6b415ef6e7770db71789a30054250ae0200b18c4
Mikel Cabezas <mikelcabezas@gmail.com>
2023-06-07 19:04:34 +0200

    more implementation on login page with tailwind css

--- staff/mikel-cabezas/fullstack/app/tailwind.baseStyles.plugin.js
+++ staff/mikel-cabezas/fullstack/app/tailwind.baseStyles.plugin.js
@@ -3,8 +3,22 @@ const plugin = require('tailwindcss/plugin')
 export default plugin(({ addBase, addComponents, addUtilities, theme }) => {
   addComponents({
     "input": {
-
+      // 'backgroundColor': 'var(--container-bg)',
+      // 'color': 'var(--main-text)',
+      // 'margin': '5px 5px 10px',
+      // 'padding': '8px 15px',
+      // 'minWidth': '220px',
+      // 'border': 'solid 2px var(--main-button)',
+      // 'borderRadius': '10px',
+      // 'textAlign': 'center',
+      // 'transition': 'linear .2s',
     }
   })
 })
 
+// 
+// ;
+// ;
+// ;
+// ;
+// ;
