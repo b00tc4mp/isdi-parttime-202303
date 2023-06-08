859bcc373e1f4664a456dae34bd0ced76503946e
Arnau Ciriquian <arnau.m.ciriquian@gmail.com>
2023-06-08 15:13:12 +0200

    APP and API backup after common js migration

---
+++ staff/arnau-ciriquian/fullstack/api.6/logic/updatePost.test.js
@@ -0,0 +1,11 @@
+const updatePost = require("./updatePost.js")
+
+updatePost('user-1', 'post-3', 'new image', 'new hello world', error => {
+    if (error) {
+        console.error(error)
+
+        return
+    }
+
+    console.log('post updated')
+})
