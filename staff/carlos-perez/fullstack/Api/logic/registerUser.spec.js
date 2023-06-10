03d9bd0717b75b0c995a509c28b6dfa8b4adf01a
carlos-perez <cjps91@gmail.com>
2023-06-10 19:21:49 +0200

    Testing failing

--- staff/carlos-perez/fullstack/Api/logic/registerUser.spec.js
+++ staff/carlos-perez/fullstack/Api/logic/registerUser.spec.js
@@ -3,13 +3,17 @@ const { readFile, writeFile } = require('fs')
 const registerUser = require('./registerUser')
 
 describe('registerUser', () => {
-    beforeEach(done => writeFile('./data/users.json', '[]', 'utf8', error => done(error)))
+    let name, email, password
 
-    it('should succeed on new user', done => {
-        const name = `name-${Math.random()}`
-        const email = `e-${Math.random()}@mail.com`
-        const password = `password-${Math.random()}`
+    beforeEach(done => {
+        name = `name-${Math.random()}`
+        email = `e-${Math.random()}@mail.com`
+        password = `password-${Math.random()}`
 
+        writeFile('./data/users.json', '[]', 'utf8', error => done(error))
+    })
+
+    it('succeeds on new user', done => {
         registerUser(name, email, password, error => {
             expect(error).to.be.null
 
@@ -33,11 +37,44 @@ describe('registerUser', () => {
         })
     })
 
-    it('should fail on existing user', done => {
-        const name = `name-${Math.random()}`
-        const email = `e-${Math.random()}@mail.com`
-        const password = `password-${Math.random()}`
+    it('succeeds on other existing user', done => {
+        const idCount = Math.round(Math.random() * 100 + 1)
+        const id2 = `user-${idCount}`
+        const name2 = `name-${Math.random()}`
+        const email2 = `e-${Math.random()}@mail.com`
+        const password2 = `password-${Math.random()}`
 
+        const users = [{ id: id2, name: name2, email: email2, password: password2 }]
+        const json = JSON.stringify(users)
+
+        writeFile('./data/users.json', json, 'utf8', error => {
+            expect(error).to.be.null
+
+            registerUser(name, email, password, error => {
+                expect(error).to.be.null
+
+                readFile('./data/users.json', 'utf8', (error, json) => {
+                    expect(error).to.be.null
+
+                    const users = JSON.parse(json)
+
+                    const user = users.find(user => user.email === email)
+
+                    expect(user).to.exist
+                    expect(user.id).to.equal(`user-${idCount + 1}`)
+                    expect(user.name).to.equal(name)
+                    expect(user.email).to.equal(email)
+                    expect(user.password).to.equal(password)
+                    expect(user.avatar).to.be.null
+                    expect(user.favs).to.have.lengthOf(0)
+
+                    done()
+                })
+            })
+        })
+    })
+
+    it('fails on existing user', done => {
         const users = [{ name, email, password }]
         const json = JSON.stringify(users)
 
@@ -52,6 +89,39 @@ describe('registerUser', () => {
             })
         })
     })
+
+    it('fails on empty name', () => {
+        // try {
+        //     registerUser('', email, password, () => { })
+        // } catch (error) {
+        //     expect(error).to.be.instanceOf(Error)
+        //     expect(error.message).to.equal('name is empty')
+        // }
+
+        expect(() => registerUser('', email, password, () => { })).to.throw(Error, 'name is empty')
+    })
+
+    it('fails on empty email', () =>
+        expect(() => registerUser(name, '', password, () => { })).to.throw(Error, 'email is empty')
+    )
+
+    it('fails on non-string name', () => {
+        expect(() => registerUser(undefined, email, password, () => { })).to.throw(Error, 'name is not a string')
+        expect(() => registerUser(1, email, password, () => { })).to.throw(Error, 'name is not a string')
+        expect(() => registerUser(true, email, password, () => { })).to.throw(Error, 'name is not a string')
+        expect(() => registerUser({}, email, password, () => { })).to.throw(Error, 'name is not a string')
+        expect(() => registerUser([], email, password, () => { })).to.throw(Error, 'name is not a string')
+    })
+
+    it('fails on non-string email', () => {
+        expect(() => registerUser(name, undefined, password, () => { })).to.throw(Error, 'email is not a string')
+        expect(() => registerUser(name, 1, password, () => { })).to.throw(Error, 'email is not a string')
+        expect(() => registerUser(name, true, password, () => { })).to.throw(Error, 'email is not a string')
+        expect(() => registerUser(name, {}, password, () => { })).to.throw(Error, 'email is not a string')
+        expect(() => registerUser(name, [], password, () => { })).to.throw(Error, 'email is not a string')
+    })
+
+    // TODO add more unhappies
 
     after(done => writeFile('./data/users.json', '[]', 'utf8', error => done(error)))
 })
