b496d6bba66ca112f164032479feda43ecb2def6
Arnau Ciriquian <arnau.m.ciriquian@gmail.com>
2023-06-10 18:07:33 +0200

    all user related logic now calls api to acces and modify data in api/data/users.json; vite .env configured

---
+++ staff/arnau-ciriquian/fullstack/app.5/src/logic/toggleHidePost.js
@@ -0,0 +1,34 @@
+import { validators } from 'com'
+const { validateCallback, validateId } = validators
+
+import { findPostById, findUserById, savePost } from "../data"
+
+export default function toggleHidePost(userId, postId, callback) {
+    validateId(userId, 'user id')
+    validateId(postId, 'post id')
+    validateCallback(callback)
+
+    findUserById(userId, user => {
+        if (!user) {
+            callback(new Error(`user not found`))
+
+            return
+        }
+
+        findPostById(postId, post => {
+            if (!post) {
+                callback(new Error('post not found'))
+    
+                return
+            }
+
+            if (post.hidden === false) {
+                post.hidden = true
+            } else {
+                post.hidden = false
+            }
+
+            savePost(post, () => callback(null))
+        })
+    })
+}
