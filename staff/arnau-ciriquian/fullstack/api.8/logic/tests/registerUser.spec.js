6af3f62a4ba9ee3640a024fa7f1f305c4d86d708
Arnau Ciriquian <arnau.m.ciriquian@gmail.com>
2023-06-10 18:09:07 +0200

    api and api.8 save after dotenv configuration with DB_PATH and PORT, all user requests from app now acces and modify api/data/user.json

---
+++ staff/arnau-ciriquian/fullstack/api.8/logic/tests/registerUser.spec.js
@@ -0,0 +1,3 @@
+require('dotenv').config() //a cada test
+
+// canviar tots els paths -> "./data/users.json" o "./data/posts.json" a `${process.env.DB_PATH}/users.json` o `${process.env.DB_PATH}/posts.json`
