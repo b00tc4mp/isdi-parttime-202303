bbe0ccf011900345be56dab6f2df24fe6e0257d8
Manuel Barzi <manuelbarzi@gmail.com>
2023-06-12 21:30:20 +0200

    refactor api: extract user id from authorization (bearer token); move utils and helpers to files; move middlewares to files

---
+++ staff/manuel-barzi/fullstack/api.5/logic/updateUserAvatar.js
@@ -0,0 +1,40 @@
+const { readFile, writeFile } = require('fs')
+const { validators: { validateId, validateUrl, validateCallback } } = require('com')
+
+module.exports = (userId, avatar, callback) => {
+    validateId(userId, 'user id')
+    validateUrl(avatar, 'avatar')
+    validateCallback(callback)
+
+    readFile(`${process.env.DB_PATH}/users.json`, (error, json) => {
+        if (error) {
+            callback(error)
+
+            return
+        }
+
+        const users = JSON.parse(json)
+
+        const user = users.find(user => user.id === userId)
+
+        if (!user) {
+            callback(new Error(`user with id ${userId} not found`))
+
+            return
+        }
+
+        user.avatar = avatar
+
+        const json2 = JSON.stringify(users)
+
+        writeFile(`${process.env.DB_PATH}/users.json`, json2, error => {
+            if (error) {
+                callback(error)
+
+                return
+            }
+
+            callback(null)
+        })
+    })
+}
--- staff/manuel-barzi/fullstack/api/index.js
+++ staff/manuel-barzi/fullstack/api/index.js
@@ -1,115 +1,23 @@
 require('dotenv').config()
 
 const express = require('express')
-const { registerUser, authenticateUser, retrieveUser } = require('./logic')
-const updateUserAvatar = require('./logic/updateUserAvatar')
+const { cors, jsonBodyParser } = require('./utils')
+const { helloApiMid, registerUserMid, authenticateUserMid, retrieveUserMid, updateUserAvatarMid, createPostMid } = require('./middlewares')
 
 const api = express()
 
-api.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', '*')
-    res.setHeader('Access-Control-Allow-Methods', '*')
+api.use(cors)
 
-    next()
-})
+api.get('/', helloApiMid)
 
-api.get('/', (req, res) => {
-    debugger
-    res.send('Hello, World!')
-})
+api.post('/users', jsonBodyParser, registerUserMid)
 
-api.get('/helloworld', (req, res) => res.json({ hello: 'world' }))
+api.post('/users/auth', jsonBodyParser, authenticateUserMid)
 
-api.post('/users', (req, res) => {
-    let json = ''
+api.get('/users', retrieveUserMid)
 
-    req.on('data', chunk => json += chunk)
+api.patch('/users', jsonBodyParser, updateUserAvatarMid)
 
-    req.on('end', () => {
-        try {
-            const { name, email, password } = JSON.parse(json)
-
-            registerUser(name, email, password, error => {
-                if (error) {
-                    res.status(400).json({ error: error.message })
-
-                    return
-                }
-
-                res.status(201).send()
-            })
-        } catch (error) {
-            res.status(400).json({ error: error.message })
-        }
-    })
-})
-
-api.post('/users/auth', (req, res) => {
-    let json = ''
-
-    req.on('data', chunk => json += chunk)
-
-    req.on('end', () => {
-        try {
-            const { email, password } = JSON.parse(json)
-
-            authenticateUser(email, password, (error, userId) => {
-                if (error) {
-                    res.status(400).json({ error: error.message })
-
-                    return
-                }
-
-                res.json({ userId })
-            })
-        } catch (error) {
-            res.status(400).json({ error: error.message })
-        }
-    })
-})
-
-api.get('/users/:userId', (req, res) => {
-    try {
-        const { userId } = req.params
-
-        retrieveUser(userId, (error, user) => {
-            if (error) {
-                res.status(400).json({ error: error.message })
-
-                return
-            }
-
-            res.json(user)
-        })
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
-
-api.patch('/users/:userId', (req, res) => {
-    let json = ''
-
-    req.on('data', chunk => json += chunk)
-
-    req.on('end', () => {
-        try {
-            const { userId } = req.params
-            const { avatar } = JSON.parse(json)
-
-            updateUserAvatar(userId, avatar, error => {
-                if (error) {
-                    res.status(400).json({ error: error.message })
-
-                    return
-                }
-
-                res.status(204).send()
-            })
-        } catch (error) {
-            res.status(400).json({ error: error.message })
-        }
-    })
-})
+api.post('/posts', jsonBodyParser, createPostMid)
 
 api.listen(process.env.PORT, () => console.log(`server running in port ${process.env.PORT}`))
