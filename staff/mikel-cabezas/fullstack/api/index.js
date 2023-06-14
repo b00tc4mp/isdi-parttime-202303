36f670ee8ab3e80d338cbec625d7907502474f2f
Mikel Cabezas <mikelcabezas@gmail.com>
2023-06-14 10:14:37 +0200

    fix register user api connection

---
+++ staff/mikel-cabezas/fullstack/api/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const { registerUser } = require('./logic')
+
+const api = express()
+
+// api.get('/helloworld', (req, res) => res.json({ hello: 'world' }))
+
+
+api.post('/users', (req, res) => {
+    let json = ''
+    req.on('data', chunk => {
+        json += chunk
+    })
+
+    req.on('end', () => {
+        try {
+            debugger
+            const { name, email, password } = JSON.parse(json)
+
+            registerUser(name, email, password, error => {
+                if (error) {
+                    res.status(400).json({ error: error.message })
+
+                    return
+                }
+                res.status(201).send()
+            })
+        } catch (error) {
+
+            res.status(400).json({ error: error.message })
+        }
+    })
+})
+
+api.post('/users/auth', (req, res) => {
+    let json = ''
+
+    req.on('data', chunk => {
+        json += chunk
+    })
+
+    // TODO call authenticate user and return userid in json
+})
+api.get('/users/:userId', (req, res) => {
+    // TODO call retrieveUser and return user in json
+})
+
+api.listen(4000)
--- staff/mikel-cabezas/fullstack/api/logic/retrieveUser.js
+++ staff/mikel-cabezas/fullstack/api/logic/retrieveUser.js
@@ -1,34 +1,36 @@
-module.exports = function () { }
+const { readFile } = require('fs')
 
-// const { readFile } = require('fs')
+const { validators: { validateUserId, validateCallback } } = require('com')
 
-// module.exports = function retrieveUser(userId, callback) {
-//     // TODO Validate inputs
+module.exports = (userId, callback) => {
+    validateUserId(userId)
+    validateCallback(callback)
 
-//     readFile('./data/users.json', 'utf8', (error, json) => {
-//         if (error) {
-//             callback(error)
+    readFile('./data/users.json', 'utf8', (error, json) => {
+        if (error) {
+            callback(error)
 
-//             return
-//         }
+            return
+        }
 
-//         const users = JSON.parse(json)
+        const users = JSON.parse(json)
 
-//         const user = users.find(user => user.id === userId)
+        const user = users.find(user => user.id === userId)
 
-//         if (!user) {
-//             callback(new Error(`user with id ${userId} not found`))
+        if (!user) {
+            callback(new Error(`User with id ${userId} not found`))
 
-//             return
-//         }
+            return
+        }
 
-//         const _user = {
-//             name: user.name,
-//             email: user.email,
-//             image: user.image,
-//             favPosts: user.favPosts
-//         }
-//         callback(null, _user)
-//     })
+        const { name, email, avatar } = user
 
-// }
+        const user2 = { name, email, avatar }
+
+        callback(null, user2)
+
+
+    })
+
+
+}
