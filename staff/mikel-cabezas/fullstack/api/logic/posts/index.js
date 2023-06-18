ddf97d583faf61d15d21bf5093fd7939159eedb9
Mikel Cabezas <mikelcabezas@gmail.com>
2023-06-18 18:16:21 +0200

    added retrieve liked posts, saved posts ins api logics. start retrieve post by id and toggle liked post

--- staff/mikel-cabezas/fullstack/api/logic/posts/index.js
+++ staff/mikel-cabezas/fullstack/api/logic/posts/index.js
@@ -2,6 +2,7 @@ module.exports = {
     createPost: require('./createPost'),
     editPost: require('./editPost'),
     deletePost: require('./deletePost'),
-    retrievePosts: require('./retrievePosts')
-
+    retrievePosts: require('./retrievePosts'),
+    retrieveLikedPosts: require('./retrieveLikedPosts'),
+    retrieveSavedPosts: require('./retrieveSavedPosts')
 }
