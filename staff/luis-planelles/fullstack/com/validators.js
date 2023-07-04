5fbc5a625449f42373063a6a4bd6ce278f43d943
BirdOnTheBranch <planelles.luis@gmail.com>
2023-07-04 20:17:03 +0200

    new valition to tokens

--- staff/luis-planelles/fullstack/com/validators.js
+++ staff/luis-planelles/fullstack/com/validators.js
@@ -1,7 +1,7 @@
 const validateName = (name) => {
   const containsSpaces = /\s/g;
 
-  if (typeof name !== 'string') throw new Error('name is not a string');
+  if (typeof name !== 'string') throw new TypeError('name is not a string');
   if (!name.length) throw new Error('name is empty');
   if (containsSpaces.test(name)) throw new Error('name contains spaces');
 
@@ -11,7 +11,7 @@ const validateName = (name) => {
 const validateEmail = (email) => {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  if (typeof email !== 'string') throw new Error('email is not a string');
+  if (typeof email !== 'string') throw new TypeError('email is not a string');
   if (!email.length) throw new Error('email is empty');
   if (!emailRegex.test(email)) throw new Error('invalid email');
 
@@ -26,7 +26,8 @@ const validatePassword = (password, type = 'password') => {
   const hasSpecialChar = /[!@#$%^&*()_+~`=[\]{}|:;"'<,>.?/]/;
   const noWhitespace = /^\S+$/;
 
-  if (typeof password !== 'string') throw new Error(`${type} is not a string`);
+  if (typeof password !== 'string')
+    throw new TypeError(`${type} is not a string`);
   if (!password.length) throw new Error(`${type} is empty`);
   if (!isLongEnough)
     throw new Error(`${type} not be at least 8 characters long`);
@@ -45,32 +46,41 @@ const validatePassword = (password, type = 'password') => {
 };
 
 const validateUrl = (url, explain = 'url') => {
-  if (typeof url !== 'string') throw new Error(`${explain} is not a string`);
+  if (typeof url !== 'string')
+    throw new TypeError(`${explain} is not a string`);
   if (!url.trim().length) throw new Error(`${explain} is empty`);
 };
 
 const validateText = (text, explain = 'text') => {
-  if (typeof text !== 'string') throw new Error(`${explain} is not a string`);
+  if (typeof text !== 'string')
+    throw new TypeError(`${explain} is not a string`);
   if (!text.trim().length) throw new Error(`${explain} is empty`);
 };
 
 const validateNumber = (number, explain = 'number') => {
   if (typeof number !== 'number' || isNaN(number))
-    throw new Error(`${explain} is not a valid number`);
+    throw new TypeError(`${explain} is not a number`);
 };
 
 const validateId = (id, explain = 'id') => {
-  if (typeof id !== 'string') throw new Error(`${explain} is not a string`);
+  if (typeof id !== 'string') throw new TypeError(`${explain} is not a string`);
+  if (token.split('.').length !== 3)
+    throw new Error(`${explain} is not a valid`);
+};
+
+const validateToken = (id, explain = 'token') => {
+  if (typeof id !== 'string') throw new TypeError(`${explain} is not a string`);
   if (!id.trim().length) throw new Error(`${explain} is empty`);
 };
 
 const validateCallback = (callback, explain = 'callback') => {
   if (typeof callback !== 'function')
-    throw new Error(`${explain} is not a function`);
+    throw new TypeError(`${explain} is not a function`);
 };
 
 module.exports = {
   validateId,
+  validateToken,
   validateText,
   validateNumber,
   validateUrl,
