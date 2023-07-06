cac998b86ed0a3a88d02a31801cd5245fa88250c
Mikel Cabezas <mikelcabezas@gmail.com>
2023-07-06 12:17:11 +0200

    add TouchableHighlight in Nearby

--- staff/mikel-cabezas/fullstack/project/app/assets/icons/index.js
+++ staff/mikel-cabezas/fullstack/project/app/assets/icons/index.js
@@ -5,4 +5,5 @@ export const MENU = require('./menu.png');
 export const MY_LOCATION = require('./my_location.png');
 export const PIN = require('./pin.png');
 export const LIKES = require('./likes.png');
-export const MORE_OPTIONS = require('./more_options.png'); 
+export const MORE_OPTIONS = require('./more_options.png');
+export const CLOSE = require('./close.png'); 
