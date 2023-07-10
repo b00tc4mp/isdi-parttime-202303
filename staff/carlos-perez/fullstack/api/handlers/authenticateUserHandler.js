f876c3fe7e2138c760cf12fe3c2da720182e8de3
carlos-perez <cjps91@gmail.com>
2023-07-10 18:57:48 +0200

    Quick auth fix

--- staff/carlos-perez/fullstack/api/handlers/authenticateUserHandler.js
+++ staff/carlos-perez/fullstack/api/handlers/authenticateUserHandler.js
@@ -2,24 +2,18 @@ const { authenticateUser } = require('../logic')
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res) => {
-    try {
+   
         const { email, password } = req.body
 
-        authenticateUser(email, password, (error, userId) => {
-            if (error) {
-                res.status(400).json({ error: error.message })
-
-                return
-            }
+        return authenticateUser(email, password).then((userId) => {
             const payload = { sub: userId }
-
+        
             const { JWT_SECRET, JWT_EXPIRATION } = process.env
-
-            const token = jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRATION })
-
+        
+            const token = jwt.sign(payload, JWT_SECRET, {
+              expiresIn: JWT_EXPIRATION,
+            })
+        
             res.json(token)
-        })
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
+          })
 }
