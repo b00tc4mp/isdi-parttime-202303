c33c1cb640cdb3130c36807f58df22e321358a0b
carlos-perez <cjps91@gmail.com>
2023-07-11 18:28:56 +0200

    Quick fix

--- staff/carlos-perez/fullstack/api/index.js
+++ staff/carlos-perez/fullstack/api/index.js
@@ -4,57 +4,41 @@ const express = require('express')
 const { cors, jsonBodyParser } = require('./utils')
 const { helloApiHandler, registerUserHandler, authenticateUserHandler, retrieveUserHandler, updateUserPasswordHandler, updateUserEmailHandler, createPostHandler, retrievePostHandler, retrievePostsHandler, deletePostHandler, updatePostHandler, toggleLikePostHandler, toggleFavPostHandler } = require('./handlers')
 
-const mongodb = require('mongodb')
+const mongoose = require('mongoose')
 
-const { MongoClient } = mongodb
-const context = require('./logic/context')
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => {
+    const api = express()
 
+    api.use(cors)
 
-const client = new MongoClient(process.env.MONGODB_URL)
+    api.get('/', helloApiHandler)
 
+    api.post('/users', jsonBodyParser, registerUserHandler)
 
-client
-  .connect()
-  
-  .then((connection) => {
-    const db = connection.db()
-    context.users = db.collection('users')
-    context.posts = db.collection('posts')
-  
+    api.post('/users/auth', jsonBodyParser, authenticateUserHandler)
 
-const api = express()
+    api.get('/users', retrieveUserHandler)
 
-api.use(cors)
+    api.patch('/users/password', jsonBodyParser, updateUserPasswordHandler)
 
-api.get('/', helloApiHandler)
+    api.patch('/users/email', jsonBodyParser, updateUserEmailHandler)
 
-api.post('/users', jsonBodyParser, registerUserHandler)
+    api.post('/posts', jsonBodyParser, createPostHandler)
 
-api.post('/users/auth', jsonBodyParser, authenticateUserHandler)
+    api.get('/posts/:postId', retrievePostHandler)
 
-api.get('/users', retrieveUserHandler)
+    api.get('/posts', retrievePostsHandler)
 
-api.patch('/users/password', jsonBodyParser, updateUserPasswordHandler)
+    api.delete('/posts/:postId', deletePostHandler)
 
-api.patch('/users/email', jsonBodyParser, updateUserEmailHandler)
+    api.patch('/posts/:postId', jsonBodyParser, updatePostHandler)
 
-api.post('/posts', jsonBodyParser, createPostHandler)
+    api.patch('/posts/:postId/likes', toggleLikePostHandler)
 
-api.get('/posts/:postId', retrievePostHandler)
+    api.patch('/posts/:postId/favs', toggleFavPostHandler)
 
-api.get('/posts', retrievePostsHandler)
+    api.listen(process.env.PORT, () => console.log(`server running in port ${process.env.PORT}`))
 
-api.delete('/posts/:postId', deletePostHandler)
-
-api.patch('/posts/:postId', jsonBodyParser, updatePostHandler)
-
-api.patch('/posts/:postId/likes', toggleLikePostHandler)
-
-api.patch('/posts/:postId/favs', toggleFavPostHandler)
-
-api.listen(process.env.PORT, () => console.log(`server running in port ${process.env.PORT}`))
-
-})
-.catch((error) => {
-  console.log(error)
-})
+  })
+  .catch(console.error)
