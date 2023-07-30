c13cef8e4e05ad930eee5bdf7745b8515ba2fc85
Mikel Cabezas <mikelcabezas@gmail.com>
2023-07-30 22:53:07 +0200

    update models.js on api

--- staff/mikel-cabezas/fullstack/project/api/data/models.js
+++ staff/mikel-cabezas/fullstack/project/api/data/models.js
@@ -51,6 +51,22 @@ const location = new Schema({
         default: 'Point',
         required: true
     },
+    city: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
     coordinates: {
         type: Array,
         length: 2,
