3e6c78f42fa7a670369ce434aab1500fdb2b0473
Mikel Cabezas <mikelcabezas@gmail.com>
2023-08-04 13:28:38 +0200

    fix models.js file

--- staff/mikel-cabezas/fullstack/project/api/data/models.js
+++ staff/mikel-cabezas/fullstack/project/api/data/models.js
@@ -23,47 +23,158 @@ const user = new Schema({
     },
     favs: {
         type: [ObjectId],
-        defualt: []
+        default: []
     }
-
 })
-const post = new Schema({
+const comments = new Schema({
+    _id: {
+        type: ObjectId,
+        required: true,
+        unique: true
+    },
+    author: {
+        type: ObjectId,
+        required: true
+    },
+    isUtil: {
+        type: Boolean,
+    },
+    comment: {
+        type: String,
+        required: true,
+        maxLength: 200
+    }
+})
+const location = new Schema({
+    type: {
+        type: String,
+        default: 'Point',
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    coordinates: {
+        type: Array,
+        length: 2,
+        required: true
+    }
+})
+const element = new Schema({
+    element: {
+        type: String,
+        required: true
+    },
+    minimumAge: {
+        type: Number,
+        required: true
+    }
+})
+const issue = new Schema({
+    _id: {
+        type: ObjectId,
+        required: true,
+        unique: true
+    },
     author: {
         type: ObjectId,
         ref: 'User',
         required: true
     },
-    image: {
+    title: {
         type: String,
-        required: true,
+        required: true
     },
-    text: {
-        type: String,
-        required: true,
+    description: {
+        type: [String],
+        required: true
+    },
+    isSolved: {
+        type: Boolean,
+        default: false
+    },
+    comments: {
+        type: [comments]
+    }
+})
+
+const playground = new Schema({
+    author: {
+        type: ObjectId,
+        ref: 'User',
+        required: true
     },
     title: {
         type: String,
         required: true,
     },
-    date: {
+    description: {
+        type: String,
+        required: false,
+    },
+    images: {
+        type: [String],
+        required: true,
+        minLength: 1
+    },
+    creationDate: {
         type: Date,
         required: true,
         default: Date.now
     },
+    lastModify: {
+        type: Date,
+        default: Date.now
+    },
     likes: {
         type: [ObjectId],
-        ref: 'User'
+        ref: 'User',
+        required: false
     },
     visibility: {
         type: String,
-        default: 'public'
-    }
+        default: 'public',
+        required: true
+    },
+    location: {
+        type: location,
+        required: true,
+        unique: true,
+
+    },
+    elements: {
+        type: [element],
+        required: true,
+    },
+    sunExposition: {
+        type: Array,
+        required: false
+    },
+    issue: {
+        type: [issue],
+        ref: 'User',
+        required: true,
+    },
 })
 
+
 const User = model('User', user)
-const Post = model('Post', post)
+const Playground = model('Playground', playground)
 
 module.exports = {
     User,
-    Post
+    Playground
 }
