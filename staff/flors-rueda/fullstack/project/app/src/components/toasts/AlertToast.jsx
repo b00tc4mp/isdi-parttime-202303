b298bca6f6c2c2913f851fc2fc7dc020e85196a0
Flors Rueda <ruedaflors@gmail.com>
2023-08-10 11:01:15 +0200

    change alert style

--- staff/flors-rueda/fullstack/project/app/src/components/toasts/AlertToast.jsx
+++ staff/flors-rueda/fullstack/project/app/src/components/toasts/AlertToast.jsx
@@ -8,11 +8,11 @@ const AlertToast = ({ handleCloseAlert, message, log }) => {
 
     return (
         <div className="fixed inset-0 flex items-center w-full h-full justify-center z-50">
-            <div className="flex items-center justify-around w-full max-w-xs text-dark100 bg-danger300 rounded-lg shadow" role="alert">
+            <div className="flex items-center px-1 justify-around w-full max-w-xs text-dark100 bg-light200 border border-danger300 rounded-lg shadow" role="alert">
                 <div className="inline-flex items-center justify-center w-8 h-8 text-dark200">
                     <i className="text-xl bi bi-exclamation-triangle"></i>
                 </div>
-                <div className="text-sm font-bold py-4">{message}</div>
+                <div className="text-sm font-bold pl-1 py-4">{message}</div>
                 <button type="button" onClick={handleCloseAlert} aria-label="Close" className="self-center">
                     <span className="sr-only">Close</span>
                     <i className="text-dark300 hover:text-danger100 bi bi-x-circle text-lg"></i>
