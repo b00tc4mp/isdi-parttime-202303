89f72d8ea5c4bc6edb429008f15569cfdd884913
BirdOnTheBranch <planelles.luis@gmail.com>
2023-08-10 19:24:02 +0200

    006-SP-(feature) add new contenttype error.

--- staff/luis-planelles/fullstack/project/com/validators.js
+++ staff/luis-planelles/fullstack/project/com/validators.js
@@ -1,9 +1,11 @@
+const { ContentError } = require('./errors');
+
 const validateName = (name) => {
   const containsSpaces = /\s/g;
 
   if (typeof name !== 'string') throw new TypeError('name is not a string');
-  if (!name.length) throw new Error('name is empty');
-  if (containsSpaces.test(name)) throw new Error('name contains spaces');
+  if (!name.length) throw new ContentError('name is empty');
+  if (containsSpaces.test(name)) throw new ContentError('name contains spaces');
 
   return name.trim();
 };
@@ -12,8 +14,8 @@ const validateEmail = (email) => {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   if (typeof email !== 'string') throw new TypeError('email is not a string');
-  if (!email.length) throw new Error('email is empty');
-  if (!emailRegex.test(email)) throw new Error('invalid email');
+  if (!email.length) throw new ContentError('email is empty');
+  if (!emailRegex.test(email)) throw new ContentError('invalid email');
 
   return email;
 };
@@ -28,19 +30,19 @@ const validatePassword = (password, type = 'password') => {
 
   if (typeof password !== 'string')
     throw new TypeError(`${type} is not a string`);
-  if (!password.length) throw new Error(`${type} is empty`);
+  if (!password.length) throw new ContentError(`${type} is empty`);
   if (!isLongEnough)
     throw new RangeError(`${type} not be at least 8 characters long`);
   if (!hasDigit.test(password))
-    throw new Error(`${type} not contains one digit`);
+    throw new ContentError(`${type} not contains one digit`);
   if (!hasLowercase.test(password))
-    throw new Error(`${type} not contains one lowercase letter`);
+    throw new ContentError(`${type} not contains one lowercase letter`);
   if (!hasUppercase.test(password))
-    throw new Error(`${type} not contains one uppercase letter`);
+    throw new ContentError(`${type} not contains one uppercase letter`);
   if (!hasSpecialChar.test(password))
-    throw new Error(`${type} not contains one special character`);
+    throw new ContentError(`${type} not contains one special character`);
   if (!noWhitespace.test(password))
-    throw new Error(`${type} contains any whitespace characters`);
+    throw new ContentError(`${type} contains any whitespace characters`);
 
   return password;
 };
