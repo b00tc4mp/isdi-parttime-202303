6ee22ad372ed6acb6699091080831b42c179b284
CristinaGurriaran <121101260+CristinaGurriaran@users.noreply.github.com>
2023-08-13 00:19:51 +0200

    Initial API setup + data models

---
+++ staff/cristina-gurriaran/fullstack/project/api/data/models.js
@@ -0,0 +1,166 @@
+const mongoose = require('mongoose')
+
+const { Schema, Schema: { Types: { ObjectId } }, model } = mongoose
+
+const GeoJSON = require('mongoose-geojson-schema')
+
+const user = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 8
+    },
+    avatar: {
+        type: String
+    },
+    favs: {
+        type: [ObjectId],
+        ref: 'Post'
+    }
+})
+
+const district = new Schema({
+    ciutatVella: Boolean,
+    gracia: Boolean,
+    horta: Boolean,
+    lEixample: Boolean,
+    lesCorts: Boolean,
+    nouBarris: Boolean,
+    santAndreu: Boolean,
+    santMarti: Boolean,
+    santsMontjuic: Boolean,
+    sarriaSantGervasi: Boolean,
+})
+
+const mapLocation = new Schema({
+    location: {
+        type: String,
+        enum: ['Point'],
+        required: true
+    },
+    coordinates: {
+        type: [Number],
+            required: true
+    }
+})
+
+const wifi = new Schema({
+    unlimitedFree: Boolean,
+    timeLimited: Boolean,
+    timeLimitedWithPurchase: Boolean,
+    paidOptions: Boolean,
+    unavailable: Boolean,
+})
+
+const plugs = new Schema({
+    none: Boolean,
+    few: Boolean,
+    plenty: Boolean,
+})
+
+const noise = new Schema({
+    quiet: Boolean,
+    moderate: Boolean,
+    loud: Boolean,
+})
+
+const location = new Schema({
+    street: {
+        type: String,
+        required: true,
+    },
+    postalCode: {
+        type: String,
+        default: 'Point',
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    districts: {
+        type: district,
+        required: true,
+    },
+    mapLocation: {
+        type: mapLocation,
+        required: true,
+    }
+})
+
+const workspot = new Schema({
+    author: {
+        type: ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    location: {
+        type: location,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    type: {
+        coffeeShop: Boolean,
+        restaurant: Boolean,
+        coWorking: Boolean,
+        library: Boolean,
+        hotelLobby: Boolean,
+    },
+    features: {
+        wifi: wifi,
+        plugs: plugs,
+        noise: noise,
+        accessibility: Boolean,
+        petFriendly: Boolean,
+        ensuiteKitchen: Boolean,
+        onSiteRestaurant: Boolean,
+        meetingRooms: Boolean,
+        parking: Boolean,
+        bikeRack: Boolean,
+        storage: Boolean,
+        printScanCopy: Boolean,
+        projector: Boolean,
+        windowView: Boolean,
+    },
+    reviews: [String],
+    likes: {
+        type: [ObjectId],
+        ref: 'User'
+    }
+})
+
+const User = model('User', user)
+const Workspot = model('Workspot', workspot)
+
+module.exports = {
+    User,
+    Workspot
+}
---
+++ staff/cristina-gurriaran/fullstack/project/api/index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const { 
+    registerUserHandler, 
+    authenticateUserHandler, 
+    retrieveUserHandler, 
+    updateUserAvatarHandler, 
+    updateUserPasswordHandler, 
+    createWorkspotHandler,
+    retrieveWorkspotsHandler,
+ } = require('./handlers')
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        const api = express()
+
+        const jsonBodyParser = bodyParser.json()
+
+        api.use(cors())
+
+        api.post('/users', jsonBodyParser, registerUserHandler)
+
+        api.post('/users/auth', jsonBodyParser, authenticateUserHandler)
+
+        api.get('/users', retrieveUserHandler)
+
+        api.patch('/users/avatar', jsonBodyParser, updateUserAvatarHandler)
+
+        api.patch('/users/password', jsonBodyParser, updateUserPasswordHandler)
+
+        api.post('/workspots', jsonBodyParser, createWorkspotHandler)
+
+        api.get('/workspots', retrieveWorkspotsHandler)
+
+
+        api.listen(process.env.PORT, () => console.log(`server running in port ${process.env.PORT}`))
+    })
+    .catch(console.error)
