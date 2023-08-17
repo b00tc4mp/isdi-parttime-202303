c771336d9d3d84ca0d0cac210700860856b6df80
Flors Rueda <ruedaflors@gmail.com>
2023-08-17 20:03:45 +0200

    update socket ip

--- staff/flors-rueda/fullstack/project/app/src/App.jsx
+++ staff/flors-rueda/fullstack/project/app/src/App.jsx
@@ -63,7 +63,7 @@ const App = () => {
 
   useEffect(() => {
     //const socket = socketIOClient('http://localhost:4321');
-    const socket = socketIOClient('http://15.188.51.248:80');
+    const socket = socketIOClient('http://node:4321');
     socket.on('connect', () => {
       const id = socket.id;
       socket.emit('sendSocketId', { id });
