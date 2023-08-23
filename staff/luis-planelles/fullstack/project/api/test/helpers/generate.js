e3f1af6b0989bb31efdfa8774f236ece0a3d4986
BirdOnTheBranch <planelles.luis@gmail.com>
2023-08-23 15:49:49 +0200

    005-SP-(feature) Add generate mission, generate explorer and populate mission feature.

--- staff/luis-planelles/fullstack/project/api/test/helpers/generate.js
+++ staff/luis-planelles/fullstack/project/api/test/helpers/generate.js
@@ -1,5 +1,9 @@
 const { ObjectId } = require('mongodb');
 
+const startDate = new Date();
+const endDate = new Date(startDate);
+endDate.setDate(startDate.getDate() + 1);
+
 const generate = {
   user: () => ({
     _id: new ObjectId(),
@@ -12,6 +16,28 @@ const generate = {
   participant: () => ({
     name: `name-${Math.random()}`,
     email: `e-${Math.random()}@mail.com`,
+  }),
+
+  explorer: (race) => ({
+    race,
+    health: 100,
+    food: 100,
+    water: 100,
+    stress: 100,
+    oxygen: 100,
+  }),
+
+  mission: (user, explorer, participant) => ({
+    _id: new ObjectId(),
+    creator: user._id,
+    traveler: explorer,
+    destination: 'moon',
+    status: 'in_progress',
+    lastUpdate: new Date(),
+    startDate,
+    endDate,
+    participants: [participant],
+    loserPrice: 'beer',
   }),
 
   nasaEvent: (date, type) => ({
--- staff/luis-planelles/fullstack/project/api/test/helpers/populate.js
+++ staff/luis-planelles/fullstack/project/api/test/helpers/populate.js
@@ -1,3 +1,15 @@
-const { User } = require('../../data/models');
+const { User, Mission } = require('../../data/models');
 
 module.exports = (_users) => User.insertMany(_users);
+
+module.exports = (_users, _missions) => {
+  const promises = [];
+
+  promises.push(User.insertMany(_users));
+
+  if (_missions && _missions.length) {
+    promises.push(Mission.insertMany(_missions));
+  }
+
+  return Promise.all(promises);
+};
