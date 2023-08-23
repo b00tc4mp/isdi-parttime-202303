b83e13ade7c72debfcf4e32f7a6acca97274ba3c
iCarlosPro <cjps91@gmail.com>
2023-08-23 16:24:10 +0200

    Improve Footer to transition better  between sizes

--- staff/carlos-perez/fullstack/project/app/amwapp/components/Footer.tsx
+++ staff/carlos-perez/fullstack/project/app/amwapp/components/Footer.tsx
@@ -5,9 +5,10 @@ import { socialMedia, musicPlatforms  } from "@/constants"
 
 const Footer = () => {
     return (
-        <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100 max-w-[1440px] mx-auto navbar-bg justify-center h-fit">
-            <div className="flex justify-between items-center flex-wrap sm:px-16 px-6 py-10">
-                <p>@2023 Alex Maybe. All Rights Reserved</p>
+        <footer className="footer">
+            <div className="footer__elements">
+                <p className="footer__copyright">@2023 Alex Maybe. All Rights Reserved</p>
+                <div className="footer__links-container">
                 <div className="footer__links">
                     {socialMedia.map((link) => (
                         <div key={link.title} className="footer__link">
@@ -44,6 +45,7 @@ const Footer = () => {
                         </div>
                     ))}
 
+                </div>
                 </div>
                 
             </div>
