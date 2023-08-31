5dda24aada601a27e3c43ae2705e5d5e13e32b32
Lara Amadeo <laraamadeo@Toqios-MacBook-Pro.local>
2023-08-31 19:46:06 +0200

    fix retrieveUser to retrieve also cart

--- staff/lara-amadeo/project/api/logic/retrieveUser.js
+++ staff/lara-amadeo/project/api/logic/retrieveUser.js
@@ -10,9 +10,9 @@ module.exports = async function retrieveUser(userId) {
     const foundUser = await User.findById(userId)
     if (!foundUser) throw new ExistanceError(`User with id ${userId} not found`)
 
-    const { username, name, email, avatar, description, tags, location, likedChefs, reviews, availability } = foundUser
+    const { username, name, email, avatar, description, tags, location, likedChefs, reviews, availability, cart } = foundUser
 
-    const user = { username, name, email, avatar, description, tags, location, likedChefs, reviews, availability }
+    const user = { username, name, email, avatar, description, tags, location, likedChefs, reviews, availability, cart }
 
     return user
 }
