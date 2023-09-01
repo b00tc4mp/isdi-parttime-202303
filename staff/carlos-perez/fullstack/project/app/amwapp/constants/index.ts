0d929012d3f081fac829c2364c13a9007bb1f0e1
carlos-perez <cjps91@gmail.com>
2023-09-01 12:50:56 +0200

    Update footer links

--- staff/carlos-perez/fullstack/project/app/amwapp/constants/index.ts
+++ staff/carlos-perez/fullstack/project/app/amwapp/constants/index.ts
@@ -2,9 +2,9 @@ export const socialMedia = [
     {
       title: "Socials",
       links: [
-        { title: "TikTok", url: "/" },
-        { title: "YouTube", url: "/" },
-        { title: "Instagram", url: "/" },
+        { title: "TikTok", url: "https://www.tiktok.com/@alex__maybe" },
+        { title: "YouTube", url: "https://www.youtube.com/@Alex__MayBe" },
+        { title: "Instagram", url: "https://www.instagram.com/Alex__MayBe/" },
         { title: "Discord", url: "/" }
       ],
     },
@@ -14,8 +14,9 @@ export const socialMedia = [
     {
         title: "Music Platforms",
         links: [
-            {title: "Apple Music", url:"/"},
-            {title: "Spotify", url:"/"}
+            {title: "Apple Music", url:"https://music.apple.com/us/artist/maybe/1434057838"},
+            {title: "Spotify", url:"https://open.spotify.com/intl-es/artist/5an0U2xsvTMIWoLdiiN1Kl"},
+            {title: "Amazon", url:"https://music.amazon.es/artists/B008QUC9HS/maybe"},
         ]
     }
   ]
