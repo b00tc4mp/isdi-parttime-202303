13318f116024a6a2c2b4ca77a44d7a973b4403be
Lara Amadeo <laraamadeo@Toqios-MacBook-Pro.local>
2023-09-03 20:02:01 +0200

    add specs for like logics in api

--- staff/lara-amadeo/project/api/logic/retrieveLikedChefs.js
+++ staff/lara-amadeo/project/api/logic/retrieveLikedChefs.js
@@ -2,11 +2,11 @@ const { User } = require('../data/models')
 const { errors: { ExistanceError, AuthError } } = require('../../com')
 
 /**
- * Returns a meal
+ * Returns an porpulated array of 
  * @param {string} meal meal's id
  * @returns {object} the founded meal
  */
-module.exports = async function retrieveMeal(userId) {
+module.exports = async function retrieveLikedChefs(userId) {
     debugger
     const user = await User.findById(userId).populate('likedChefs', '-password -likedChefs -order -selledMeals -__v -cart').lean()
     if (!user) throw new ExistanceError(`User with id ${userId} not found`)
