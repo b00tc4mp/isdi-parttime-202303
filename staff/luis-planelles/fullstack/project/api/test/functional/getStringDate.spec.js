4f6332618f0a165aaccc37f54bfb2e7ee4fa25eb
BirdOnTheBranch <planelles.luis@gmail.com>
2023-09-04 16:41:24 +0200

    014-SP-(feature) New getStringDate function to format Date object and its test.

---
+++ staff/luis-planelles/fullstack/project/api/test/functional/getStringDate.spec.js
@@ -0,0 +1,32 @@
+const { expect } = require('chai');
+
+const { getStringDate } = require('../../logic/helpers');
+
+describe('getStringDate', () => {
+  it('success on format a Date object"', () => {
+    const date = new Date('2023-09-15');
+
+    const formattedDate = getStringDate(date);
+
+    expect(formattedDate).to.equal('2023-09-14');
+  });
+
+  it('should format a Date object for single-digit months and days', () => {
+    const date = new Date('2023-01-05');
+
+    const formattedDate = getStringDate(date);
+
+    expect(formattedDate).to.equal('2023-01-04');
+  });
+
+  it('should raise a TypeError if date is not a valid Date object', () => {
+    const date = '2023-01-05';
+
+    try {
+      getStringDate(date);
+    } catch (error) {
+      expect(error).to.be.instanceOf(TypeError);
+      expect(error.message).to.equal('date is not a valid Date object');
+    }
+  });
+});
