ccacc0d15e512fed11a78d54d6cedd3d3cb775bb
Juan Ignacio Díaz Lazaro <juanignacio.diaz@ideauto.com>
2023-09-05 21:12:45 +0200

    update filteredProduct

--- staff/juan-ignacio-diaz/project/api/logic/lists/products/reviewFilteredProducts.js
+++ staff/juan-ignacio-diaz/project/api/logic/lists/products/reviewFilteredProducts.js
@@ -31,59 +31,70 @@ module.exports = (listId, userId, filter, order) => {
         if (!list) throw new ExistenceError('list not found')
 
         if (!(list.guests.some(tmpId => tmpId.toString() === userId))) throw new InvalidDataError('invalid user')
-       
-        const tmpList = await List.find(filter, 'products._id products.name products.date products.howMany products.state products.type products.stores products.comment products.view, products.buyer')
+
+        const tmpList = await List.find({_id: filter._id}, 'products._id products.name products.date products.howMany products.state products.type products.stores products.comment products.view, products.buyer, products.price')
             .populate('products.likes', 'name avatar').lean()
+ 
+        if(tmpList.length>0) {           
+            let products = tmpList[0].products
+            if(products.length>0) {
+                if (filter.storesCheck && filter.stores)
+                    products = products.filter(product => product.stores.some(store => filter.stores.some(fstore => fstore === store.toString())))
+                if (filter.typeCheck && filter.type)
+                     products = products.filter(product => filter.type.some(ftype => ftype === product.type))
+                if (filter.statesCheck && filter.states)
+                     products = products.filter(product => filter.states.some(fstate => fstate === product.state))   
+                if (filter.likesCheck && filter.likes)
+                     products = products.filter(product => product.likes.length > filter.likes)                     
 
-            
-        const products = tmpList[0].products
-        if(products.length>0) {
-            products.forEach(product => {                        
-                product.id = product._id.toString()
-                delete product._id
+                products.forEach(product => {                        
+                    product.id = product._id.toString()
+                    delete product._id
 
-                if (product.likes.length>0) {
-                    product.likes.forEach(like => {
-                        if (like._id) {
-                            like.id = like._id.toString()
-                            delete like._id
-                        }
-                    })
-                } 
+                    if (product.likes.length>0) {
+                        product.likes.forEach(like => {
+                            if (like._id) {
+                                like.id = like._id.toString()
+                                delete like._id
+                            }
+                        })
+                    } 
 
-                if(product.buyer && product.buyer.toString() === userId) {
-                    delete product.buyer
-                    product.buyer = true
-                }
-                else {
-                    product.buyer = false
-                }
-                
+                    if(product.buyer && product.buyer.toString() === userId) {
+                        delete product.buyer
+                        product.buyer = true
+                    }
+                    else {
+                        product.buyer = false
+                    }
+                    
 
-                if (product.view) {
-                    product.untried = product.view.some(user => user._id.toString() === userId)
-                    delete product.view
-                }
-            })
-
-            if(order !== '') {
-                const orderField = order
-
-                products.sort((a,b) =>{
-                    if(a[orderField] > b[orderField]) return -1
-                    if(a[orderField] < b[orderField]) return 1
-                    return 0
+                    if (product.view) {
+                        product.untried = product.view.some(user => user._id.toString() === userId)
+                        delete product.view
+                    }
                 })
-            }
-   
-            const listView = await List.findById(listId, 'products')
-            const productsView = listView.products
-            productsView.forEach(product => {  
-                product.view = product.view.filter(tmpUser => tmpUser._id.toString() !== userId)
-            })
-            listView.save()
 
+                if(order !== '') {
+                    const orderField = order
+
+                    products.sort((a,b) =>{
+                        if(a[orderField] > b[orderField]) return -1
+                        if(a[orderField] < b[orderField]) return 1
+                        return 0
+                    })
+                }
+    
+                const listView = await List.findById(listId, 'products')
+                const productsView = listView.products
+                productsView.forEach(product => {  
+                    product.view = product.view.filter(tmpUser => tmpUser._id.toString() !== userId)
+                })
+                listView.save()
+
+            }
+            return products
         }
-        return products
+        return []
     })()
 }
