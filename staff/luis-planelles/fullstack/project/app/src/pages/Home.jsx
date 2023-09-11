3e2bc2e5388199e44b732691435ea5c1ab7a88ce
BirdOnTheBranch <planelles.luis@gmail.com>
2023-09-11 13:07:09 +0200

    019-SP-(feature) Implement a new function to log out the user, manage tokens, and add a button on the homepage.

--- staff/luis-planelles/fullstack/project/app/src/pages/Home.jsx
+++ staff/luis-planelles/fullstack/project/app/src/pages/Home.jsx
@@ -1,11 +1,20 @@
+import { useAppContext } from '../hooks'
+import logoutUser from '../logic/logoutUser'
 
 const Home = () => {
- 
+  const { navigate} = useAppContext()
+
+  const hadleLogOutButton = () =>  {
+    logoutUser()
+    
+    navigate('/login')
+  }
   
   return <div className='home page container'>
           <header className='home-header'>
           </header>
             <p>Space pursuit</p>
+            <button className='home-header-logout' onClick={hadleLogOutButton}>logout</button>
           <footer className='home-footer'>
           </footer>
         </div>
