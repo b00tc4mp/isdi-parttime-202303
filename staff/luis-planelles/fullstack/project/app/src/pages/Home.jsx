e87cb3e67bc8d108ce12573e381ef11444ac1c3e
BirdOnTheBranch <planelles.luis@gmail.com>
2023-09-12 18:49:15 +0200

    027-SP-(feature) Add logic to retrieve user from api endpoint and use it at home page.

--- staff/luis-planelles/fullstack/project/app/src/pages/Home.jsx
+++ staff/luis-planelles/fullstack/project/app/src/pages/Home.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AddMissionModal from '../components/AddMissionModal'
 import Missions from '../components/Missions'
 import { useAppContext } from '../hooks'
 import logoutUser from '../logic/logoutUser'
+import retrieveUser from '../logic/retrieveUser'
 
 const Home = () => {
   const { navigate} = useAppContext()
 
   const [view, setView] = useState('mission'),
     [modal, setModal] = useState(null),
-    [lastUpdate, setLastUpdate] = useState(null)
+    [lastUpdate, setLastUpdate] = useState(null),
+    [user, setUser] = useState(null);
 
+  useEffect(() => {
+  
+    try {
+      retrieveUser()
+      .then(setUser)
+      .catch(error => alert(error.message))
+      
+    } catch (error){
+      alert(error.message)
+      }
+    }, [])
 
-  const handleOpenAddPost = () => setModal('add-mission')
+  const handleOpenAddMission = () => setModal('add-mission')
 
   const handleCloseModal = () => setModal(null)
   
@@ -29,6 +42,12 @@ const Home = () => {
   
   return <div className='home page container'>
           <header className='home-header'>
+            {user && (
+            <nav className="home-header-nav">
+              <p>{user.name}</p>
+            </nav>
+            )}
+            <button className='home-header-logout' onClick={hadleLogOutButton}>logout</button>
           </header>
             <p>Space pursuit</p>
             <main>
@@ -43,9 +62,8 @@ const Home = () => {
               />
             )}
             </main>
-            <button className='home-header-logout' onClick={hadleLogOutButton}>logout</button>
           <footer className='home-footer'>
-          <button className='add-mission-button' onClick={handleOpenAddPost}>+</button>
+          <button className='add-mission-button' onClick={handleOpenAddMission}>+</button>
           </footer>
         </div>
     }
