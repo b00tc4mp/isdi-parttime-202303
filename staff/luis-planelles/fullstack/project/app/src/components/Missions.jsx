d14b86d00e17f587b263ce829a2c6a2ee9472c91
BirdOnTheBranch <planelles.luis@gmail.com>
2023-09-14 10:39:02 +0200

    032-SP-(feature) Call API to update missions and invoke a function in missions to retrieve the updated data.

--- staff/luis-planelles/fullstack/project/app/src/components/Missions.jsx
+++ staff/luis-planelles/fullstack/project/app/src/components/Missions.jsx
@@ -1,25 +1,33 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import retrieveMissions from '../logic/retrieveMissions';
+import updateMission from '../logic/updateMission';
 import { context } from '../ui';
 import Mission from './Mission';
 
 
 const Missions = ({lastMissionUpdate }) => {
     const [missions, setMissions] = useState();
+    const [missionStatus, setMissionStatus] = useState()
 
     useEffect(() => handleRefreshMission(), [])
 
     const handleRefreshMission = () => {
-        try {
-
-            retrieveMissions(context.token).then((missions) => {
-
-            setMissions(missions)
-            }).catch(error => alert(error))
-        } catch (error) {
-            alert(error.message)
-        }
+      try {
+        retrieveMissions(context.token)
+          .then(async (missions) => {
+            for (const mission of missions) {
+              if (mission.status === 'in_progress') {
+                await updateMission(mission.id);
+                setMissionStatus(mission.status)
+              }
+            }
+            setMissions(missions);
+          })
+          .catch((error) => alert(error));
+      } catch (error) {
+        alert(error.message);
+      }
     };
     
     useEffect(() =>{
