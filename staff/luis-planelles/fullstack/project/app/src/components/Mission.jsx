2397c1c9f8ba0cfc06c13c23063b27c4ecc0aa3f
BirdOnTheBranch <planelles.luis@gmail.com>
2023-09-14 10:30:22 +0200

    025-SP-(refactor) Remove the userId parameter from the logic and enhance the mission JSX.

--- staff/luis-planelles/fullstack/project/app/src/components/Mission.jsx
+++ staff/luis-planelles/fullstack/project/app/src/components/Mission.jsx
@@ -3,39 +3,56 @@ import formatDateTime from '../logic/formatDateTime';
 const Mission = (
   { 
     mission: 
-    { 
-    id, 
-    traveler, 
-    destination, 
-    status, 
-    participants, 
-    loserPrice, 
-    startDate, 
-    endDate 
-    } 
+    {
+      id, 
+      traveler, 
+      destination, 
+      status, 
+      participants, 
+      loserPrice, 
+      startDate, 
+      endDate
+    }
   }) => {
 
-  const { name } = participants[0];
-  const { race } = traveler[0];
+  const missionNumber = id.slice(-3);
 
   return (
     <article className='mission bg-white shadow-md rounded-lg p-4 my-4 border'>
       <div className='mission-header'>
-        <div className='text-xl font-semibold'>{race}</div>
-        <div className='text-lg'>{destination}</div>
-        <div className={`text-sm ${status === 'in_progress' ? 'text-green-500' : 'text-red-500'}`}>
-          {status}
+      <div className='text-xl font-semibold'> Mission {missionNumber} </div>
+      <br />
+        <div className='text-lg'>Explorer:</div>
+        <ul className='list-disc ml-6'>
+          {traveler.map((travelerInfo, index) => (
+            <li key={index} className='text-lg'>{travelerInfo.race}</li>
+          ))}
+        </ul>
+        <div className='text-lg'>Destination:</div>
+        <ul className='list-disc ml-6'>
+          <li className='text-lg'>{destination}</li>
+        </ul>
+        <div className={`text-sm ${status === 'in_progress' ? 'text-gray-500' : status === 'failure' ? 'text-red-500' : 'text-green-500'}`}>
+          Mission Status: {status}
         </div>
-        <div className='text-lg'>{name}</div>
-        <div className='text-lg'>{loserPrice}</div>
+        <div className='text-lg'>Participants:</div>
+        <ul className='list-disc ml-6'>
+          {participants.map((participant, index) => (
+            <li key={index} className='text-lg'>{participant.name}</li>
+          ))}
+        </ul>
+        <div className='text-lg'>Loser Price:</div>
+        <ul className='list-disc ml-6'>
+          <li className='text-lg'>{loserPrice}</li>
+        </ul>
         <div className='text-sm'>
           <time className='text-xs' dateTime={startDate}>
-            {formatDateTime(startDate)}
+            Mission Starts: {formatDateTime(startDate)}
           </time>
         </div>
         <div className='text-sm'>
           <time className='text-xs' dateTime={endDate}>
-            {formatDateTime(endDate)}
+            Mission Ends: {formatDateTime(endDate)}
           </time>
         </div>
       </div>
@@ -44,4 +61,6 @@ const Mission = (
 };
 
 export default Mission;
+
+
 
--- staff/luis-planelles/fullstack/project/app/src/logic/retrieveMission.js
+++ staff/luis-planelles/fullstack/project/app/src/logic/retrieveMission.js
@@ -1,5 +1,4 @@
 import { validators } from 'com';
-import context from './context';
 
 const { validateId } = validators;
 
@@ -9,7 +8,6 @@ const retrieveMission = (missionId) => {
   return fetch(`${import.meta.env.VITE_API_URL}/missions/${missionId}`, {
     headers: {
       'Content-type': 'application/json',
-      Authorization: `Bearer ${context.token}`,
     },
   }).then((res) => {
     if (res.status !== 200)
