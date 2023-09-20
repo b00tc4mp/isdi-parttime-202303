aeb757d85b095b4f69697b9856ef5e5312d0a6fc
BirdOnTheBranch <planelles.luis@gmail.com>
2023-09-20 12:34:32 +0200

    013-SP (refactor) Deleted a function after delete in api.

--- staff/luis-planelles/fullstack/project/api/handlers/retrieveNasaDataHandler.js
+++
@@ -1,8 +0,0 @@
-const { retrieveNasaData } = require('../logic');
-const { handleErrors, extractUserId } = require('./helpers');
-
-module.exports = handleErrors((req, res) => {
-  const userId = extractUserId(req);
-
-  return retrieveNasaData(userId).then((data) => res.json(data));
-});
