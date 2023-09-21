d570e2e7a8474e5d40ebc1899fcdaba2a987998b
BirdOnTheBranch <planelles.luis@gmail.com>
2023-09-21 11:56:42 +0200

    015-SP-(refactor) Delete console log.

--- staff/luis-planelles/fullstack/project/api/handlers/retrieveNasaEventsHandler.js
+++ staff/luis-planelles/fullstack/project/api/handlers/retrieveNasaEventsHandler.js
@@ -4,6 +4,5 @@ const { handleErrors } = require('./helpers');
 module.exports = handleErrors((req, res) => {
   const { missionId } = req.params;
 
-  console.log(missionId);
   return retrieveNasaEvents(missionId).then((events) => res.json(events));
 });
