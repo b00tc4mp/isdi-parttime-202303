9ebf0b30159d6e51538454c4a9ffcf3cc1ea7946
BirdOnTheBranch <planelles.luis@gmail.com>
2023-09-28 23:44:32 +0200

    037-SP-(feature) New logic to participant feedback and page with form confirmation on mission.

--- staff/luis-planelles/fullstack/project/app/src/App.jsx
+++ staff/luis-planelles/fullstack/project/app/src/App.jsx
@@ -5,6 +5,7 @@ import isUserLoggedIn from './logic/isUserLoggedIn';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import MissionDetail from './pages/MissionDetail';
+import ParticipantConfirmation from './pages/ParticipantConfimation';
 import Register from './pages/Register';
 
 const { Provider } = AppContext
@@ -14,10 +15,11 @@ const App = () => {
   
   return <Provider value={{ alert, navigate }}>
             <Routes>
-              <Route path="/login" element={isUserLoggedIn() ? <Navigate to="/" /> : <Login />} />
-              <Route path="/" element={isUserLoggedIn() ? <Home /> : <Navigate to="/login" />} />
-              <Route path="/register" element={isUserLoggedIn() ? <Navigate to="/" /> : <Register />} />
-              <Route path="/mission/:missionId" element={<MissionDetail />} />
+              <Route path='/login' element={isUserLoggedIn() ? <Navigate to='/' /> : <Login />} />
+              <Route path='/' element={isUserLoggedIn() ? <Home /> : <Navigate to='/login' />} />
+              <Route path='/register' element={isUserLoggedIn() ? <Navigate to='/' /> : <Register />} />
+              <Route path='/mission/:missionId' element={<MissionDetail />} />
+              <Route path='/participant-feedback/:missionId/:participantId' element={<ParticipantConfirmation />} />
             </Routes>
           </Provider>
 };
