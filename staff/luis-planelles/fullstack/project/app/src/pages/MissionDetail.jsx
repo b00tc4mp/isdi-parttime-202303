637c060942f4286be05c505869ae9df9753ef446
BirdOnTheBranch <planelles.luis@gmail.com>
2023-10-02 20:21:31 +0200

    029-SP-(refactor) Easthetic changes in display page and charts, and listen events changes to re render page.

--- staff/luis-planelles/fullstack/project/app/src/pages/MissionDetail.jsx
+++ staff/luis-planelles/fullstack/project/app/src/pages/MissionDetail.jsx
@@ -66,7 +66,7 @@ const MissionDetail = () => {
     } catch (error){
       alert(error.message)
       }
-    }, []);
+    }, [events]);
 
   const updateTimer = (endDate) => {
     const timeRemainingData = calculateTimeRemaining(endDate);
@@ -91,17 +91,19 @@ const MissionDetail = () => {
   };
 
   const chartOptions = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
       },
-      title: {
-        display: true,
+      y: {
+        display: false,
       },
     },
   };
-
+  
+  
   let travelerImage = null;
 
   if (mission && mission.traveler[0]) {
@@ -126,139 +128,142 @@ const MissionDetail = () => {
     }
   }
   return (
-<div className='flex justify-center items-start mt-8'>
-  <article className='mission bg-white shadow-md rounded-lg p-4 my-4 border'>
-    <div className='flex justify-between'>
-      <div>
-        <div className='text-xl font-semibold'>
-          MISSION: {missionId.slice(-3)}
-        </div>
-        {mission && (
-        <div className='text-lg'>
-          DEFIANT: {mission.creatorName}
-        </div>
-        )}
-      </div>
-      <div className='flex flex-col'>
-        <button
-          onClick={copyToClipboard}
-          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2'
-        >
-          Copy link
-        </button>
-        <br />
-        {context.token && 
-        <p className='mt-2'>
-          <Link to='/' className='text-blue-500 hover:underline'>Back to Home</Link>
-        </p>
-        }
-      </div>
-    </div>
-        <div>
-          <div className='flex flex-row items-start'>
-            <div 
-              style={{ width: '24rem', height: '24rem' }} 
-              className='mr-4 rounded-full object-cover'
-              >
-              {travelerImage}
-            </div>
-            <div>
-              <Bar
-                data={chartData}
-                options={{ ...chartOptions, aspectRatio: 1 }}
-                style={{ width: '22rem', height: '22rem' }}
-              />
-            </div>
+    <div className='flex justify-center items-start mt-8'>
+      <article className='mission bg-white shadow-md rounded-lg p-4 my-4 border'>
+        <div className="mx-auto px-4 text-white bg-blue-500 rounded-lg">
+          <div className='flex justify-between'>
+            <footer className="w-full py-16">
+              <div className="flex justify-between items-center">
+                <div className="flex flex-col">
+                  <div className="font-bold text-black text-lg mb-2">
+                    <span style={{ color: 'black' }}>MISSION:</span> {missionId.slice(-3)}
+                  </div>
+                  {mission && (
+                    <div className="font-bold text-black text-lg mt-2">
+                      <span style={{ color: 'black' }}>DEFIANT:</span> {mission.creatorName}
+                    </div>
+                  )}
+                </div>
+                <div className="flex flex-col">
+                  <button
+                    onClick={copyToClipboard}
+                    className="bg-white hover:bg-gray-300 text-blue-500 font-bold py-1 px-4 mt-4 rounded mb-2"
+                  >
+                    Copy link
+                  </button>
+                  {context.token && (
+                    <p className="back-home-button">
+                      <Link to="/">Back to Home</Link>
+                    </p>
+                  )}
+                </div>
+              </div>
+            </footer>
           </div>
         </div>
         <br />
-        <div className='mission-header'>
-          {mission && (
             <div>
-              <div className='text-lg'>
-                EXPLORER: {mission.traveler[0].race}
-              </div>
-              <div className='text-lg'>DESTINATION: {mission.destination}</div>
-              <div
-                className={`text-sm ${
-                  mission.status === 'in_progress'
-                    ? 'text-gray-500'
-                    : mission.status === 'failure'
-                      ? 'text-red-500'
-                      : 'text-green-500'
-                }`}
-              >
-                MISSION STATUS: {mission.status}
-              </div>
-              BET VS:
-              {mission.participants.filter((participant) => participant.confirmation === 'accept').length > 0 ? (
-                mission.participants
-                  .filter((participant) => participant.confirmation === 'accept')
-                  .map((participant, index) => (
-                    <div key={index}>
-                      <div className="text-lg">{participant.name}</div>
-                    </div>
-                  ))
-              ) : (
-                <div className="text-red-500">
-                  No participants have accepted the mission yet, we are waiting for feedback.
+              <div className='flex flex-row items-start'>
+                <div 
+                  style={{ width: '17.5rem', height: '17.5rem', padding: '0.5rem', marginTop: 'rem', background: '#4299E1'}} 
+                  className='mr-4 rounded-full object-cover'
+                  >
+                  {travelerImage}
                 </div>
-              )}
-              <div className='text-lg'>LOSER PRICE: {mission.loserPrice}</div>
-              <div className='text-sm'>
-                <time className='text-xs' dateTime={mission.startDate}>
-                  MISSION STARTS: {formatDateTime(mission.startDate)}
-                </time>
-              </div>
-              <div className='text-sm'>
-                <time className='text-xs' dateTime={mission.endDate}>
-                  MISSION ENDS: {formatDateTime(mission.endDate)}
-                </time>
-              </div>
-
-              {mission.status === 'in_progress' && (
                 <div>
-                  <h3>TIME REMAINING:</h3>
-                  <p>
-                    {
-                      `${timeRemaining.days} days, 
-                      ${timeRemaining.hours} hours, 
-                      ${timeRemaining.minutes} minutes, 
-                      ${timeRemaining.seconds} seconds`
-                    }
-                  </p>
+                  <Bar
+                    data={chartData}
+                    options={{ ...chartOptions, aspectRatio: 1 }}
+                    style={{ width: '20rem', height: '20rem', padding: '1rem'}}
+                  />
                 </div>
-              )}
+              </div>
             </div>
-          )}
-          {mission && mission.status === 'failure' && (
-            <div className='flex justify-center items-start mt-8'>
-              <article className='mission bg-white shadow-md rounded-lg p-4 my-4 border'>
-                {mission.participants.map((participant, index) => (
-                  <div key={index} className='text-lg text-green-500'>
-                    Congratulations {participant.name}! You are the winner.
+            <br />
+            <div className='mission-header'>
+            {mission && (
+              <div className='flex justify-between'>
+                <div className='flex-1 justify-start'>
+                  <div className='text-lg mr-4'>
+                  <strong>DESTINATION: </strong>
+                    {mission.destination} {' '}
+                  <span 
+                    className={`${mission.status === 'in_progress' 
+                    ? 'text-gray-500' 
+                    : mission.status === 'failure' 
+                    ? 'text-red-500' 
+                    : 'text-green-500'}`}
+                    > 
+                    {mission.status}
+                  </span>
+                </div>
+                <div className="text-sm mt-2">
+                  <strong>BET VS:</strong>
+                  {mission.status === 'in_progress'
+                    ? mission.participants.map((participant, index) => (
+                        <div key={index}>
+                          <div className='text-lg font-roboto'>
+                            {participant.name}
+                            <span 
+                            className={
+                              `${participant.confirmation === 'pending' 
+                                ? 'text-blue-500' 
+                                : participant.confirmation === 'accept' 
+                                ? 'text-green-500' 
+                                : 'text-red-500'
+                              }`
+                            }
+                              > 
+                              {' '}{participant.confirmation}
+                              </span>
+                          </div>
+                        </div>
+                      ))
+                    : mission.participants
+                        .filter((participant) => participant.confirmation === 'accept')
+                        .map((participant, index) => (
+                          <div key={index}>
+                            <div className='text-lg font-roboto'>
+                              {participant.name} <span className='text-green-500'> {participant.confirmation}</span>
+                            </div>
+                          </div>
+                        ))}
+                </div>
+              </div>
+                <div className='flex-1'>
+                <div className="ml-2">
+                  <div className='text-lg'>
+                    <strong >LOSER PRICE:</strong> {mission.loserPrice}
                   </div>
-                ))}
-                <div className='text-lg'>
-                  {mission.creatorName} owes you a {mission.loserPrice}.
+                  <div className='text-sm'>
+                    <strong >MISSION STARTS:</strong> {formatDateTime(mission.startDate)}
+                  </div>
+                    <br />
+                  {mission.status === 'in_progress' && (
+                    <div>
+                      <h3 className="text-black"><strong>TIME REMAINING:</strong></h3>
+                      <p>
+                        {
+                          `${timeRemaining.days} days, 
+                          ${timeRemaining.hours} hours, 
+                          ${timeRemaining.minutes} minutes, 
+                          ${timeRemaining.seconds} seconds`
+                        }
+                      </p>
+                    </div>
+                  )}
+                  {mission.status !== 'in_progress' && (
+                    <div className='text-sm'>
+                      <strong >MISSION ENDS:</strong> {formatDateTime(events[0].date)}
+                    </div>
+                  )}
                 </div>
-              </article>
+              </div>
+              </div>
+            )}
             </div>
-          )}
-          {mission && mission.status === 'success' && (
-            <div className='text-lg text-green-500'>
-              The {mission.traveler[0].race} has reached the {mission.destination}. 
-              Therefore, {mission.participants.map((participant, index) => (
-                <span key={index}>
-                  {participant.name}
-                  {index < mission.participants.length - 1 ? ', ' : ''}
-                </span>
-              ))} 
-              owes {mission.creatorName} a {mission.loserPrice}.
-            </div>
-          )}
-          {events && events.length > 0 && (
-            <div className='mt-4 p-4 border border-gray-300 rounded-lg'>
+            {events && events.length > 0 && (
+            <div className='mt-4 p-4 border rounded-lg'>
               <h2 className='text-lg font-semibold mb-2'>EVENTS DAMAGE REPORT:</h2>
               {events.map((event, index) => (
                 <div key={index} className='mb-4'>
@@ -280,12 +285,11 @@ const MissionDetail = () => {
                     </a>
                   </div>
                 </div>
-              ))}
-            </div>
-          )}
-        </div>
-      </article>
-    </div>
+              ))} 
+              </div>
+            )}
+        </article>
+      </div>
   );
 }
 
